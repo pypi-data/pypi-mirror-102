345db5c6689dce836aa12dd8db89468f3c92316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:46 +0000

    Add package animec/0.1.1/animec-0.1.1-py3-none-any.whl

---
+++ packages/animec/animec-0.1.1-py3-none-any.whl/animec/__init__.py
@@ -0,0 +1,13 @@
+from .charsearch import *
+from .aninews import *
+from .anicore import *
+
+'''
+An unofficial API to get data about anime characters, anime news and more.
+'''
+
+__author__ = "DriftAsimov"
+__title__ = "animec"
+__copyright__ = "Copyright Drift Asimov 2021"
+__version__ = "0.1.1"
+__license__ = "MIT"
---
+++ packages/animec/animec-0.1.1-py3-none-any.whl/animec/aninews.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+class TooManyRequests(Exception):
+    pass
+
+class aninews:
+
+    def __init__(self, amount: int = 3):
+
+        if amount >= 15:
+            raise TooManyRequests("Keep requests below 15 to avoid stressing the API.")
+
+        html_page = urlopen('https://myanimelist.net/news/tag/new_anime')
+        soup = BeautifulSoup(html_page, 'html.parser')
+
+        news_container = soup.findAll('div', {'class' : 'news-unit clearfix rect'})
+        stripped_list = news_container[:amount]
+
+        titles, links, description, images = [], [], [], []
+
+        for i in stripped_list:
+            
+            text = i.findChildren("p")[0]
+            a = text.find('a', href = True)
+            text = a.get_text()
+            
+            link = a['href']
+
+            image = i.find("img")
+            image_url = image['src']
+
+            desc = i.findChildren("div", {'class' : 'text'})[0]
+
+            titles.append(text)
+            links.append(link)
+            description.append(" ".join(desc.text.split()))
+            images.append(image_url)
+
+        news = zip(titles, links, description, images)
+
+        self.__init__ = list(news)
+        
+        self.titles = titles
+        self.links = links
+        self.description = description
+        self.images = images
