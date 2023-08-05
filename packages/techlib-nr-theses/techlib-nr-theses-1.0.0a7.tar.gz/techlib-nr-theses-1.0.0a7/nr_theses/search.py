507ab044377d74706800a344c353109b74b59fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:24 +0000

    Add package techlib-nr-theses/1.0.0a7/techlib-nr-theses-1.0.0a7.tar.gz

---
+++ packages/techlib-nr-theses/techlib-nr-theses-1.0.0a7.tar.gz/techlib-nr-theses-1.0.0a7/nr_theses/search.py
@@ -0,0 +1,17 @@
+from nr_common.search import NRRecordsSearch
+
+
+class ThesisRecordSearch(NRRecordsSearch):
+    LIST_SOURCE_FIELDS = [
+        'control_number', 'oarepo:validity.valid', 'oarepo:draft',
+        'title', 'dateIssued', 'creator', 'resourceType',
+        'contributor', 'keywords', 'subject', 'abstract', 'state',
+        '_administration.primaryCommunity',
+        '_administration.communities',
+        '$schema'
+    ]
+    HIGHLIGHT_FIELDS = {
+        'title.cs': None,
+        'title._': None,
+        'title.en': None
+    }
