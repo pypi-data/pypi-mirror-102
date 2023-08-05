6a009e94eb1b60870b535e84ca4f23abdd7d4816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:35 +0000

    Add package dk-test-py2/0.0.14/dk_test_py2-0.0.14-py3-none-any.whl

---
+++ packages/dk-test-py2/dk_test_py2-0.0.14-py3-none-any.whl/collpay/__init__.py
@@ -0,0 +1,24 @@
+from collpay.api import Collpay
+
+# env types
+ENV_PRODUCTION = 1
+ENV_SANDBOX = 2
+
+# versions
+V1 = "v1"
+
+# payment transaction statuses
+TRANSACTION_PROCESSING = "Processing"
+TRANSACTION_NOTIFIED = "Notified"
+TRANSACTION_PARTIALLY_PAID = "Partially Paid"
+TRANSACTION_EXPIRED = "Expired"
+TRANSACTION_CONFIRMED = "Confirmed & Done"
+# TRANSACTION_COMPLETED = "Completed"
+# TRANSACTION_FAILED = "Failed"
+# TRANSACTION_REJECTED = "Rejected"
+# TRANSACTION_BLOCKED = "Blocked"
+# TRANSACTION_REFUNDED = "Refunded"
+# TRANSACTION_VOIDED = "Voided"
+
+# ipn or webhook events
+PAYMENT_EVENT = "payment"
