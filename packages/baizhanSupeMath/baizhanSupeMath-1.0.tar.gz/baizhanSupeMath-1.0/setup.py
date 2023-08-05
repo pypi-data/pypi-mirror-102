0772ea33e5fa1c7b7e144eaeee29333b628ee188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package baizhanSupeMath/1.0/baizhanSupeMath-1.0.tar.gz

---
+++ packages/baizhanSupeMath/baizhanSupeMath-1.0.tar.gz/baizhanSupeMath-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="baizhanSupeMath",
+    version="1.0",
+    description="这是第一个对外发布的模块，里面只有数学方法，测试哦",
+    author="gaoqi",
+    author_email="gaoqi110@163.com",
+    py_modules=["baizhanSuperMath.demo1","baizhanSuperMath.demo2"]
+)
