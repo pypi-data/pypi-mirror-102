45eb500100a733fc665c0b5ec7c2cd0f2ea62480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package syneto-api/0.2.5/syneto_api-0.2.5.tar.gz

---
+++ packages/syneto-api/syneto_api-0.2.5.tar.gz/syneto_api-0.2.5/src/syneto_api/__init__.py
@@ -0,0 +1,5 @@
+from .api_client import APIException
+from .auth_api import Authentication
+from .virt_api import Virtualization
+from .storage_api import Storage
+from .protection_api import Protection
