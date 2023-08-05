7ee8d62cc491628d96a07a420da5a021aa0c5eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:10 +0000

    Add package oarepo-taxonomies/3.0.0a2/oarepo_taxonomies-3.0.0a2.tar.gz

---
+++ packages/oarepo-taxonomies/oarepo_taxonomies-3.0.0a2.tar.gz/oarepo_taxonomies-3.0.0a2/oarepo_taxonomies/mappings.py
@@ -0,0 +1,29 @@
+import deepmerge
+from oarepo_mapping_includes import Mapping
+
+
+def taxonomy_term(content=None, **kwargs):
+    ret = {
+        "type": "nested",
+        "properties": {
+            "is_ancestor": {
+                "type": "boolean"
+            },
+            "links": {
+                "type": "object",
+                "properties": {
+                    "self": {
+                        "type": "keyword"
+                    },
+                    "parent": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "level": {
+                "type": "integer"
+            }
+        }
+    }
+    ret = deepmerge.always_merger.merge(ret, content)
+    return Mapping(ret, merge=False)
