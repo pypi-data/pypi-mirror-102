dbd72888281faec869e51569903e749af8eea02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:33 +0000

    Add package seven_wxapp_zxchj/1.0.0/seven_wxapp_zxchj-1.0.0.tar.gz

---
+++ packages/seven_wxapp_zxchj/seven_wxapp_zxchj-1.0.0.tar.gz/seven_wxapp_zxchj-1.0.0/seven_wxapp_zxchj/handlers/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJingCan
+@Date: 2020-04-16 14:38:22
+:LastEditTime: 2021-04-13 14:38:59
+:LastEditors: HuangJingCan
+@Description: 
+"""
+__all__ = ["buy_order", "coin", "coupon", "erp", "exchange_order", "home_page", "lottery", "machine", "pay", "prize_order", "task", "user"]
---
+++ packages/seven_wxapp_zxchj/seven_wxapp_zxchj-1.0.0.tar.gz/seven_wxapp_zxchj-1.0.0/seven_wxapp_zxchj/handlers/base/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJingCan
+@Date: 2020-04-16 14:38:22
+:LastEditTime: 2021-04-13 14:39:24
+:LastEditors: HuangJingCan
+@Description: 
+"""
+__all__ = ["core_base", "erp_base"]
