dd3d2b7c53b816773af3bb51f3086038e590d5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package brainmri-ps/0.1.2/brainmri_ps-0.1.2.tar.gz

---
+++ packages/brainmri-ps/brainmri_ps-0.1.2.tar.gz/brainmri_ps-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import os
+from pkg_resources import parse_requirements
+from setuptools import setup
+install_requires = []
+requirements = 'requirements.txt'
+if os.path.isfile(requirements):
+    with open(requirements) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='brainmri_ps',
+    version='0.1.2',
+    packages=['brainmri_ps'],
+    url='',
+    license='LICENSE.txt',
+    author='lhkhiem, tuantran',
+    author_email='',
+    description='Automatically classify Brain MRI series by pulse sequence types: DWI, FLAIR, T1, T2, and Others',
+    install_requires=install_requires,
+    package_data={'checkpoints': ['checkpoints/*.pt']}
+)
