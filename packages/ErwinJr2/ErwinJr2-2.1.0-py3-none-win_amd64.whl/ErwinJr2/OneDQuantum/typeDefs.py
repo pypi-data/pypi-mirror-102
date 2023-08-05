a68095fd325ee1d364b3bb9756feba2f0aba7ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:55 +0000

    Add package ErwinJr2/2.1.0/ErwinJr2-2.1.0-py3-none-win_amd64.whl

---
+++ packages/ErwinJr2/ErwinJr2-2.1.0-py3-none-win_amd64.whl/ErwinJr2/OneDQuantum/typeDefs.py
@@ -0,0 +1,12 @@
+import typing
+import numpy as np
+
+floatOrArray = typing.Union[float, np.ndarray]
+doubleArray = np.ctypeslib.ndpointer(
+    dtype=np.float64, ndim=1, flags="C_CONTIGUOUS")
+doubleMatrix = np.ctypeslib.ndpointer(dtype=np.float64,
+                                      ndim=2, flags="C_CONTIGUOUS")
+intArray = np.ctypeslib.ndpointer(
+    dtype=np.int32, ndim=1, flags="C_CONTIGUOUS")
+
+# vim: ts=4 sw=4 sts=4 expandtab
