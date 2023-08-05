2eebd510bb70572fa07e3ec2d7129a46551cf160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:34 +0000

    Add package quicly-redis/0.0.2/quicly-redis-0.0.2.tar.gz

---
+++ packages/quicly-redis/quicly-redis-0.0.2.tar.gz/quicly-redis-0.0.2/quicly_redis/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+from .quicly_redis import (
+    QuiclyRedisClient,
+    QuiclyRedisLock,
+    QuiclyRedisLockError,
+    QuiclyRedisInt,
+    QuiclyRedisVariable,
+    QuiclyRedisReentrantLock,
+)
---
+++ packages/quicly-redis/quicly-redis-0.0.2.tar.gz/quicly-redis-0.0.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+import os
+import setuptools
+
+requirements = []
+
+requirements_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')
+if os.path.isfile(requirements_txt):
+    with open(requirements_txt, 'r+') as fo:
+        for line in fo.readlines():
+            item = line.split('#')[0].strip()
+            if len(item):
+                requirements.append(item)
+
+setuptools.setup(
+    name="quicly-redis",
+    version="0.0.2",
+    keywords=["redis"],
+    author="muxiaofei",
+    author_email="muxiaofei@gmail.com",
+    description="Quicly Redis",
+    long_description="",
+    long_description_content_type="text/x-rst",
+    url="https://gitee.com/muxiaofei/quicly-redis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+)
