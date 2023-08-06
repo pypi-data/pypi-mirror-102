9d9fdcf1dd052ce1e215f402f7033d231c569273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:18 +0000

    Add package pydocgen/0.0.4rc0/pydocgen-0.0.4rc0.tar.gz

---
+++ packages/pydocgen/pydocgen-0.0.4rc0.tar.gz/pydocgen-0.0.4rc0/setup.py
@@ -0,0 +1,34 @@
+"""Setup script for pydocgen"""
+
+import os.path
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="pydocgen",
+    version="0.0.4rc",
+    description="Automatically generate docstrings and documentation files for your code!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/m0bi5/pydocgen",
+    author="m0bi5",
+    author_email="",
+    license="MIT",
+    classifiers=[
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    packages=["pydocgen"],
+    include_package_data=True,
+    install_requires=[
+        "pdoc3", "Jinja2"
+    ],
+    entry_points={"console_scripts": ["pydocgen=pydocgen.__main__:main"]},
+)
