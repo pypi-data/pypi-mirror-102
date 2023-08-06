1b02dd951decddb847a6043e235e11bbbaf38172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:21 +0000

    Add package retratodefases/0.1.1/retratodefases-0.1.1-py3-none-any.whl

---
+++ packages/retratodefases/retratodefases-0.1.1-py3-none-any.whl/retratodefases/utils/utils.py
@@ -0,0 +1,31 @@
+from ..exceptions import exceptions2D
+
+def is_number(x):
+    return isinstance(x, (float,int))
+
+def is_range(U):
+    return isinstance(U, (list,tuple))
+
+def construct_interval_1d(var):
+    try:
+        if is_number(var):
+            return sorted([-var, var])
+        if is_range(var):
+             return sorted(var)
+    except Exception as e:
+        raise exceptions2D.RangoInvalid(f"{var} como rango 1d dio el error: "+str(e))
+
+def construct_interval_2d(var, *, depht=0):
+    try:
+        if is_number(var):
+            if depht == 0:
+                return [sorted([0, var])]*2
+            elif depht == 1:
+                return sorted([-var, var])
+        if is_range(var):
+            if depht == 0:
+                return [construct_interval_2d(i, depht=depht+1) for i in var]
+            if depht == 1:
+                return var
+    except Exception as e:
+        raise exceptions2D.RangoInvalid(f"{var} como rango 2d dio el error: "+str(e))
