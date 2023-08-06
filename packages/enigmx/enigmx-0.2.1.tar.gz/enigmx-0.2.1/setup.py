24b3c728d611bc5af4405488d737d403d8936f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:48 +0000

    Add package enigmx/0.2.1/enigmx-0.2.1.tar.gz

---
+++ packages/enigmx/enigmx-0.2.1.tar.gz/enigmx-0.2.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'enigmx',      
+  include_package_data = True,
+  packages=setuptools.find_packages(),
+  version = '0.2.1',    
+  license='MIT',        
+  description = 'enigmx package',   
+  author = 'Quantmoon Technologies', 
+  author_email = 'info@quantmoon.tech',
+  url = 'https://www.quantmoon.tech',   
+  install_requires=[        
+          "pandas>=0.22.0",
+          "numpy>=1.18.2",
+          "zarr",
+          "fracdiff==0.1.0",
+          "xarray",
+          "finnhub-python",
+          "tsfresh",
+          "SQLAlchemy",
+          "ray",
+          "numba",
+          "pandas-market-calendars",
+          "pyodbc", "imbalanced-learn", "TA-Lib", "matplotlib"
+      ],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+  ],
+  python_requires = ">=3.5",
+)
