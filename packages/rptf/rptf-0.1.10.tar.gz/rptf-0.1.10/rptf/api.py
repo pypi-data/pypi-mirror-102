a165dfe5a64a7f53b86d500e0876e79b5e1eb949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:57 +0000

    Add package rptf/0.1.10/rptf-0.1.10.tar.gz

---
+++ packages/rptf/rptf-0.1.10.tar.gz/rptf-0.1.10/rptf/api.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+import requests
+
+
+def headers():
+    return {'Private-Token': os.environ['GITLAB_ACCESS_TOKEN']}
+
+
+def trigger_pipeline(project_id, ref='master', variables=None):
+    # prepare variables
+    if variables is None:
+        variables = {}
+    variables_list = []
+    for key in variables.keys():
+        variables_list.append({'key': key, 'value': variables[key]})
+    # request
+    url = 'https://gitlab.com/api/v4/projects/%s/pipeline?ref=%s' % (project_id, ref)
+    res = requests.post(url=url, headers=headers(), json={'variables': variables_list})
+    res_dict = json.loads(res.text)
+    return res_dict['id'], res_dict['web_url']
+
+
+def get_pipeline(project_id, pipeline_id):
+    url = 'https://gitlab.com/api/v4/projects/%s/pipelines/%s' % (project_id, pipeline_id)
+    res = requests.get(url=url, headers=headers())
+    return json.loads(res.text)
+
+
+def get_jobs(project_id, pipeline_id):
+    url = 'https://gitlab.com/api/v4/projects/%s/pipelines/%s/jobs' % (project_id, pipeline_id)
+    res = requests.get(url=url, headers=headers())
+    return json.loads(res.text)
