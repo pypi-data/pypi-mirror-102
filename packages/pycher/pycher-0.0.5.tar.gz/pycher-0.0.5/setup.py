90dcc14d9a4d951d54392c9007c89ff2e27d33b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:50 +0000

    Add package pycher/0.0.5/pycher-0.0.5.tar.gz

---
+++ packages/pycher/pycher-0.0.5.tar.gz/pycher-0.0.5/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pycher",
+    version="0.0.5",
+    description="Quick desktop python launcher",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahidalgob/pycher",
+    author="Augusto Hidalgo",
+    author_email="ahidalgo94@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=['pycher'],
+
+    include_package_data=True,
+    install_requires=[],
+    entry_points={"console_scripts": [
+        "pycher=pycher.__main__:main",
+    ]},
+)
