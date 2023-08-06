81ba644069a5f62a1aeaa7d94bf6cdec08d713ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:23 +0000

    Add package mkrunner/1.2.0/mkrunner-1.2.0.tar.gz

---
+++ packages/mkrunner/mkrunner-1.2.0.tar.gz/mkrunner-1.2.0/mkrunner/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "1.2.0"
+__description__ = "One-stop solution for HTTP(S) testing."
+
+# import firstly for monkey patch if needed
+from mkrunner.ext.locust import main_locusts
+from mkrunner.parser import parse_parameters as Parameters
+from mkrunner.runner import mkrunner
+from mkrunner.testcase import Config, Step, RunRequest, RunTestCase
+from mkrunner.log import logger
+
+
+
+__all__ = [
+    "logger",
+    "__version__",
+    "__description__",
+    "mkrunner",
+    "Config",
+    "Step",
+    "RunRequest",
+    "RunTestCase",
+    "Parameters",
+]
