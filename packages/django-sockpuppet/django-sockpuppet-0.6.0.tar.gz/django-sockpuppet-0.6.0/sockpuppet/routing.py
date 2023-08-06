5ef705a43526b32698fea82270318e7ca3a09188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:08 +0000

    Add package django-sockpuppet/0.6.0/django-sockpuppet-0.6.0.tar.gz

---
+++ packages/django-sockpuppet/django-sockpuppet-0.6.0.tar.gz/django-sockpuppet-0.6.0/sockpuppet/routing.py
@@ -0,0 +1,23 @@
+from django.urls import re_path
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import channels
+
+channels_version = channels.__version__.split('.')[0]
+if int(channels_version) >= 3:
+    from sockpuppet.consumer import SockpuppetConsumerAsgi as SockpuppetConsumer
+    socket_patterns = [
+        re_path(r'ws/sockpuppet-sync', SockpuppetConsumer.as_asgi())
+    ]
+else:
+    from sockpuppet.consumer import SockpuppetConsumer
+    socket_patterns = [re_path(r'ws/sockpuppet-sync', SockpuppetConsumer)]
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(socket_patterns)
+        ),
+    )
+})
