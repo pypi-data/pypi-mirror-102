48a42884f0c8e120151d7005c24bc7335075da43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:47 +0000

    Add package ncbi-api/0.2/ncbi_api-0.2-py3-none-any.whl

---
+++ packages/ncbi-api/ncbi_api-0.2-py3-none-any.whl/ncbi_api/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+DateTime   : 2021/04/16 15:25
+Author     : ZhangYafei
+Description: 
+"""
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
