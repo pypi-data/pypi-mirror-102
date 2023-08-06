e291d9ce667c7adf3ec55966c8d7a61e1e528b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:38 +0000

    Add package rndm-2/0.0.1/rndm-2-0.0.1.tar.gz

---
+++ packages/rndm-2/rndm-2-0.0.1.tar.gz/rndm-2-0.0.1/README.md
@@ -0,0 +1,1 @@
+small random number package
---
+++ packages/rndm-2/rndm-2-0.0.1.tar.gz/rndm-2-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rndm-2", # Replace with your own username
+    version="0.0.1",
+    scripts=['src/rndm'],
+    author="Tommy Kuan",
+    author_email="kuan0020@umn.edu",
+    description="A small example random number package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
