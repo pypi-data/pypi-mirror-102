9f087550e3ded45c2a13160c546a98da662a0848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:04 +0000

    Add package athletelistsbhp/1.1.0/athletelistsbhp-1.1.0.tar.gz

---
+++ packages/athletelistsbhp/athletelistsbhp-1.1.0.tar.gz/athletelistsbhp-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'athletelistsbhp',
+    version = '1.1.0',
+    py_modules = ['athletelistsbhp'],
+    author = 'sourav',
+    author_email = 'bhsourav17@gmail.com',
+    description = 'Athlete list class'
+    )
