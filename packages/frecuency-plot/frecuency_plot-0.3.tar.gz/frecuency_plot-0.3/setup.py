f9731643b439f25a7964dac954897d7f999ecc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:40 +0000

    Add package frecuency-plot/0.3/frecuency_plot-0.3.tar.gz

---
+++ packages/frecuency-plot/frecuency_plot-0.3.tar.gz/frecuency_plot-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='frecuency_plot',
+      version='0.3',
+      description='Example package to bar-plot percentages',
+      packages=['frecuency_plot'],
+      author = 'Ruben Vilela Freire',
+      author_email = 'rubenvf92@gmail.com',
+      url="https://github.com/rubenvf/Frecuency-plot-package",
+      zip_safe=False)
