a0b3504da14855caae13b42c58af6284064b7d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:59 +0000

    Add package django-graphs/0.0.1/django-graphs-0.0.1.tar.gz

---
+++ packages/django-graphs/django-graphs-0.0.1.tar.gz/django-graphs-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="django-graphs",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://www.merixstudio.com/",
+    author="Merixstudio",
+    author_email="code@merixstudio.com",
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
