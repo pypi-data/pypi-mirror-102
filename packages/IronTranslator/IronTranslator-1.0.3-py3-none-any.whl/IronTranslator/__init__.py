7a45665b23612224b5b00c8aebaf96a3f87c00a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:03 +0000

    Add package IronTranslator/1.0.3/IronTranslator-1.0.3-py3-none-any.whl

---
+++ packages/IronTranslator/IronTranslator-1.0.3-py3-none-any.whl/IronTranslator/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 31 20:59:08 2021
+
+@author: M.ABDELMOULA
+"""
+
+from constants import LANGUAGES_CODES, LANGUAGES_NAMES, MAIN_LINK, SPECIAL_CHAR
+from GoogleTranslate import Translator
