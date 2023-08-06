6b5392b2ab4a5344cc045befc53ef30693c6390a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:46 +0000

    Add package logfmt-logger/0.1.2/logfmt-logger-0.1.2.tar.gz

---
+++ packages/logfmt-logger/logfmt-logger-0.1.2.tar.gz/logfmt-logger-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['logfmt_logger']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'logfmt-logger',
+    'version': '0.1.2',
+    'description': 'Logger following the logfmt format with sound defaults',
+    'long_description': None,
+    'author': 'Michael Mercier',
+    'author_email': 'michael.mercier@ryax.tech',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
