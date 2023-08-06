6acae196ca804b8ed52774a26c0ec9b8bbb591e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:57 +0000

    Add package notecoin/0.2.1/notecoin-0.2.1-py3-none-any.whl

---
+++ packages/notecoin/notecoin-0.2.1-py3-none-any.whl/notecoin/huobi/model/__init__.py
@@ -0,0 +1,8 @@
+class Response:
+    def __init__(self, dict_data, code=None, data=None):
+        self.code = dict_data.get('code', -1)
+        self.data = dict_data.get('data', [])
+        self.dict_data = dict_data
+
+    def __str__(self):
+        return self.code + " " + len(self.data)
