c64ea3cb5d9e5357bcfaf433eb22dd07630c8d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:32 +0000

    Add package datawidgets/0.0.8.post1/datawidgets-0.0.8.post1-py3-none-any.whl

---
+++ packages/datawidgets/datawidgets-0.0.8.post1-py3-none-any.whl/datawidgets/all.py
@@ -0,0 +1,6 @@
+from datawidgets.imports import *
+from datawidgets.data import *
+from datawidgets.dataset import *
+from datawidgets.ui import *
+from datawidgets.utils import *
+from datawidgets.interface import *
