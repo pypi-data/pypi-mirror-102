28a5e7120d7a95632ee13d84e6873183bbe34341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:03 +0000

    Add package verta/0.17.6a3/verta-0.17.6a3.tar.gz

---
+++ packages/verta/verta-0.17.6a3.tar.gz/verta-0.17.6a3/verta/operations/monitoring/client.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import itertools
+
+from verta._tracking import _Context
+
+from .monitored_entity import MonitoredEntity
+from .notification_channel._entities import NotificationChannels
+from .profilers import Profilers
+from .summaries import Summaries, SummarySamples
+from .labels import Labels
+from .alert._entities import Alerts
+
+
+class Client(object):
+    def __init__ (self, verta_client):
+        self._client = verta_client
+        self.profilers = Profilers(self._conn, self._conf, self._client)
+        self.summaries = Summaries(self._conn, self._conf)
+        self.summary_samples = SummarySamples(self._conn, self._conf)
+        self.labels = Labels(self._conn, self._conf)
+
+    @property
+    def _conn(self):
+        return self._client._conn
+
+    @property
+    def _conf(self):
+        return self._client._conf
+
+    @property
+    def _ctx(self):
+        return self._client._ctx
+
+    @property
+    def alerts(self):
+        return Alerts(self._conn, self._conf)
+
+    @property
+    def notification_channels(self):
+        return NotificationChannels(self._conn, self._conf)
+
+    def get_or_create_monitored_entity(self, name=None, workspace=None, id=None):
+        if name is not None and id is not None:
+            raise ValueError("cannot specify both `name` and `id`")
+
+        name = self._client._set_from_config_if_none(name, "monitored_entity")
+        if workspace is None:
+            workspace = self._client.get_workspace()
+
+        ctx = self._ctx
+        ctx.workspace_name = workspace
+
+        resource_name = "MonitoredEntity"
+        if id is not None:
+            entity = MonitoredEntity._get_by_id(self._conn, self._conf, id)
+        else:
+            entity = MonitoredEntity._get_or_create_by_name(
+                self._conn,
+                name,
+                lambda name: MonitoredEntity._get_by_name(
+                    self._conn, self._conf, name=name, parent=workspace
+                ),
+                lambda name: MonitoredEntity._create(
+                    self._conn, self._conf, ctx, name=name
+                ),
+                lambda: self.__noop_checker(),
+            )
+
+        entity._set_client(self)
+        return entity
+
+
+    @staticmethod
+    def __error():
+        raise NotImplementedError()
+
+    @staticmethod
+    def __noop_checker():
+        pass
