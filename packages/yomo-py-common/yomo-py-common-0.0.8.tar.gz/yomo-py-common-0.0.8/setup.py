a4dffef1a98b597425e64370425eedf868f26e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:45 +0000

    Add package yomo-py-common/0.0.8/yomo-py-common-0.0.8.tar.gz

---
+++ packages/yomo-py-common/yomo-py-common-0.0.8.tar.gz/yomo-py-common-0.0.8/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'yomo-py-common',
+  packages = ['yomoPyCommon', 'yomoPyCommon/db', 'yomoPyCommon/message'],
+  include_package_data=True,
+  version = '0.0.8',
+  license='MIT',
+  description = 'Define some common function for easy use',
+  author = 'Chunhua Zhang',
+  author_email = 'syunka.tyo@gmail.com',
+  url = 'https://github.com/luyomo/yomo-py-common',
+  download_url = 'https://github.com/luyomo/yomo-py-common/archive/refs/tags/0.0.8.tar.gz',
+  keywords = ['postgres', 'wechat'] ,
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
---
+++ packages/yomo-py-common/yomo-py-common-0.0.8.tar.gz/yomo-py-common-0.0.8/yomoPyCommon/__init__.py
@@ -0,0 +1,5 @@
+from yomoPyCommon.db               import PGBase
+from yomoPyCommon.db               import MongoData
+from yomoPyCommon.message          import wechat
+
+__all__ = ['db', 'message']
