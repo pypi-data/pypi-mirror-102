4c5bb710f4ceaf5d1a05406a1aac31c68faaa6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:15 +0000

    Add package glocaltokens/0.3.1/glocaltokens-0.3.1-py3-none-any.whl

---
+++ packages/glocaltokens/glocaltokens-0.3.1-py3-none-any.whl/glocaltokens/utils/network.py
@@ -0,0 +1,32 @@
+"""Network utilities"""
+import socket
+
+
+def is_valid_ipv4_address(address: str) -> bool:
+    """Check if ip address is ipv4"""
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count(".") == 3
+    except socket.error:  # not a valid address
+        return False
+
+    return True
+
+
+def is_valid_ipv6_address(address: str) -> bool:
+    """Check if ip address is ipv6"""
+    try:
+        socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:  # not a valid address
+        return False
+    return True
+
+
+def is_valid_port(port: int) -> bool:
+    """Check if port is valid"""
+    return 0 <= port <= 65535
