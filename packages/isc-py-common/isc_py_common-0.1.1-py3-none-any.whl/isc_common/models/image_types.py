85cc11a2aa063b8acf4e5390e75b4dc0334dd503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:50 +0000

    Add package isc-py-common/0.1.1/isc_py_common-0.1.1-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.1.1-py3-none-any.whl/isc_common/models/image_types.py
@@ -0,0 +1,48 @@
+import logging
+
+from isc_common.models.base_ref import BaseRefHierarcy, BaseRefManager, BaseRefQuerySet
+
+logger = logging.getLogger(__name__)
+
+
+class Image_typesQuerySet(BaseRefQuerySet):
+    def delete(self):
+        return super().delete()
+
+    def create(self, **kwargs):
+        return super().create(**kwargs)
+
+    def filter(self, *args, **kwargs):
+        return super().filter(*args, **kwargs)
+
+
+class Image_typesManager(BaseRefManager):
+
+    @staticmethod
+    def getRecord(record):
+        res = {
+            'id': record.id,
+            'code': record.code,
+            'name': record.name,
+            'description': record.description,
+            'parent': record.parent.id if record.parent else None,
+            'editing': record.editing,
+            'deliting': record.deliting,
+        }
+        return res
+
+    def get_queryset(self):
+        return Image_typesQuerySet(self.model, using=self._db)
+
+
+class Image_types(BaseRefHierarcy):
+    objects = Image_typesManager()
+
+    def __str__(self):
+        return f'ID:{self.id}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    class Meta:
+        verbose_name = 'Image types'
