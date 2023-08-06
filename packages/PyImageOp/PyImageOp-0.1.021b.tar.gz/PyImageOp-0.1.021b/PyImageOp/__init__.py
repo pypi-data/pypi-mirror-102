9acdce9815479b787c0c9c470432c75430e883ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:48 +0000

    Add package PyImageOp/0.1.021b/PyImageOp-0.1.021b.tar.gz

---
+++ packages/PyImageOp/PyImageOp-0.1.021b.tar.gz/PyImageOp-0.1.021b/PyImageOp/__init__.py
@@ -0,0 +1,4 @@
+from PyImageOp.Capturing import Capture
+from PyImageOp.Filters import Filters
+from PyImageOp.HSV import HsvFilter
+from PyImageOp.LiveSettings import Settings
---
+++ packages/PyImageOp/PyImageOp-0.1.021b.tar.gz/PyImageOp-0.1.021b/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='PyImageOp',
+    packages=['PyImageOp'],
+    version='0.1.021b',
+    license='MIT',
+    description='Small library to help in live editing image with python, for now beta version',
+    author='allahaka',
+    author_email='allahaka90@gmail.com',
+    url='https://github.com/allahaka/PyImageOp',
+    download_url='https://github.com/allahaka/PyImageOp/archive/refs/tags/0.1.01b.tar.gz',
+    keywords=['Image', 'AI', 'ImageCapture', 'Image Filters', 'Image Recognition'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'scipy',
+        'opencv_python',
+        'numpy',
+        'pillow',
+        'mss',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
