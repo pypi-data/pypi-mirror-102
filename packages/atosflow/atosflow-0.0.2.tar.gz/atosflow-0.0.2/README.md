aa86a144081433397c77faab7ec5169c238fef60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:44 +0000

    Add package atosflow/0.0.2/atosflow-0.0.2.tar.gz

---
+++ packages/atosflow/atosflow-0.0.2.tar.gz/atosflow-0.0.2/README.md
@@ -0,0 +1,1 @@
+# BDS_MlOps_Pipeline
---
+++ packages/atosflow/atosflow-0.0.2.tar.gz/atosflow-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(
+	name="atosflow",
+	version="0.0.2",
+	packages=find_packages(), # permet de récupérer tout les fichiers 
+	description="mlops",
+	url="https://atos.net/en/",
+	author="atos",
+	license="WTFPL",
+	python_requires=">=2.7"
+	)
