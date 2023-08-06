0ef68df5580f2be06cd31c6bfaea82d77043b211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:16 +0000

    Add package hudman/3.1.0/hudman-3.1.0-py3-none-any.whl

---
+++ packages/hudman/hudman-3.1.0-py3-none-any.whl/hudman/app/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2016-2021 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
---
+++ packages/hudman/hudman-3.1.0-py3-none-any.whl/hudman/app/run.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2016-2021 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import argparse
+
+from hudman import HUDManager
+
+
+def main():
+    """
+    The main entry point of the application.
+    """
+    # Parse command-line arguments...
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--huddb', '-d', help='Specify path to HUDs database file.', required=True)
+    parser.add_argument('--outdir', '-o', help='Specify path to save downloaded files.', required=True)
+    parser.add_argument('--save', '-s', help='Automatically save changes in HUD database file.', action='store_true',
+                        required=False)
+    cmdline = parser.parse_args()
+
+    # Run mirror script...
+    hm = HUDManager(cmdline.huddb, cmdline.outdir)
+    hm.getall()
+
+    # Saving changes if enabled...
+    if cmdline.save:
+        hm.save()
+
+
+if __name__ == '__main__':
+    main()
