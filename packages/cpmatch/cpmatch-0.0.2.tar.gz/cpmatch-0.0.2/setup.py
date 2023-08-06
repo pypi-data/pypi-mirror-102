1227f92a9b9eb49b60fa9f3df959ce07d2eb4207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:41 +0000

    Add package cpmatch/0.0.2/cpmatch-0.0.2.tar.gz

---
+++ packages/cpmatch/cpmatch-0.0.2.tar.gz/cpmatch-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cpmatch",
+    version="0.0.2",
+    author="yangmiao",
+    author_email="1446174581@qq.com",
+    description="Python wrapper for kailp: natural language processing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ]
+
+)
