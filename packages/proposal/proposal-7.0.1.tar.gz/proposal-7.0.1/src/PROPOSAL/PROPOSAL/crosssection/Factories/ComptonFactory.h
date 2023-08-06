9b3033adcc66f6146e58339b29628ef0e87025bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:10 +0000

    Add package proposal/7.0.1/proposal-7.0.1.tar.gz

---
+++ packages/proposal/proposal-7.0.1.tar.gz/proposal-7.0.1/src/PROPOSAL/PROPOSAL/crosssection/Factories/ComptonFactory.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+#include <nlohmann/json.hpp> // TODO: Use json_fwd.hpp as soon as https://github.com/conan-io/conan-center-index/pull/5149 is merged
+
+namespace PROPOSAL {
+    struct CrossSectionBase;
+    struct ParticleDef;
+    class Medium;
+    class EnergyCutSettings;
+
+    using cross_ptr = std::unique_ptr<CrossSectionBase>;
+}
+
+namespace PROPOSAL {
+    cross_ptr make_compton(const ParticleDef&, const Medium&,
+                           std::shared_ptr<const EnergyCutSettings>, bool,
+                           const nlohmann::json&);
+}
---
+++ packages/proposal/proposal-7.0.1.tar.gz/proposal-7.0.1/src/PROPOSAL/PROPOSAL/crosssection/Factories/PhotoPairProductionFactory.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <memory>
+#include <nlohmann/json.hpp> // TODO: Use json_fwd.hpp as soon as https://github.com/conan-io/conan-center-index/pull/5149 is merged
+
+namespace PROPOSAL {
+    struct CrossSectionBase;
+    struct ParticleDef;
+    class Medium;
+
+    using cross_ptr = std::unique_ptr<CrossSectionBase>;
+}
+
+namespace PROPOSAL {
+    cross_ptr make_photopairproduction(const ParticleDef&, const Medium&, bool,
+                                       const nlohmann::json&);
+}
