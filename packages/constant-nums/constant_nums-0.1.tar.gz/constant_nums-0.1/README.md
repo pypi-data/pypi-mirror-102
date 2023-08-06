678159fabfef134924fe4bae63939ae1f74991b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:01 +0000

    Add package constant-nums/0.1/constant_nums-0.1.tar.gz

---
+++ packages/constant-nums/constant_nums-0.1.tar.gz/constant_nums-0.1/README.md
@@ -0,0 +1,9 @@
+### Constant
+##### How to use!
+from constant import final
+<constant_name> = final.cons(<any_datatype>,<constant_name>)
+
+To make constant you have to give same constant name on both the sides.
+example : 
+    pi = final.cons(3.14,"pi")
+so, that it will work properly
---
+++ packages/constant-nums/constant_nums-0.1.tar.gz/constant_nums-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="constant_nums"
+,version="0.1"
+,description="A final function for your constant variables"
+,long_description = "You can used it as final variable, just add value and name of constant any"
+,author = "mejoshi"
+,packages = ['constant_nums'],
+install_requires = [])
