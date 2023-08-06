e10d46606c11e62eb0f5cad074d5b33d1214db96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:05 +0000

    Add package python-minimize/0.0.1/python-minimize-0.0.1.tar.gz

---
+++ packages/python-minimize/python-minimize-0.0.1.tar.gz/python-minimize-0.0.1/CHANGELOG.md
@@ -0,0 +1,6 @@
+0.0.1 (2021-04-13)
+------------------
+
+### :notebook: Notes
+
+- Initial release of minimize
---
+++ packages/python-minimize/python-minimize-0.0.1.tar.gz/python-minimize-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+version = "0.0.1"
+here = Path(__file__).parent.resolve()
+
+with open(here.joinpath("README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(here.joinpath("requirements.txt"), encoding="utf-8") as f:
+    requirements = f.readlines()
+
+setup(
+    name="python-minimize",
+    version=version,
+    description="Reduce the size of MicroPython projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TSNobleSoftware/minimize",
+    author="Tom Noble",
+    author_email="t.s.noble@outlook.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: MicroPython",
+    ],
+    keywords=["python", "micropython", "cli"],
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[requirements],
+    project_urls={
+        "Bug Reports": "https://github.com/TSNobleSoftware/minimize/issues",
+        "Source": "https://github.com/TSNobleSoftware/minimize",
+    }
+)
