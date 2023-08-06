6dc61986be6dcecec0b2580d915a8bf79b8c5f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:16 +0000

    Add package aiida-pseudo/0.6.0/aiida-pseudo-0.6.0.tar.gz

---
+++ packages/aiida-pseudo/aiida-pseudo-0.6.0.tar.gz/aiida-pseudo-0.6.0/aiida_pseudo/cli/params/options.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""Reusable options for CLI commands."""
+import shutil
+
+import click
+
+from aiida.cmdline.params.options import OverridableOption
+from .types import PseudoPotentialFamilyTypeParam
+
+__all__ = (
+    'VERSION', 'FUNCTIONAL', 'RELATIVISTIC', 'PROTOCOL', 'PSEUDO_FORMAT', 'STRINGENCY', 'DEFAULT_STRINGENCY',
+    'TRACEBACK', 'FAMILY_TYPE', 'ARCHIVE_FORMAT', 'UNIT', 'DOWNLOAD_ONLY'
+)
+
+VERSION = OverridableOption(
+    '-v', '--version', type=click.STRING, required=False, help='Select the version of the installed configuration.'
+)
+
+FUNCTIONAL = OverridableOption(
+    '-x',
+    '--functional',
+    type=click.STRING,
+    required=False,
+    help='Select the functional of the installed configuration.'
+)
+
+RELATIVISTIC = OverridableOption(
+    '-r',
+    '--relativistic',
+    type=click.STRING,
+    required=False,
+    help='Select the type of relativistic effects included in the installed configuration.'
+)
+
+PROTOCOL = OverridableOption(
+    '-p', '--protocol', type=click.STRING, required=False, help='Select the protocol of the installed configuration.'
+)
+
+PSEUDO_FORMAT = OverridableOption(
+    '-f',
+    '--pseudo-format',
+    type=click.STRING,
+    required=True,
+    help='Select the pseudopotential file format of the installed configuration.'
+)
+
+STRINGENCY = OverridableOption(
+    '-s', '--stringency', type=click.STRING, required=False, help='Stringency level for the recommended cutoffs.'
+)
+
+DEFAULT_STRINGENCY = OverridableOption(
+    '-s',
+    '--default-stringency',
+    type=click.STRING,
+    required=False,
+    help='Select the default stringency level for the installed configuration.'
+)
+
+TRACEBACK = OverridableOption(
+    '-t', '--traceback', is_flag=True, help='Include the stacktrace if an exception is encountered.'
+)
+
+FAMILY_TYPE = OverridableOption(
+    '-T',
+    '--family-type',
+    type=PseudoPotentialFamilyTypeParam(),
+    default='pseudo.family',
+    show_default=True,
+    help='Choose the type of pseudo potential family to create.'
+)
+
+ARCHIVE_FORMAT = OverridableOption(
+    '-F', '--archive-format', type=click.Choice([fmt[0] for fmt in shutil.get_archive_formats()])
+)
+
+UNIT = OverridableOption(
+    '-u',
+    '--unit',
+    type=click.STRING,
+    required=False,
+    default='eV',
+    show_default=True,
+    help='Specify the energy unit of the cutoffs. Must be recognized by the ``UnitRegistry`` of the ``pint`` library.'
+)
+
+DOWNLOAD_ONLY = OverridableOption(
+    '--download-only',
+    is_flag=True,
+    help=(
+        'Only download the pseudopotential files to the current working directory, without installing the '
+        'pseudopotential family.'
+    )
+)
