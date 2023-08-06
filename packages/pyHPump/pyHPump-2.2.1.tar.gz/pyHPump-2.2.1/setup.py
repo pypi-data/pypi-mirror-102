11931f07718839a9b4482a67b641de5cc2aaa5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:58 +0000

    Add package pyHPump/2.2.1/pyHPump-2.2.1.tar.gz

---
+++ packages/pyHPump/pyHPump-2.2.1.tar.gz/pyHPump-2.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='pyHPump',
+  version='2.2.1',
+  description='basic pump application',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Camil Milos',
+  author_email='mcamil16@yahoo.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='psd',
+  packages=find_packages(),
+  install_requires=['pyserial']
+)
