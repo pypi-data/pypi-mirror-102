5382516749c5e357194a5dcade863d42133cbcd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:00 +0000

    Add package Isconna/1.0.1/Isconna-1.0.1.tar.gz

---
+++ packages/Isconna/Isconna-1.0.1.tar.gz/Isconna-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("VERSION") as file:  # Created in CI
+	version = file.readline()
+
+setup(
+	name="Isconna",
+	version=version,
+	author="Rui LIU",
+	author_email="xxliuruiabc@gmail.com",
+	description="Python porting of the Isconna algorithm",
+	long_description="Pypi does not support an AsciiDoc README, please visit our GitHub repo https://github.com/liurui39660/Isconna.Python.",
+	long_description_content_type="text/markdown",
+	url="https://github.com/liurui39660/Isconna.Python",
+	project_urls={},
+	classifiers=[
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: Apache Software License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3"
+	],
+	package_dir={"": "src"},
+	packages=["Isconna"],
+	install_requires=["numba", "numpy"],
+	python_requires=">=3.6",
+)
