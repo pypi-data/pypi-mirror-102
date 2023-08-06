2946be1d6ad7e3a9575a876639af91e834f59051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:19 +0000

    Add package sharestats-item-editor/0.1.12/sharestats-item-editor-0.1.12.tar.gz

---
+++ packages/sharestats-item-editor/sharestats-item-editor-0.1.12.tar.gz/sharestats-item-editor-0.1.12/sharestats_item_editor/consts.py
@@ -0,0 +1,25 @@
+APPNAME = "ShareStats Item Editor"
+
+UNKNOWN_TYPE = "unknown"
+EXTYPES = {"schoice": "Multiple choice",
+           "mchoice": "Multiple answer",
+           "num": "Fill in the blank numbers",
+           "string": "Fill in the blank text/essay",
+           "cloze": "Combinations"}
+
+HAVE_ANSWER_LIST = ["schoice", "mchoice", "cloze"]
+
+TAG_NL = "-nl"
+TAG_ENG = "-en"
+TAG_BILINGUAL = "-[nl/en]"
+
+COLOR_THEME = 'SystemDefault1'
+COLOR_BKG_INACTIVE = "#8A8A8A"
+COLOR_BKG_ACTIVE = "#FFFFFF"
+COLOR_BKG_TAX_LIST = "#E0E0D0"
+COLOR_QUEST = "#BBBBDD"
+COLOR_SOLUTION = "#BBDDBB"
+COLOR_META_INFO = "#DDBBBB"
+COLOR_RED_BTN = ("black", COLOR_META_INFO)
+
+MAX_RECENT_DIRS = 5
