b9725ef35c07d27e125c6e0ef71b604134547651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:29 +0000

    Add package SimilarityText/0.1.5/SimilarityText-0.1.5.tar.gz

---
+++ packages/SimilarityText/SimilarityText-0.1.5.tar.gz/SimilarityText-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+def installs(file):
+    return [req.strip() for req in open(file).readlines()]
+
+
+setup(
+    name="SimilarityText",
+    version="0.1.5",
+    author= "Fabio Alberti",
+    author_email = "fabiocax@gmail.com",
+    description="Find the similarity between two texts using AI",
+    #long_description = file: README.m,
+    #long_description_content_type = text/markdown,
+    url = "https://github.com/fabiocax/SimilarityText",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=installs("requeriments.txt"),
+
+)
