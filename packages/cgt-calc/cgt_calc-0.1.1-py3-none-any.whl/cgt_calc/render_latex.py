288af576e53307e564b62a81a6ae0b03c4e1afa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:53 +0000

    Add package cgt-calc/0.1.1/cgt_calc-0.1.1-py3-none-any.whl

---
+++ packages/cgt-calc/cgt_calc-0.1.1-py3-none-any.whl/cgt_calc/render_latex.py
@@ -0,0 +1,58 @@
+from decimal import Decimal
+import os
+import subprocess
+import tempfile
+
+import jinja2
+
+from .const import PACKAGE_NAME, TEMPLATE_NAME
+from .model import CalculationLog
+from .util import round_decimal
+
+
+def render_calculations(
+    calculation_log: CalculationLog, tax_year: int, date_from_index, output_file: str
+) -> None:
+    print("Generate calculations report")
+    latex_template_env = jinja2.Environment(
+        block_start_string="\\BLOCK{",
+        block_end_string="}",
+        variable_start_string="\\VAR{",
+        variable_end_string="}",
+        comment_start_string="\\#{",
+        comment_end_string="}",
+        line_statement_prefix="%%",
+        line_comment_prefix="%#",
+        trim_blocks=True,
+        autoescape=False,
+        loader=jinja2.PackageLoader(PACKAGE_NAME, "resources"),
+    )
+    template = latex_template_env.get_template(TEMPLATE_NAME)
+    output_text = template.render(
+        calculation_log=calculation_log,
+        tax_year=tax_year,
+        date_from_index=date_from_index,
+        round_decimal=round_decimal,
+        Decimal=Decimal,
+    )
+    generated_file_fd, generated_file = tempfile.mkstemp(suffix=".tex")
+    os.write(generated_file_fd, output_text.encode())
+    os.close(generated_file_fd)
+
+    current_directory = os.path.abspath(".")
+    output_filename = "calculations"
+    subprocess.run(
+        [
+            "pdflatex",
+            f"-output-directory={current_directory}",
+            f"-jobname={output_filename}",
+            "-interaction=batchmode",
+            generated_file,
+        ],
+        check=True,
+        stdout=subprocess.DEVNULL,
+    )
+    os.remove(generated_file)
+    os.remove(f"{output_filename}.log")
+    os.remove(f"{output_filename}.aux")
+    os.rename(output_filename + ".pdf", output_file)
---
+++ packages/cgt-calc/cgt_calc-0.1.1-py3-none-any.whl/cgt_calc/util.py
@@ -0,0 +1,8 @@
+import decimal
+from decimal import Decimal
+
+
+def round_decimal(value: Decimal, digits: int = 0) -> Decimal:
+    with decimal.localcontext() as ctx:
+        ctx.rounding = decimal.ROUND_HALF_UP
+        return Decimal(round(value, digits))
