6d20619fa9e338407811042aa1e9c51d19d7cac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:24 +0000

    Add package napari-stpt/0.0.8.10/napari-stpt-0.0.8.10.tar.gz

---
+++ packages/napari-stpt/napari-stpt-0.0.8.10.tar.gz/napari-stpt-0.0.8.10/napari_stpt/__main__.py
@@ -0,0 +1,19 @@
+"""
+napari-STPT reads zarr files and displays them
+"""
+import sys
+from napari_stpt.napari_stpt import NapariSTPT
+#from napari_stpt import NapariSTPT
+
+#print('Test entry point napari1')
+#if __name__ == "__main__":
+#    print('Test entry point napari2')
+#napari_stpt.NapariSTPT().main(sys.argv[1:])
+
+def main():
+    print('inside main')
+    nap = NapariSTPT()
+    nap.main()
+
+if __name__ == "__main__":
+    main()
