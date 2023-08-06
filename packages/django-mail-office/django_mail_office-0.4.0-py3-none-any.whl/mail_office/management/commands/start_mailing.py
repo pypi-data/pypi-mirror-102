e1fee4a64dfb590d2081c610919523f3341993a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:16 +0000

    Add package django-mail-office/0.4.0/django_mail_office-0.4.0-py3-none-any.whl

---
+++ packages/django-mail-office/django_mail_office-0.4.0-py3-none-any.whl/mail_office/management/commands/start_mailing.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from mail_office.mail_office import MailOffice
+from mail_office.models import EmailMessage
+from mail_office.settings import SETTINGS
+
+
+class Command(BaseCommand):
+    def handle(self) -> None:
+        postmen_count = SETTINGS.pop("POSTMEN_COUNT")
+
+        mail_office = MailOffice(
+            **{
+                setting_name.lower(): setting_value
+                for setting_name, setting_value in SETTINGS.items()
+            }
+        )
+
+        EmailMessage.objects.reset_messages()
+        self.stdout.write("Starting mailing.")
+        try:
+            mail_office.start_mailing(postmen_count)
+        except (KeyboardInterrupt, SystemExit):
+            self.stdout.write(
+                "Received KeyboardInterrupt\SystemExit."
+                "Completion of mailing."
+            )
+            mail_office.stop_postmen()
