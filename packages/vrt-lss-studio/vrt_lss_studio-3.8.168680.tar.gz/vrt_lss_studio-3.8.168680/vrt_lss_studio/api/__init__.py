ea67c5a24dd2c0f0cf4f3d22c8123d60309b464a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:45 +0000

    Add package vrt-lss-studio/3.8.168680/vrt_lss_studio-3.8.168680.tar.gz

---
+++ packages/vrt-lss-studio/vrt_lss_studio-3.8.168680.tar.gz/vrt_lss_studio-3.8.168680/vrt_lss_studio/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from vrt_lss_studio.api.reports_api import ReportsApi
+from vrt_lss_studio.api.statistics_api import StatisticsApi
+from vrt_lss_studio.api.system_api import SystemApi
