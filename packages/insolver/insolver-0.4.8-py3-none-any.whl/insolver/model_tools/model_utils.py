e758ae50d723729abb85cb2ffe60b31c7f02f8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:09 +0000

    Add package insolver/0.4.8/insolver-0.4.8-py3-none-any.whl

---
+++ packages/insolver/insolver-0.4.8-py3-none-any.whl/insolver/model_tools/model_utils.py
@@ -0,0 +1,211 @@
+import os
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+from numpy import log, sum, maximum, unique, true_divide, linspace, ndarray
+
+import matplotlib.pyplot as plt
+
+from pandas import DataFrame, Series, concat, qcut
+from sklearn.model_selection import train_test_split
+
+
+def download_dataset(name, folder='datasets'):
+    """Function for downloading and unzipping example datasets
+
+    Args:
+        name (str): Dataset name. Available datasets are freMPL-R, US_Accidents and Lending_Club
+        folder (str): Path to the folder to dataset saving
+
+    Returns:
+        str: Information about saved dataset
+
+    """
+    datasets = {
+        'freMPL-R': 'https://github.com/MindSetLib/Insolver/releases/download/v0.4.4/freMPL-R.zip',
+        'US_Accidents': 'https://github.com/MindSetLib/Insolver/releases/download/v0.4.4/US_Accidents_June20.zip',
+        'US_Accidents_small': 'https://github.com/MindSetLib/Insolver/releases/download/v0.4.5/US_Accidents_small.zip',
+        'Lending_Club': 'https://github.com/MindSetLib/Insolver/releases/download/v0.4.4/LendingClub.zip'
+    }
+    if name not in datasets.keys():
+        return f'Dataset {name} is not found. Available datasets are {", ".join(datasets.keys())}'
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    url = datasets[name]
+    with urlopen(url) as file:
+        with ZipFile(BytesIO(file.read())) as zfile:
+            zfile.extractall(folder)
+
+    return f'Dataset {name} saved to "{folder}" folder'
+
+
+def train_val_test_split(*arrays, val_size, test_size, random_state=0, shuffle=True, stratify=None):
+    """Function for splitting dataset into train/validation/test partitions.
+
+    Args:
+        *arrays (array_like): Arrays to split into train/validation/test sets containing predictors.
+        val_size (float): The proportion of the dataset to include in validation partition.
+        test_size (float): The proportion of the dataset to include in test partition.
+        random_state (:obj:`int`, optional): Random state, passed to train_test_split() from scikit-learn. (default=0).
+        shuffle (:obj:`bool`, optional): Passed to train_test_split() from scikit-learn. (default=True).
+        stratify (:obj:`array_like`, optional): Passed to train_test_split() from scikit-learn. (default=None).
+
+    Returns:
+        tuple: (x_train, x_valid, x_test, y_train, y_valid, y_test).
+
+        A tuple of partitions of the initial dataset.
+    """
+    n_arrays = len(arrays)
+    split1 = train_test_split(*arrays, random_state=random_state, shuffle=shuffle,
+                              test_size=test_size, stratify=stratify)
+    if n_arrays > 1:
+        train, test = split1[0::2], split1[1::2]
+        if val_size != 0:
+            split2 = train_test_split(*train, random_state=random_state, shuffle=shuffle,
+                                      test_size=val_size / (1 - test_size), stratify=stratify)
+            train, valid = split2[0::2], split2[1::2]
+            return *train, *valid, *test
+        else:
+            return train, test
+    else:
+        train, test = split1[0], split1[1]
+        if val_size != 0:
+            split2 = train_test_split(train, random_state=random_state, shuffle=shuffle,
+                                      test_size=val_size / (1 - test_size), stratify=stratify)
+            train, valid = split2[0], split2[1]
+            return train, valid, test
+        else:
+            return train, test
+
+
+def train_test_column_split(x, y, df_column):
+    """Function for splitting dataset into train/test partitions w.r.t. a column (pd.Series).
+
+    Args:
+        x (pd.DataFrame): DataFrame containing predictors.
+        y (pd.DataFrame): DataFrame containing target variable.
+        df_column (pd.Series): Series for train/test split, assuming it is contained in x.
+
+    Returns:
+        tuple: (x_train, x_test, y_train, y_test).
+
+        A tuple of partitions of the initial dataset.
+    """
+    x1, y1, col_name = x.copy(), y.copy(), df_column.name
+    y1[col_name] = df_column
+    return (x1[x1[col_name] == 'train'].drop(col_name, axis=1), x1[x1[col_name] == 'test'].drop(col_name, axis=1),
+            y1[y1[col_name] == 'train'].drop(col_name, axis=1), y1[y1[col_name] == 'test'].drop(col_name, axis=1))
+
+
+def deviance_poisson(y_hat, y, weight=None):
+    """Function for Poisson Deviance evaluation.
+
+    Args:
+        y_hat: Array with predictions.
+        y: Array with target variable.
+        weight: Weights for weighted metric.
+
+    Returns:
+        float, value of the Poisson deviance.
+    """
+    t_hat, t = y_hat + 1, y + 1
+    if weight:
+        return sum(2 * weight * (t * log(t / t_hat) - (t - t_hat)))
+    else:
+        return sum(2 * (t * log(t / t_hat) - (t - t_hat)))
+
+
+def deviance_gamma(y_hat, y, weight=None):
+    """Function for Gamma Deviance evaluation.
+
+    Args:
+        y_hat: Array with predictions.
+        y: Array with target variable.
+        weight: Weights for weighted metric.
+
+    Returns:
+        float, value of the Gamma deviance.
+    """
+    if weight:
+        return sum(2 * weight * (-log(y/y_hat) + (y-y_hat)/y_hat))
+    else:
+        return sum(2 * (-log(y/y_hat) + (y-y_hat)/y_hat))
+
+
+def inforamtion_value_woe(data, target, bins=10, cat_thresh=10, detail=False):
+    """Function for Information value and Weight of Evidence computation.
+
+    Args:
+        data (pd.DataFrame): DataFrame with data to compute IV and WoE.
+        target (:obj:`str` or :obj:`pd.Series`): Target variable to compute IV and WoE.
+        bins (:obj:`int`, optional): Number of bins for WoE calculation for continuous variables.
+        cat_thresh (:obj:`int`, optional): Maximum number of categories for non-binned WoE calculation.
+        detail (:obj:`bool`, optional):  Whether to return detailed results DataFrame or not. Short by default.
+
+    Returns:
+        pd.DataFrame, DataFrame containing the data on Information Value (depends on detail argument).
+    """
+    detailed_result, short_result = DataFrame(), DataFrame()
+    target = target.name if isinstance(target, Series) else target
+    cols = data.columns
+    for ivars in cols[~cols.isin([target])]:
+        if (data[ivars].dtype.kind in 'bifc') and (len(unique(data[ivars])) > cat_thresh):
+            binned_x = qcut(data[ivars], bins,  duplicates='drop')
+            d0 = DataFrame({'x': binned_x, 'y': data[target]})
+        else:
+            d0 = DataFrame({'x': data[ivars], 'y': data[target]})
+        d = d0.groupby("x", as_index=False).agg({"y": ["count", "sum"]})
+        d.columns = ['Cutoff', 'N', 'Events']
+        d['% of Events'] = maximum(d['Events'], 0.5) / d['Events'].sum()
+        d['Non-Events'] = d['N'] - d['Events']
+        d['% of Non-Events'] = maximum(d['Non-Events'], 0.5) / d['Non-Events'].sum()
+        d['WoE'] = log(d['% of Events'] / d['% of Non-Events'])
+        d['IV'] = d['WoE'] * (d['% of Events'] - d['% of Non-Events'])
+        d.insert(loc=0, column='Variable', value=ivars)
+        temp = DataFrame({"Variable": [ivars], "IV": [d['IV'].sum()]}, columns=["Variable", "IV"])
+        detailed_result = concat([detailed_result, temp], axis=0)
+        short_result = concat([short_result, d], axis=0)
+    return short_result if detail else detailed_result
+
+
+def gain_curve(predict, exposure):
+    if isinstance(predict, (Series, ndarray)) and isinstance(exposure, Series):
+        temp_df = concat([Series(predict, name='Predict').reset_index(drop=True),
+                          exposure.reset_index(drop=True)], axis=1)
+        temp_df = temp_df.sort_values('Predict', ascending=False).reset_index(drop=True)
+        normalized_df = temp_df.cumsum()/temp_df.sum()
+        w = sum(temp_df[exposure.name])
+        m = true_divide(sum(temp_df[exposure.name] * temp_df['Predict']), sum(temp_df[exposure.name]))
+        temp_df['Rank'] = 0
+        temp_df.loc[0, 'Rank'] = 1 + 0.5 * (temp_df.loc[0, exposure.name] - 1)
+        for x in range(1, len(temp_df)):
+            temp_df.loc[x, 'Rank'] = (temp_df.loc[x-1, 'Rank'] + 0.5 * (temp_df.loc[x-1, exposure.name] + 1)
+                                      + 0.5 * (temp_df.loc[x, exposure.name] - 1))
+        gini = 1 + 1/w - 2/(w**2 * m) * sum(temp_df[exposure.name] * temp_df['Predict'] * temp_df['Rank'])
+        plt.plot(normalized_df[exposure.name], normalized_df['Predict'], label=f'Predict (Gini: {round(gini, 3)})')
+    elif isinstance(predict, DataFrame) and isinstance(exposure, Series):
+        temp_df = concat([predict.reset_index(drop=True), exposure.reset_index(drop=True)], axis=1)
+        for pred_col in temp_df.columns[:-1]:
+            temp_df2 = temp_df[[pred_col, exposure.name]].sort_values(pred_col, ascending=False).reset_index(drop=True)
+            normalized_df = temp_df2.cumsum()/temp_df2.sum()
+            w = sum(temp_df2[exposure.name])
+            m = true_divide(sum(temp_df2[exposure.name] * temp_df2[pred_col]), sum(temp_df2[exposure.name]))
+            temp_df2['Rank'] = 0
+            temp_df2.loc[0, 'Rank'] = 1 + 0.5 * (temp_df2.loc[0, exposure.name] - 1)
+            for x in range(1, len(temp_df2)):
+                temp_df2.loc[x, 'Rank'] = (temp_df2.loc[x-1, 'Rank'] + 0.5 * (temp_df2.loc[x-1, exposure.name] + 1)
+                                           + 0.5 * (temp_df2.loc[x, exposure.name] - 1))
+            gini = 1 + 1/w - 2/(w**2 * m) * sum(temp_df2[exposure.name] * temp_df2[pred_col] * temp_df2['Rank'])
+            plt.plot(normalized_df[exposure.name], normalized_df[pred_col],
+                     label=f'{pred_col} (Gini: {round(gini, 3)})')
+    else:
+        raise Exception
+    plt.legend()
+    plt.plot(linspace(0, 1, 2), linspace(0, 1, 2), c='red', linestyle='--', linewidth=0.7)
+    plt.title('Gains curve')
+    plt.xlabel('Cumulative exposure')
+    plt.ylabel('Cumulative response')
+    plt.show()
