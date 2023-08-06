e3473bdb13f254479dc7f31d7858ef3b936c262c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:25 +0000

    Add package amogus/0.1/amogus-0.1.tar.gz

---
+++ packages/amogus/amogus-0.1.tar.gz/amogus-0.1/setup.py
@@ -0,0 +1,15 @@
+﻿from setuptools import setup
+
+with open('README.md', 'r') as f:
+	DESCRIPTION = f.read()
+
+setup(name='amogus',
+      version='0.1',
+      description='AMOGUS',
+      long_description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      url='http://github.com/dmitrijkotov634/amogus',
+      author='Dmitry Kotov',
+      author_email='dmitrijkotov634@gmail.com',
+      license='MIT',
+      packages=['amogus'])
