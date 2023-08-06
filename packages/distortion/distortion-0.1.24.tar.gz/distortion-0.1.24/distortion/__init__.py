b7d91be6e13c057938123e7e50971272621e27cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:13 +0000

    Add package distortion/0.1.24/distortion-0.1.24.tar.gz

---
+++ packages/distortion/distortion-0.1.24.tar.gz/distortion-0.1.24/distortion/__init__.py
@@ -0,0 +1,14 @@
+from distortion.distortion import (
+    init,
+    Distortioner,
+    Identity,
+    Dropout,
+    Cropout,
+    Crop,
+    Resize,
+    GaussianBlur,
+    JPEGBase,
+    JPEGCompression,
+    JPEGMask,
+    JPEGDrop,
+)
