57e6912a295fe862f576127b55eb857bf7d5eb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:28 +0000

    Add package spreadsnake/1.0a1/spreadsnake-1.0a1.tar.gz

---
+++ packages/spreadsnake/spreadsnake-1.0a1.tar.gz/spreadsnake-1.0a1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'spreadsnake',
+    packages = ['spreadsnake'],
+    version = '1.0a1',
+    license = 'MIT',
+    description = 'A python spreadsheet api',
+    author = 'RandomKiddo',
+    author_email = 'nghugare2@outlook.com',
+    url = 'https://github.com/RandomKiddo/spreadsnake',
+    download_url = 'https://github.com/RandomKiddo/spreadsnake/archive/1.0a1.tar.gz',
+    keywords = ['PYTHON', 'SPREADSHEETS', 'API'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',     
+        'Programming Language :: Python :: 3.8',
+    ]
+)
