7d737deb9134a08f0151e3c395330705c42213dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:10 +0000

    Add package pyrogis/0.3.0/pyrogis-0.3.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/pyrogis/pyrogis-0.3.0-cp39-cp39-manylinux2014_x86_64.whl/pyrogis/__init__.py
@@ -0,0 +1,11 @@
+"""
+image and animation processing framework
+
+pyrogis.ingredients -> Ingredient types
+
+pyrogis.kitchen -> cooking "runtime"
+
+pyrogis.restaurant -> feedback/terminal interface handler
+
+pyrogis.course -> animation wrapper
+"""
