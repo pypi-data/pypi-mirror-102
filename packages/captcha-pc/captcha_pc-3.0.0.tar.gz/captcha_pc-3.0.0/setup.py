6d50969239ae4bb918030befba470d8117fea6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:52 +0000

    Add package captcha-pc/3.0.0/captcha_pc-3.0.0.tar.gz

---
+++ packages/captcha-pc/captcha_pc-3.0.0.tar.gz/captcha_pc-3.0.0/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name = 'captcha_pc',
+    version = '3.0.0',
+    author = 'Fabrício Antoniasse',
+    author_email = 'fabricio.antoniasse@hotmail.com',
+    packages = ['captcha_pc'],
+    description = 'Função que faz o download de arquivos através de URLs informadas',
+    license = 'MIT',
+    keywords = 'captcha_pc',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent'
+    ]
+)
