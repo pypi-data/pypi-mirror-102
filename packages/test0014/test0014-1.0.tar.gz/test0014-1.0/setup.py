fa1ff1546153beff6ea9c613f87714d1b63b9bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:18 +0000

    Add package test0014/1.0/test0014-1.0.tar.gz

---
+++ packages/test0014/test0014-1.0.tar.gz/test0014-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test0014',
+    version='1.0',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages()
+)
+
