987305a432c161d99e2cb97d3f632da90d007cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:15 +0000

    Add package Amino.py/1.2.15.2/Amino.py-1.2.15.2-py3-none-any.whl

---
+++ packages/Amino.py/Amino.py-1.2.15.2-py3-none-any.whl/amino/lib/util/helpers.py
@@ -0,0 +1,25 @@
+import json
+import base64
+
+from hashlib import sha1
+from functools import reduce
+import platform
+
+def generate_device_info():
+    try: deviceId = '01' + (hardwareInfo := sha1(platform.processor().encode("utf-8"))).hexdigest() + sha1(bytes.fromhex('01') + hardwareInfo.digest() + base64.b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()
+    except Exception: deviceId = "01534834CD64C0C0519BD11B6B627469C0FD008333EDC0FBCD0285FE9BEA491D45C192BE40D616417A"
+
+    return {
+        "device_id": deviceId,
+        "device_id_sig": "Aa0ZDPOEgjt1EhyVYyZ5FgSZSqJt",
+        "user_agent": "Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-G973N Build/beyond1qlteue-user 5; com.narvii.amino.master/3.4.33562)"
+    }
+
+# okok says: please use return annotations :(( https://www.python.org/dev/peps/pep-3107/#return-values
+
+def decode_sid(sid: str) -> dict:
+    return json.loads(base64.b64decode(reduce(lambda a, e: a.replace(*e), ("-+","_/"), sid + "=" * (-len(sid) % 4)).encode())[1:-20].decode())
+
+def sid_to_uid(SID: str) -> str: return decode_sid(SID)["2"]
+
+def sid_to_ip_address(SID: str) -> str: return decode_sid(SID)["4"]
