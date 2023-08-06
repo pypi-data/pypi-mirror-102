e5cd48b3c972a819f65fbe2c41d11b37aab71992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:43 +0000

    Add package streamlit-echarts/0.3.0/streamlit-echarts-0.3.0.tar.gz

---
+++ packages/streamlit-echarts/streamlit-echarts-0.3.0.tar.gz/streamlit-echarts-0.3.0/streamlit_echarts/frontend/build/precache-manifest.43ac149f7aa36125ebc93e74224abdf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ff8f0e32d9e6f5b9d93fd4c2c39c82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b91dfa16a8132a01683a",
+    "url": "./static/css/main.d984dddf.chunk.css"
+  },
+  {
+    "revision": "884b88bbfa5203f98c41",
+    "url": "./static/js/2.000a4646.chunk.js"
+  },
+  {
+    "revision": "b1827f4ae21eedbcfae928266c233207",
+    "url": "./static/js/2.000a4646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91dfa16a8132a01683a",
+    "url": "./static/js/main.04485873.chunk.js"
+  },
+  {
+    "revision": "177d041b66bb68f23701",
+    "url": "./static/js/runtime-main.34185f1e.js"
+  }
+]);
---
+++ packages/streamlit-echarts/streamlit-echarts-0.3.0.tar.gz/streamlit-echarts-0.3.0/streamlit_echarts/frontend/src/EchartsChart.tsx
@@ -0,0 +1,100 @@
+import React from "react"
+import {
+  ComponentProps,
+  Streamlit,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import { isObject } from "lodash"
+
+import * as echarts from "echarts"
+import "echarts-gl"
+import "echarts-liquidfill"
+import "echarts-wordcloud"
+import ReactEcharts from "echarts-for-react"
+
+import deepMap from "./utils"
+
+interface Map {
+  mapName: string
+  geoJson: object
+  specialAreas: object
+}
+
+/**
+ * Arguments Streamlit receives from the Python side
+ */
+interface PythonArgs {
+  options: object
+  theme: string | object
+  onEvents: object
+  height: string
+  width: string
+  renderer: "canvas" | "svg"
+  map: Map
+}
+
+const EchartsChart = (props: ComponentProps) => {
+  const JS_PLACEHOLDER = "--x_x--0_0--"
+
+  const registerTheme = (themeProp: string | object) => {
+    const customThemeName = "custom_theme"
+    if (isObject(themeProp)) {
+      echarts.registerTheme(customThemeName, themeProp)
+    }
+    return isObject(themeProp) ? customThemeName : themeProp
+  }
+
+  const convertJavascriptCode = (obj: object) => {
+    let funcReg = new RegExp(
+      `${JS_PLACEHOLDER}\\s*(function\\s*.*)\\s*${JS_PLACEHOLDER}`
+    )
+
+    // Look in all nested values of options for Pyecharts Javascript placeholder
+    return deepMap(obj, function (v: string) {
+      let match = funcReg.exec(v)
+      if (match) {
+        const funcStr = match[1]
+        return new Function("return " + funcStr)()
+      } else {
+        return v
+      }
+    })
+  }
+
+  const {
+    options,
+    theme,
+    onEvents,
+    height,
+    width,
+    renderer,
+    map,
+  }: PythonArgs = props.args
+  const cleanTheme = registerTheme(theme)
+
+  if (isObject(map)) {
+    echarts.registerMap(map.mapName, map.geoJson, map.specialAreas)
+  }
+
+  const cleanOptions = convertJavascriptCode(options)
+  const cleanOnEvents = convertJavascriptCode(onEvents)
+
+  return (
+    <>
+      <ReactEcharts
+        option={cleanOptions}
+        notMerge={true}
+        lazyUpdate={true}
+        style={{ height: height, width: width }}
+        theme={cleanTheme}
+        onChartReady={() => {
+          Streamlit.setFrameHeight()
+        }}
+        onEvents={cleanOnEvents}
+        opts={{ renderer: renderer }}
+      />
+    </>
+  )
+}
+
+export default withStreamlitConnection(EchartsChart)
