f63d525b5f83cbd36139bd4cc0e6b9dd6aa54508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:36 +0000

    Add package weibo-spider/0.2.6/weibo-spider-0.2.6.tar.gz

---
+++ packages/weibo-spider/weibo-spider-0.2.6.tar.gz/weibo-spider-0.2.6/weibo_spider/downloader/__init__.py
@@ -0,0 +1,5 @@
+from .origin_picture_downloader import OriginPictureDownloader
+from .retweet_picture_downloader import RetweetPictureDownloader
+from .video_downloader import VideoDownloader
+
+__all__ = [OriginPictureDownloader, RetweetPictureDownloader, VideoDownloader]
