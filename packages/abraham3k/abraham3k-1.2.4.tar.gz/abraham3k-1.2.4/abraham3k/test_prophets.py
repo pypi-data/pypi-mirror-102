056f9108959af43c8195110da90daa08ec72d416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:29 +0000

    Add package abraham3k/1.2.4/abraham3k-1.2.4.tar.gz

---
+++ packages/abraham3k/abraham3k-1.2.4.tar.gz/abraham3k-1.2.4/abraham3k/test_prophets.py
@@ -0,0 +1,22 @@
+from prophets import Isaiah
+import sys
+from pprint import pprint
+
+if __name__ == "__main__":
+    # print(f"Abraham Version: {open('version').read().strip()}")
+    darthvader = Isaiah(
+        news_source="newsapi",
+        api_key="3530e04f04034a85a0ebf4d925c83c69",
+        splitting=False,
+    )  # splitting means that it recursively splits a large text into sentences and analyzes each individually
+
+    args = [sys.argv[1:]] if sys.argv[1:] else ["tesla"]  # default args
+
+    scores = darthvader.sentiment(
+        *args,
+        window=1,  # how many days back from up_to to get news from
+    )  # latest date to get news from
+    pprint(scores)
+    print(scores["tesla"]["title"])
+    print(scores["tesla"]["desc"])
+    print(scores["tesla"]["text"])
