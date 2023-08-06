60b59686eeb4c88b53293da5837e3c349aa717e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:25 +0000

    Add package modern-bencode/1.3.0/modern_bencode-1.3.0-py3-none-any.whl

---
+++ packages/modern-bencode/modern_bencode-1.3.0-py3-none-any.whl/bencode/__init__.py
@@ -0,0 +1,3 @@
+"""We import some functions here, so they are available on the package level"""
+from .bencode import decode, encode  # noqa
+from .torrent import decode_torrent, encode_torrent  # noqa
