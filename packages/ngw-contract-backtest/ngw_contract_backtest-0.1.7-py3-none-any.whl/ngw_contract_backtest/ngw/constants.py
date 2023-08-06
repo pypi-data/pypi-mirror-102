de416a2be569f46c667edd94a7c6f927965a133f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:54 +0000

    Add package ngw-contract-backtest/0.1.7/ngw_contract_backtest-0.1.7-py3-none-any.whl

---
+++ packages/ngw-contract-backtest/ngw_contract_backtest-0.1.7-py3-none-any.whl/ngw_contract_backtest/ngw/constants.py
@@ -0,0 +1,37 @@
+
+
+# celery_post
+
+# host = '127.0.0.1:6001'
+host = 'stq.niuguwang.com'
+
+# -----------------------------------------------------------------------------------------
+# 主要入库
+
+
+# ubuntu
+# MYSQL_HOST = '192.168.157.129'
+# MYSQL_PORT = 3306
+# MYSQL_DATABASE = 'contract_strategy'
+# MYSQL_USER = 'root'
+# MYSQL_PASSWORD = 'mysql'
+
+
+
+
+# 生产 8.104
+MYSQL_HOST = 'stq.mysql.ngw.zw.in'
+MYSQL_PORT = 3306
+MYSQL_DATABASE = 'contract_strategy'
+MYSQL_USER = 'tjlh'
+MYSQL_PASSWORD = 'tjXx^Egw3NdnLK'
+
+
+# -----------------------------------------------------------------------------------------
+LOGFLAG = True
+# LOGFLAG = False
+
+
+
+
+
