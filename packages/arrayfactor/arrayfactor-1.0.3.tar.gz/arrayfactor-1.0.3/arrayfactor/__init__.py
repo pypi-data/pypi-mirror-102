5731d521f54224703212b79e75fec98534e6574e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:33 +0000

    Add package arrayfactor/1.0.3/arrayfactor-1.0.3.tar.gz

---
+++ packages/arrayfactor/arrayfactor-1.0.3.tar.gz/arrayfactor-1.0.3/arrayfactor/__init__.py
@@ -0,0 +1,13 @@
+"""
+arrayfactor package.
+
+@author: Merve Tascioglu
+merve.tascioglu@barkhauseninstitut.org
+
+GitHub: https://github.com/Barkhausen-Institut/AntennaArraySimulator
+
+"""
+from arrayfactor import af
+
+
+__version__ = '1.0.3'
