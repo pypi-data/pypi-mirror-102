d23523c86434a0640efd3109f6cb94f2a4d11a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package cloudx-nac-api/1.3.3/cloudx_nac_api-1.3.3-py3-none-any.whl

---
+++ packages/cloudx-nac-api/cloudx_nac_api-1.3.3-py3-none-any.whl/cloudx_paas_api/database_management.py
@@ -0,0 +1,10 @@
+"""
+This module allows Cloud^X Edge PaaS Database Management API set
+Following functions are exposed:
+
+    create_database         allow easy to create database service
+    destroy_database        allow easy to delete database service   
+    subscribe_to_service    allow to subscribe to service
+    unsubscribe_to_service  allow to unsubscribe to service
+
+"""
