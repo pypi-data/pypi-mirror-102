8111c6cdfccd4aae0ad4483cb076bac68045b2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:24 +0000

    Add package netbox-plugin-software-manager/0.0.2/netbox_plugin_software_manager-0.0.2-py3-none-any.whl

---
+++ packages/netbox-plugin-software-manager/netbox_plugin_software_manager-0.0.2-py3-none-any.whl/software_manager/__init__.py
@@ -0,0 +1,17 @@
+from extras.plugins import PluginConfig
+
+
+class SoftwareManager(PluginConfig):
+    name = "software_manager"
+    verbose_name = "Software Manager"
+    description = "Software Manager for Cisco IOS/IOS-XE devices"
+    version = "0.0.2"
+    author = "Alexander Ignatov"
+    author_email = "ignatov.alx@gmail.com"
+    required_settings = []
+    default_settings = {}
+    base_url = "software-manager"
+    caching_config = {}
+
+
+config = SoftwareManager
