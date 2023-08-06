32d5651bb372b59fc215e5fd9f1fddebdf4a0523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:58 +0000

    Add package nida/0.1.0/nida-0.1.0.tar.gz

---
+++ packages/nida/nida-0.1.0.tar.gz/nida-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="nida",
+    version="0.1.0",
+    description="A python libary to Easy fetching user information based on National ID (Tanzania)",
+    url="https://github.com/Kalebu/nida",
+    download_url="https://github.com/Kalebu/Nida/archive/0.1.tar.gz",
+    author="Jordan Kalebu",
+    author_email="isaackeinstein@gmail.com",
+    license="MIT",
+    packages=["nida"],
+    keywords=[
+        "nida",
+        "nida api"
+        "nida python package"
+        "python nida"
+        "tanzania nida",
+        "python-tanzania",
+    ],
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
