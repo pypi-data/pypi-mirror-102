f4c4571e23a8868fd0ff118474b1205dfb7816cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:05 +0000

    Add package onlyoffice.connector/2.0.0/onlyoffice.connector-2.0.0.tar.gz

---
+++ packages/onlyoffice.connector/onlyoffice.connector-2.0.0.tar.gz/onlyoffice.connector-2.0.0/CHANGELOG.md
@@ -0,0 +1,14 @@
+# Change Log
+
+## 2.0.0
+## Added
+ - JWT support
+## Fixed
+ - Issue when files inside unpublished folders couldn't be edited
+
+## 1.0.0
+## Added
+ - Edit option for DOCX, XLSX, PPTX.
+ - View template for documents
+ - Configuration page
+ - Translations for BR, CZ, DE, EN, ES, FR, IT, JP, NL, RU, ZH
---
+++ packages/onlyoffice.connector/onlyoffice.connector-2.0.0.tar.gz/onlyoffice.connector-2.0.0/src/onlyoffice/connector/core/fileUtils.py
@@ -0,0 +1,54 @@
+#
+# (c) Copyright Ascensio System SIA 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+def getFileName(str):
+    ind = str.rfind('/')
+    return str[ind+1:]
+
+def getFileNameWithoutExt(str):
+    fn = getFileName(str)
+    ind = fn.rfind('.')
+    return fn[:ind]
+
+def getFileExt(str):
+    fn = getFileName(str)
+    ind = fn.rfind('.')
+    return fn[ind:].lower()
+
+def getFileType(str):
+    ext = getFileExt(str)
+    if ext in [ ".doc", ".docx", ".docm", ".dot", ".dotx", ".dotm", ".odt", ".fodt", ".ott", ".rtf", ".txt", ".html", ".htm", ".mht", ".pdf", ".djvu", ".fb2", ".epub", ".xps" ]:
+        return 'text'
+    if ext in [ ".xls", ".xlsx", ".xlsm", ".xlt", ".xltx", ".xltm", ".ods", ".fods", ".ots", ".csv" ]:
+        return 'spreadsheet'
+    if ext in [ ".pps", ".ppsx", ".ppsm", ".ppt", ".pptx", ".pptm", ".pot", ".potx", ".potm", ".odp", ".fodp", ".otp"]:
+        return 'presentation'
+
+    return 'text'
+
+def canView(str):
+    ext = getFileExt(str)
+    if ext in [ ".doc", ".docx", ".docm", ".dot", ".dotx", ".dotm", ".odt", ".fodt", ".ott", ".rtf", ".txt", ".html", ".htm", ".mht", ".pdf", ".djvu", ".fb2", ".epub", ".xps", ".xls", ".xlsx", ".xlsm", ".xlt", ".xltx", ".xltm", ".ods", ".fods", ".ots", ".csv", ".pps", ".ppsx", ".ppsm", ".ppt", ".pptx", ".pptm", ".pot", ".potx", ".potm", ".odp", ".fodp", ".otp" ]:
+        return True
+
+    return False
+
+def canEdit(str):
+    ext = getFileExt(str)
+    if ext in [ ".docx", ".xlsx", ".pptx" ]:
+        return True
+
+    return False
