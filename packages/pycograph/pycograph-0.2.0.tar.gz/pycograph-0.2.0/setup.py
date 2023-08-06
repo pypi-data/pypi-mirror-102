5b671fdfc4d57b5dd5fa73ea9db164ff840fe38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:54 +0000

    Add package pycograph/0.2.0/pycograph-0.2.0.tar.gz

---
+++ packages/pycograph/pycograph-0.2.0.tar.gz/pycograph-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pycograph', 'pycograph.helpers', 'pycograph.schemas']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.8,<2.0', 'redisgraph>=2.3,<3.0', 'typer==0.3.2']
+
+entry_points = \
+{'console_scripts': ['pycograph = pycograph.cli:app']}
+
+setup_kwargs = {
+    'name': 'pycograph',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'reka',
+    'author_email': 'reka@hey.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
