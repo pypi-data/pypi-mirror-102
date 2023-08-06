2f61c858a72b4b1a0854e87425342cb8efc4ecfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:03 +0000

    Add package barcodeSpliter/1.7/barcodeSpliter-1.7.tar.gz

---
+++ packages/barcodeSpliter/barcodeSpliter-1.7.tar.gz/barcodeSpliter-1.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="barcodeSpliter",
+    version="1.7",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    description = '"barcodeSpliter" can use barcode label to \
+        separate different samples in mixed-sample sequencing.',
+    author = "Haopeng Yu",
+    author_email = "atlasbioin4@gmail.com",
+    url = "https://github.com/atlasbioinfo/barcodeSpliter",
+    license='Apache-2.0',
+    entry_points = {
+        'console_scripts' : [
+            'barcodeSpliter = barcodeSpliter:main'
+        ]
+    },
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    package_data = {
+        '':['*.gz','*.tsv']
+    },
+    python_requires='>=3.6',
+)
