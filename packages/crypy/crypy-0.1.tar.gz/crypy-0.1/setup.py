1b2dfb1ce7ea48ec6fdc8435a94b4460f9b527c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:05 +0000

    Add package crypy/0.1/crypy-0.1.tar.gz

---
+++ packages/crypy/crypy-0.1.tar.gz/crypy-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='crypy',
+    version='0.1',
+    packages=['crypy'],
+    license='MIT',
+    url='https://github.com/petitnau/crypy',
+    download_url='https://github.com/petitnau/crypy/archive/refs/tags/v0.1-alpha.tar.gz',
+    author='petitnau, piier, geladen',
+    author_email='roberto.pettinau99@gmail.com, erik.piersante@gmail.com, cuncuemanuele@outlook.it',
+    description='',
+    install_requires=[
+        'sympy',
+        'pycryptodome',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
