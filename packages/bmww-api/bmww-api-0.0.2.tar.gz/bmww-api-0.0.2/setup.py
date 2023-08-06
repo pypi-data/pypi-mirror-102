a3d166e20f2154a9bfd4ab5c8a6ab6032e2db622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:09 +0000

    Add package bmww-api/0.0.2/bmww-api-0.0.2.tar.gz

---
+++ packages/bmww-api/bmww-api-0.0.2.tar.gz/bmww-api-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A basic unofficial batmanwonderwoman.com (bmww) api'
+long_description = "A basic bmww api, check github' readme for usage"
+
+# Setting up
+setup(
+    name="bmww-api",
+    version=VERSION,
+    author="Noche-10",
+    author_email="nocheffic@gmail.com",
+    long_description=long_description,
+    url='https://github.com/Noche-10/bmww-api',
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['beautifulsoup4', 'requests'],
+    keywords=['python', 'api', 'batman', 'wonderwoman', 'bmww', 'batmanwonderwoman'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
