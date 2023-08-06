928ad227f6df3437794b12386cd803f0b8c95035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:58 +0000

    Add package fakechain/0.0.0a1/fakechain-0.0.0a1.tar.gz

---
+++ packages/fakechain/fakechain-0.0.0a1.tar.gz/fakechain-0.0.0a1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    packages=[
+        'fakechain',
+        ],
+    name='fakechain',
+    version='0.0.0a1',
+    description='Planned project for a naive DLT implementation for prototyping and education purposes',
+        )
+
