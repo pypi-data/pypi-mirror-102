ab939983d07fee4d96bff0a0a50d8d1daeee1638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:13 +0000

    Add package rattools/0.4.1/rattools-0.4.1.tar.gz

---
+++ packages/rattools/rattools-0.4.1.tar.gz/rattools-0.4.1/rattools/investing.py
@@ -0,0 +1,19 @@
+
+def get_stock_data(tickers):
+    
+    import finviz
+    import pandas as pd
+
+    sp500 = pd.DataFrame()
+    
+    for ticker in tickers:
+        try:
+        	ticker = ticker.upper()
+            stock = finviz.get_stock(ticker)
+    
+            df = pd.DataFrame.from_dict(stock, orient='index').T
+            df["ticker"] = ticker
+            sp500 = sp500.append(df)
+        except:
+            pass
+    return sp500
