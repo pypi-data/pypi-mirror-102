5b444321c07631bca57c3696d77ba72b0fa9da46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:34 +0000

    Add package redash-tools/1.0.2/redash_tools-1.0.2.tar.gz

---
+++ packages/redash-tools/redash_tools-1.0.2.tar.gz/redash_tools-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='redash_tools',
+      packages=['redash_tools'],
+      version='1.0.2',
+      license='MIT',
+      description='Tools to backup, batch update, template redash queries and dashboards',
+      author='Marina Pavlova',
+      author_email='pavlova.marina.v@gmail.com',
+      url='http://github.com/pavlova-marina/redash-tools',
+      download_url='https://github.com/pavlova-marina/redash-tools/archive/refs/tags/v1.0.2.tar.gz',
+      keywords=['redash'],
+      install_requires=['requests'],
+      python_requires='>=3.6',
+      classifiers=[
+                  'Development Status :: 3 - Alpha',
+                  'Intended Audience :: Developers',
+                  'Topic :: Software Development :: Build Tools',
+                  'License :: OSI Approved :: MIT License',
+                  'Operating System :: OS Independent',
+                  'Programming Language :: Python :: 3',
+                  'Programming Language :: Python :: 3.4',
+                  'Programming Language :: Python :: 3.5',
+                  'Programming Language :: Python :: 3.6',
+              ]
+      )
