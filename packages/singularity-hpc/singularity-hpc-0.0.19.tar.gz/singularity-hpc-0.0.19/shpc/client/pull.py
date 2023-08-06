6bc995334c3ae16fb79ffeffa7cc43961be772b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:05 +0000

    Add package singularity-hpc/0.0.19/singularity-hpc-0.0.19.tar.gz

---
+++ packages/singularity-hpc/singularity-hpc-0.0.19.tar.gz/singularity-hpc-0.0.19/shpc/client/pull.py
@@ -0,0 +1,15 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2021, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+import shpc.main.container as container
+
+
+def main(args, parser, extra):
+
+    # We currently support GitHub and Docker URIs
+    if not re.search("^(gh|docker)", args.uri):
+        logger.exit("unique resource identifier %s is not recognized." % args.uri)
+
+    client = container.SingularityContainer()
+    client.pull(args.uri, args.path)
