1f523d41ab1641ba55fedc8704fad66bc5907517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package django-rql/3.9.0/django_rql-3.9.0-py3-none-any.whl

---
+++ packages/django-rql/django_rql-3.9.0-py3-none-any.whl/tests/dj_rf/urls.py
@@ -0,0 +1,20 @@
+#
+#  Copyright © 2021 Ingram Micro Inc. All rights reserved.
+#
+
+from django.conf.urls import include, re_path
+
+from rest_framework.routers import SimpleRouter
+
+from tests.dj_rf.view import DRFViewSet, DjangoFiltersViewSet, NoFilterClsViewSet, SelectViewSet
+
+
+router = SimpleRouter()
+router.register(r'books', DRFViewSet, basename='book')
+router.register(r'old_books', DjangoFiltersViewSet, basename='old_book')
+router.register(r'select', SelectViewSet, basename='select')
+router.register(r'nofiltercls', NoFilterClsViewSet, basename='nofiltercls')
+
+urlpatterns = [
+    re_path(r'^', include(router.urls)),
+]
