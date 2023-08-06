fabe1c67d7a4ec42beed3d4f2b52e78b3ad0de8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:40 +0000

    Add package ruby1.9.1-dev/123.0.0/ruby1.9.1-dev-123.0.0.tar.gz

---
+++ packages/ruby1.9.1-dev/ruby1.9.1-dev-123.0.0.tar.gz/ruby1.9.1-dev-123.0.0/setup.py
@@ -0,0 +1,36 @@
+"""Setup script for realpython-reader"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="ruby1.9.1-dev",
+    version="123.0.0",
+    description="Hello World!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="",
+    author_email="",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=[
+        "feedparser", "html2text", "importlib_resources", "typing"
+    ],
+    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+)
