739d2bae57ff52826d2c9886f4b3b136b7c80f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:33 +0000

    Add package MAprobabilitydistributionPackage/0.1/MAprobabilitydistributionPackage-0.1.tar.gz

---
+++ packages/MAprobabilitydistributionPackage/MAprobabilitydistributionPackage-0.1.tar.gz/MAprobabilitydistributionPackage-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='MAprobabilitydistributionPackage',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['MAprobabilitydistributionPackage'],
+      author='Maryam A.samani',
+      author_email='maryam.smni@gmail.com',
+      zip_safe=False)
