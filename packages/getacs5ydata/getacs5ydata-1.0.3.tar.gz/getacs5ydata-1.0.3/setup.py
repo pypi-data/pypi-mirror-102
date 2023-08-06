3fcfcf25c3a9b4cbcc91de82a4c8930a2eaa3912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:23 +0000

    Add package getacs5ydata/1.0.3/getacs5ydata-1.0.3.tar.gz

---
+++ packages/getacs5ydata/getacs5ydata-1.0.3.tar.gz/getacs5ydata-1.0.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+PACKAGES = ['getacs5ydata']
+
+setuptools.setup(
+    name="getacs5ydata",
+    version="1.0.3",
+    author="Will Armstrong",
+    author_email="williamqarmstrong@gmail.com",
+    description="Gets ACS 5 Y Data",
+    long_description="Gets ACS 5 Y Data",
+    long_description_content_type="text/markdown",
+    url="https://github.com/will3921/getacs5ydata",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
