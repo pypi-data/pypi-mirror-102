294c7754f198dfe9edf7751809fa0d0b1de1a56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:51 +0000

    Add package stopover/1.214.0/stopover-1.214.0-py3-none-any.whl

---
+++ packages/stopover/stopover-1.214.0-py3-none-any.whl/stopover/utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import msgpack
+import snappy
+import json
+import time
+import random
+import hashlib
+from uuid import uuid4
+
+
+def pack(message: dict) -> bytes:
+    return msgpack.packb(message)
+
+
+def unpack(message: bytes) -> dict:
+    return msgpack.unpackb(message)
+
+
+def compress(message: bytes) -> bytes:
+    return snappy.compress(message)
+
+
+def decompress(message: bytes) -> bytes:
+    return snappy.decompress(message)
+
+
+def get_timestamp_ms() -> int:
+    return int(round(time.time() * 1000))
+
+
+def string_to_sha3_256(text: str):
+    return hashlib.sha3_256(text.encode('utf-8')).hexdigest()
+
+
+def get_uid():
+    return string_to_sha3_256(str(uuid4()))[:12]
