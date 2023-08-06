ef840461a401c46c04ea7d560b07dc3c2ffe29e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:36 +0000

    Add package btmodified/0.6.0/btmodified-0.6.0-py3-none-any.whl

---
+++ packages/btmodified/btmodified-0.6.0-py3-none-any.whl/btmodified/__init__.py
@@ -0,0 +1,11 @@
+from btmodified._btmodified import BERTopic
+from btmodified._ctfidf import ClassTFIDF
+from btmodified._embeddings import languages
+
+__version__ = "0.6.0"
+
+__all__ = [
+    "BERTopic",
+    "ClassTFIDF",
+    "languages"
+]
