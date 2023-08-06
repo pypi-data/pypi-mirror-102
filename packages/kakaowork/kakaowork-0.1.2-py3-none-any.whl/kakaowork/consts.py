debbd4dbd732960c0f211d8922bb5af8c10d0c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:27 +0000

    Add package kakaowork/0.1.2/kakaowork-0.1.2-py3-none-any.whl

---
+++ packages/kakaowork/kakaowork-0.1.2-py3-none-any.whl/kakaowork/consts.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from datetime import timedelta, timezone
+
+
+class StrEnum(str, Enum):
+    """
+    String enumeration type
+    """
+    pass
+
+
+LIMIT = 10
+
+BASE_URL = 'https://api.kakaowork.com'
+BASE_PATH_USERS = '/v1/users'
+BASE_PATH_CONVERSATIONS = '/v1/conversations'
+BASE_PATH_MESSAGES = '/v1/messages'
+BASE_PATH_DEPARTMENTS = '/v1/departments'
+BASE_PATH_SPACES = '/v1/spaces'
+BASE_PATH_BOTS = '/v1/bots'
+
+KST = timezone(timedelta(hours=9))
