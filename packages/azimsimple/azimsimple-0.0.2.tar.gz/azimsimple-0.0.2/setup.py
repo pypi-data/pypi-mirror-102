462a587ace314257d41496146da17c4d21245dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:56 +0000

    Add package azimsimple/0.0.2/azimsimple-0.0.2.tar.gz

---
+++ packages/azimsimple/azimsimple-0.0.2.tar.gz/azimsimple-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='azimsimple',
+  version='0.0.2',
+  description='A very simple library',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Abdul Azim',
+  author_email='abdulazim0402@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='hello world', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
