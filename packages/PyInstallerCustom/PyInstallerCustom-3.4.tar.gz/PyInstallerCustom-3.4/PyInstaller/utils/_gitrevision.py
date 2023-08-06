f45b23eb8e1075ff9b9a3879f56fcf1c7e2dafd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:41 +0000

    Add package PyInstallerCustom/3.4/PyInstallerCustom-3.4.tar.gz

---
+++ packages/PyInstallerCustom/PyInstallerCustom-3.4.tar.gz/PyInstallerCustom-3.4/PyInstaller/utils/_gitrevision.py
@@ -0,0 +1,12 @@
+#
+# The content of this file will be filled in with meaningful data
+# when creating an archive using `git archive` or by downloading an
+# archive from github, e.g. from github.com/.../archive/develop.zip
+#
+rev = "0f31b35fe9"     # abbreviated commit hash
+commit = "0f31b35fe96de59e1a6faf692340a9ef93492472"  # commit hash
+date = "2018-09-09 20:11:29 +0200"   # commit date
+author = "Hartmut Goebel <h.goebel@crazy-compilers.com>"
+ref_names = "tag: v3.4"  # incl. current branch
+commit_message = """Release 3.4.
+"""
