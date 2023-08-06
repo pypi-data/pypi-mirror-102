88675aec74ee86429c61a87aa1cad279f77f722d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:34 +0000

    Add package django-kmuhelper/1.4.1/django-kmuhelper-1.4.1.tar.gz

---
+++ packages/django-kmuhelper/django-kmuhelper-1.4.1.tar.gz/django-kmuhelper-1.4.1/kmuhelper/models.py
@@ -0,0 +1,6 @@
+# pylint: disable=unused-wildcard-import
+
+from kmuhelper.main.models import *
+from kmuhelper.app.models import *
+from kmuhelper.emails.models import *
+from kmuhelper.api.models import *
