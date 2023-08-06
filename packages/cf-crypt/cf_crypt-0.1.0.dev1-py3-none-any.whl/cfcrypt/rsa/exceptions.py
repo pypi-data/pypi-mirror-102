a764fb78e7f013f90d3ee9c33fe0cbf5da0b5e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:11 +0000

    Add package cf-crypt/0.1.0.dev1/cf_crypt-0.1.0.dev1-py3-none-any.whl

---
+++ packages/cf-crypt/cf_crypt-0.1.0.dev1-py3-none-any.whl/cfcrypt/rsa/exceptions.py
@@ -0,0 +1,44 @@
+import sys
+
+try:
+	from cfcrypt.exceptions import CryptException
+except ImportError:
+	CryptException = RuntimeError
+
+
+ENABLE_STACK_TRACE = False
+
+
+class RSAException(CryptException):
+	pass
+
+
+class CryptExceptionNoStack(RSAException):
+	"""The exception handler is hooked to not show the stack trace for these exceptions."""
+	pass
+
+
+class EncryptionFailed(CryptExceptionNoStack):
+	pass
+
+
+class DecryptionFailed(CryptExceptionNoStack):
+	pass
+
+
+class InvalidSignature(CryptExceptionNoStack):
+	pass
+
+
+current_handler = sys.excepthook
+
+
+def exception_handler(exception_type, exception, traceback):
+	if not ENABLE_STACK_TRACE and issubclass(exception_type, CryptExceptionNoStack):
+		print("%s: %s" % (exception_type.__name__, exception), file=sys.stderr)
+		sys.exit(1)
+	return current_handler(exception_type, exception, traceback)
+
+
+# noinspection SpellCheckingInspection
+sys.excepthook = exception_handler
