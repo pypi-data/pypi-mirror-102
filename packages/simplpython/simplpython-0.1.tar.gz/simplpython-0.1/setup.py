958419042942dc1da56f06f7b2dbc0232408f285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:28 +0000

    Add package simplpython/0.1/simplpython-0.1.tar.gz

---
+++ packages/simplpython/simplpython-0.1.tar.gz/simplpython-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='simplpython',
+      version='0.1',
+      description='Easy Python | Python For Everybody',
+      packages=['simplpython', 'gtts', 'playsound'],
+      author_email='samit.mohnot@gmail.com',
+      zip_safe=False)
