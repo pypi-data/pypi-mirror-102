289fc3d8f6445440397af5b0da6c6a72af8d1515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:17 +0000

    Add package nbterm/0.0.5/nbterm-0.0.5-py3-none-any.whl

---
+++ packages/nbterm/nbterm-0.0.5-py3-none-any.whl/nbterm/format.py
@@ -0,0 +1,53 @@
+import json
+
+from .cell import Cell
+
+
+class Format:
+
+    nb_path: str
+
+    def read_nb(self) -> None:
+        with open(self.nb_path) as f:
+            self.nb_json = json.load(f)
+        self.cells = [
+            Cell(self, idx=idx, cell_json=self.nb_json["cells"][idx])
+            for idx, cell in enumerate(self.nb_json["cells"])
+        ]
+
+    def save_nb(self, path: str = "") -> None:
+        path = path or self.nb_path
+        if path:
+            print(path)
+            with open(path, "wt") as f:
+                json.dump(self.nb_json, f, indent=1)
+                f.write("\n")
+
+    def create_nb(self) -> None:
+        self.nb_json = {
+            "cells": [
+                {
+                    "cell_type": "code",
+                    "execution_count": 0,
+                    "metadata": {},
+                    "source": [],
+                    "outputs": [],
+                }
+            ],
+            "metadata": {
+                "kernelspec": {
+                    "display_name": "Python 3",
+                    "language": "python",
+                    "name": "python3",
+                },
+                "language_info": {
+                    "file_extension": ".py",
+                    "mimetype": "text/x-python",
+                    "name": "python",
+                    "version": "3.9.2",
+                },
+            },
+            "nbformat": 4,
+            "nbformat_minor": 4,
+        }
+        self.cells = [Cell(self, idx=0, cell_json=self.nb_json["cells"][0])]
