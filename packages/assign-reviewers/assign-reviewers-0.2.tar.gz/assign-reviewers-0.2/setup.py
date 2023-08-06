09e7c992e90e2c637771f4c315bc8d18982ed815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:15 +0000

    Add package assign-reviewers/0.2/assign-reviewers-0.2.tar.gz

---
+++ packages/assign-reviewers/assign-reviewers-0.2.tar.gz/assign-reviewers-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='assign-reviewers',
+    version='0.2',
+    author='Julien Cohen-Adad',
+    author_email='jcohen@polymtl.ca',
+    packages=find_packages(),
+    url='https://github.com/jcohenadad/assign-reviewers',
+    license='LICENSE',
+    description='Assign reviewers and create scoring excel sheets.',
+    long_description=open('README.md').read(),
+    # install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'assign-reviewers = assign_reviewers.assign_reviewers:main',
+            ]
+        },
+    install_requires=[
+        "pandas",
+        "pytest",
+        "pytest-cov",
+        ]
+    )
