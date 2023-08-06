d2e3870278e2cb2a1fe21e6bec79b9e29b833ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:25 +0000

    Add package novelWriter/1.2.3/novelWriter-1.2.3.tar.gz

---
+++ packages/novelWriter/novelWriter-1.2.3.tar.gz/novelWriter-1.2.3/nw/constants/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from nw.constants.iso import isoLanguage, isoCountry
+from nw.constants.constants import (
+    nwConst, nwLists, nwRegEx, nwFiles, nwKeyWords, nwLabels, nwQuotes,
+    nwUnicode, nwHtmlUnicode
+)
+from nw.constants.enum import (
+    nwAlert, nwDocAction, nwItemClass, nwItemLayout, nwItemType, nwOutline,
+    nwDocInsert, nwWidget
+)
+
+__all__ = [
+    "isoCountry",
+    "isoLanguage",
+    "nwConst",
+    "nwLists",
+    "nwRegEx",
+    "nwFiles",
+    "nwKeyWords",
+    "nwLabels",
+    "nwQuotes",
+    "nwUnicode",
+    "nwHtmlUnicode",
+    "nwAlert",
+    "nwDocAction",
+    "nwItemClass",
+    "nwItemLayout",
+    "nwItemType",
+    "nwOutline",
+    "nwDocInsert",
+    "nwWidget",
+]
