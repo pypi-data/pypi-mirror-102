1561275b6312c9f04720d9f401cd124a460169a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:48 +0000

    Add package disparity-interpolation/1.0.0/disparity_interpolation-1.0.0.tar.gz

---
+++ packages/disparity-interpolation/disparity_interpolation-1.0.0.tar.gz/disparity_interpolation-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+import numpy as np
+from Cython.Build import cythonize
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="disparity_interpolation",
+    version="1.0.0",
+    packages=find_packages(),
+    author="Jhony Kaesemodel Pontes",
+    description="Nearest neighbor interpolation for disparity images with missing parts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhonykaesemodel/disparity_interpolation",
+    ext_modules=cythonize(["disparity_interpolation/disparity_interpolation.pyx"]),
+    include_dirs=["/usr/local/include/opencv4/", np.get_include()],
+    install_requires=["numpy==1.19"],
+    language="c++",
+    extra_compile_args=["-w", "-O3", "std=c++11"],
+)
