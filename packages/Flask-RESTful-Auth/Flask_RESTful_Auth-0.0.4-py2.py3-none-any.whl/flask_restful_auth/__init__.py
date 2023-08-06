9fd976757fe48f5496b5f5fe33136bb068e13ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:13 +0000

    Add package Flask-RESTful-Auth/0.0.4/Flask_RESTful_Auth-0.0.4-py2.py3-none-any.whl

---
+++ packages/Flask-RESTful-Auth/Flask_RESTful_Auth-0.0.4-py2.py3-none-any.whl/flask_restful_auth/__init__.py
@@ -0,0 +1,8 @@
+# This file is used to export classes, modules and functions and store
+# package-global variables.
+#
+# Otherwise this file should remain mostly empty.
+
+__version__ = "0.0.4"
+
+test_message = "Hello Flask!"
---
+++ packages/Flask-RESTful-Auth/Flask_RESTful_Auth-0.0.4-py2.py3-none-any.whl/flask_restful_auth/__main__.py
@@ -0,0 +1,6 @@
+# This file is used when the package is pip installed and one runs `python -m flask_restful_auth`
+
+
+print("TODO: Main example is run here..")
+from . import test_message
+print(test_message)
