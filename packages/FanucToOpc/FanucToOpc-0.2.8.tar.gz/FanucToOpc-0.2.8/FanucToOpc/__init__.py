99078fed1fcab0d30cd2151c32dd35a10c22f165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:32 +0000

    Add package FanucToOpc/0.2.8/FanucToOpc-0.2.8.tar.gz

---
+++ packages/FanucToOpc/FanucToOpc-0.2.8.tar.gz/FanucToOpc-0.2.8/FanucToOpc/__init__.py
@@ -0,0 +1,5 @@
+from FanucToOpc.FanucToOpc import connect, get_reg_state, update_reg_state
+#from FanucToOpc.connect import *
+#from FanucToOpc.get_reg_state import *
+#from FanucToOpc.update_reg_state import *
+
