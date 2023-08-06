91c150c1ac35fa605d78cefd818a85dedf5043b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:22 +0000

    Add package PymoNNto/0.9.2/PymoNNto-0.9.2.tar.gz

---
+++ packages/PymoNNto/PymoNNto-0.9.2.tar.gz/PymoNNto-0.9.2/PymoNNto/Exploration/Evolution/test_slave.py
@@ -0,0 +1,11 @@
+from PymoNNto.Exploration.Evolution.Interface_Functions import *
+
+print('slave')
+
+print('genes: ', get_genome())
+print('gene a: ', get_gene('a', None))
+print('gene b: ', gene('b', 1))
+
+score = get_gene('a', 0) + get_gene('b', 0)
+
+set_score(score)
