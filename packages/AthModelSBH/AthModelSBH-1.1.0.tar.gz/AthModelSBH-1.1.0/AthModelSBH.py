fd02b6914d59bd878c209a5133c1377964edddbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:09 +0000

    Add package AthModelSBH/1.1.0/AthModelSBH-1.1.0.tar.gz

---
+++ packages/AthModelSBH/AthModelSBH-1.1.0.tar.gz/AthModelSBH-1.1.0/AthModelSBH.py
@@ -0,0 +1,16 @@
+import json
+import sqlite3
+
+def get_ath_name():
+    connection = sqlite3.connect('coachdata.sqlite')
+    cursor = connection.cursor()
+
+    names = cursor.execute("""SELECT name FROM athletes""")
+    names_list = [row[0] for row in names.fetchall()]
+    names_json = json.dump(names_list)
+
+    connection.close()
+
+    return(names_json)
+
+
---
+++ packages/AthModelSBH/AthModelSBH-1.1.0.tar.gz/AthModelSBH-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'AthModelSBH',
+    version = '1.1.0',
+    py_modules = ['AthModelSBH'],
+    author = 'sourav',
+    author_email = 'bhsourav17@gmail.com',
+    description = 'Athlete list class'
+    )
