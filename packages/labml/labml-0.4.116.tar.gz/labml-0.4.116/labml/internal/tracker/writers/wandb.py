b6aa7f5110d203066e3fe6eb7fb2a3a90f41da7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:11 +0000

    Add package labml/0.4.116/labml-0.4.116.tar.gz

---
+++ packages/labml/labml-0.4.116.tar.gz/labml-0.4.116/labml/internal/tracker/writers/wandb.py
@@ -0,0 +1,40 @@
+from typing import Dict
+
+import wandb
+
+from . import Writer as WriteBase
+from ..indicators import Indicator
+from ..indicators.numeric import NumericIndicator
+
+
+class Writer(WriteBase):
+    def __init__(self):
+        super().__init__()
+        self.wandb = wandb
+        self.wandb.init()
+
+    @staticmethod
+    def _parse_key(key: str):
+        return key
+
+    def write_h_parameters(self, hparams: Dict[str, any]):
+        self.wandb.config.update(hparams)
+
+    def _write_indicator(self, global_step: int, indicator: Indicator):
+        if indicator.is_empty():
+            return
+
+        if isinstance(indicator, NumericIndicator):
+            hist_data = indicator.get_histogram()
+            if hist_data is not None:
+                self.wandb.log({self._parse_key(indicator.name): self.wandb.Histogram(hist_data)},
+                               step=global_step)
+
+            self.wandb.log({self._parse_key(indicator.mean_key): indicator.get_mean()},
+                           step=global_step)
+
+    def write(self, *,
+              global_step: int,
+              indicators: Dict[str, Indicator]):
+        for ind in indicators.values():
+            self._write_indicator(global_step, ind)
