d678925b09697e11029e87ada1f2722a14c381a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:55 +0000

    Add package BetterString/2.0.3/BetterString-2.0.3.tar.gz

---
+++ packages/BetterString/BetterString-2.0.3.tar.gz/BetterString-2.0.3/README.md
@@ -0,0 +1,5 @@
+# BetterString
+### This is a QoL (Quality of Life) package   
+It implements some more functionality to the normal Python str    
+Example:   
+You can specify how many characters should be uppercase in .upper
---
+++ packages/BetterString/BetterString-2.0.3.tar.gz/BetterString-2.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="BetterString",
+    version="2.0.3",
+    # Major version 2
+    # Minor version 0
+    # Maintenance version 1
+
+    author="DerSchinken (aka DrBumm)",
+    description="Like a normal string but with more functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
