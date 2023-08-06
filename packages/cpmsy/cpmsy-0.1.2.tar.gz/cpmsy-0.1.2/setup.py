e20dd45954f2a1b66e0e276626b1ea3711d9494f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:06 +0000

    Add package cpmsy/0.1.2/cpmsy-0.1.2.tar.gz

---
+++ packages/cpmsy/cpmsy-0.1.2.tar.gz/cpmsy-0.1.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "cpmsy",
+    version = "0.1.2",
+    author = "yangmiao",
+    author_email = "1446174581@qq.com",
+    description = "point cloud match",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = "MIT",
+    url = "https://pypi.org/manage/projects/cpmsy",
+    packages = ['cpmsy'],
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+)
