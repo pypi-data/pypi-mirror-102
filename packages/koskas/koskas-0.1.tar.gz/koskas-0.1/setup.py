f25dfa3d68f0ff7488750cd90c9027ef2d8a5e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:25 +0000

    Add package koskas/0.1/koskas-0.1.tar.gz

---
+++ packages/koskas/koskas-0.1.tar.gz/koskas-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='koskas',
+    version='0.1',    
+    description='My python package',
+    url='https://github.com/yairKoskas/koskas',
+    author='Yair Koskas',
+    author_email='yairi2003@gmail.com',
+    license='BSD 2-clause',
+    packages=['koskas'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
