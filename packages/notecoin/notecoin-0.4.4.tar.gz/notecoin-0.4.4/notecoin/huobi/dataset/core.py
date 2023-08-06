0564e0bb0442952225174dcbb5a1c9a738a5461e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:08 +0000

    Add package notecoin/0.4.4/notecoin-0.4.4.tar.gz

---
+++ packages/notecoin/notecoin-0.4.4.tar.gz/notecoin-0.4.4/notecoin/huobi/dataset/core.py
@@ -0,0 +1,79 @@
+import os
+
+from notedata.tables import SqliteTable
+
+
+class TradeDetail(SqliteTable):
+    def __init__(self, table_name='trade_detail', db_path=None, *args, **kwargs):
+        if db_path is None:
+            db_path = os.path.abspath(
+                os.path.dirname(__file__)) + '/data/coin.db'
+
+        super(TradeDetail, self).__init__(db_path=db_path, table_name=table_name, *args, **kwargs)
+        self.columns = ['trade_id', 'ts', 'direction', 'price', 'amount']
+
+    def create(self):
+        self.execute("""
+            create table if not exists {} (
+               trade_id       BIGINT         
+              ,amount         FLOAT 
+              ,price          FLOAT
+              ,ts             BIGINT
+              ,direction      VARCHAR(5)
+              ,primary key (trade_id)           
+              )
+            """.format(self.table_name))
+
+
+class SymbolInfo(SqliteTable):
+    def __init__(self, table_name='stock_basic', db_path=None, *args, **kwargs):
+        if db_path is None:
+            db_path = os.path.abspath(
+                os.path.dirname(__file__)) + '/data/coin.db'
+
+        super(SymbolInfo, self).__init__(db_path=db_path,
+                                         table_name=table_name, *args, **kwargs)
+        self.columns = ['symbol', 'base_currency', 'quote_currency', 'price_precision', 'amount_precision',
+                        'symbol_partition',
+                        'state', 'value_precision',
+                        'min_order_amt', 'max_order_amt',
+                        'limit_order_min_order_amt', 'limit_order_max_order_amt',
+                        'sell_market_min_order_amt', 'sell_market_max_order_amt',
+                        'buy_market_max_order_value',
+                        'min_order_value', 'max_order_value',
+                        'leverage_ratio', 'underlying', 'mgmt_fee_rate', 'charge_time',
+                        'rebal_time', 'rebal_threshold',
+                        'init_nav', 'api_trading'
+                        ]
+
+    def create(self):
+        self.execute("""
+                create table if not exists {} (
+                symbol		                    VARCHAR	-- 交易对
+                ,base_currency                  VARCHAR -- 交易对中的基础币种
+                ,quote_currency		            VARCHAR	-- 交易对中的报价币种
+                ,price_precision		        integer	-- 交易对报价的精度（小数点后位数）
+                ,amount_precision		        integer	-- 交易对基础币种计数精度（小数点后位数）
+                ,symbol_partition		        VARCHAR	-- 交易区，可能值: [main，innovation]
+                ,`state`		                VARCHAR	-- 交易对状态；可能值: [online，offline,suspend] online _ 已上线；offline _ 交易对已下线，不可交易；suspend -- 交易暂停；pre_online _ 即将上线
+                ,value_precision		        integer	-- 交易对交易金额的精度（小数点后位数）
+                ,min_order_amt		            float	-- 交易对限价单最小下单量 ，以基础币种为单位（即将废弃）
+                ,max_order_amt		            float	-- 交易对限价单最大下单量 ，以基础币种为单位（即将废弃）
+                ,limit_order_min_order_amt		float	-- 交易对限价单最小下单量 ，以基础币种为单位（NEW）
+                ,limit_order_max_order_amt		float	-- 交易对限价单最大下单量 ，以基础币种为单位（NEW）
+                ,sell_market_min_order_amt		float	-- 交易对市价卖单最小下单量，以基础币种为单位（NEW）
+                ,sell_market_max_order_amt		float	-- 交易对市价卖单最大下单量，以基础币种为单位（NEW）
+                ,buy_market_max_order_value		float	-- 交易对市价买单最大下单金额，以计价币种为单位（NEW）
+                ,min_order_value		        float	-- 交易对限价单和市价买单最小下单金额 ，以计价币种为单位
+                ,max_order_value		        float	-- 交易对限价单和市价买单最大下单金额 ，以折算后的USDT为单位（NEW）
+                ,leverage_ratio		            float	-- 交易对杠杆最大倍数(仅对逐仓杠杆交易对、全仓杠杆交易对、杠杆ETP交易对有效）
+                ,underlying		                VARCHAR	-- 标的交易代码 (仅对杠杆ETP交易对有效)
+                ,mgmt_fee_rate		            float	-- 持仓管理费费率 (仅对杠杆ETP交易对有效)
+                ,charge_time		            VARCHAR	-- 持仓管理费收取时间 (24小时制，GMT+8，格式：HH:MM:SS，仅对杠杆ETP交易对有效)
+                ,rebal_time		                VARCHAR	-- 每日调仓时间 (24小时制，GMT+8，格式：HH:MM:SS，仅对杠杆ETP交易对有效)
+                ,rebal_threshold		        float	-- 临时调仓阈值 (以实际杠杆率计，仅对杠杆ETP交易对有效)
+                ,init_nav		                float	-- 初始净值（仅对杠杆ETP交易对有效）
+                ,api_trading		            VARCHAR	-- API交易使能标记（有效值：enabled, disabled）
+                ,PRIMARY KEY (symbol)
+                )
+                """.format(self.table_name))
