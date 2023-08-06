5526fdb2906e3b4314a550d85401a8b9bb1e4767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:54 +0000

    Add package wikinet/0.0.9/wikinet-0.0.9.tar.gz

---
+++ packages/wikinet/wikinet-0.0.9.tar.gz/wikinet-0.0.9/setup.py
@@ -0,0 +1,57 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wikinet",
+    version="0.0.9",
+    author="Harang Ju",
+    author_email="harangju@gmail.com",
+    description="Network of wikipedia articles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harangju/wikinet",
+    project_urls={
+        "Bug Tracker": "https://github.com/harangju/wikinet/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires="==3.7", # need to make >=
+    install_requires=[
+        'cython',
+        'jupyter',
+        'numpy',
+        'scipy',
+        'pandas>1.0.0',
+        'networkx',
+        'gensim',
+        'python-Levenshtein', # gensim warning
+        'mpmath',
+        'sphinx',
+        'nbconvert==5.6.1', # https://github.com/ipython-contrib/jupyter_contrib_nbextensions/issues/1529#issuecomment-695057809
+        'plotly',
+#        'plotly-orca', # saving plotly
+        'psutil', # saving plotly
+        'leidenalg',
+        'python-igraph',
+        'bctpy>=0.5.2',
+        'mwparserfromhell',
+        'sklearn',
+        'dionysus<=2.0.7', # weird dependency issue with python>3.7
+        'pybind11',
+        'cpnet<=0.0.6',
+        'powerlaw',
+        'sphinx_rtd_theme',
+        'cufflinks',
+        'dill',
+        'rpy2',
+        'build',
+        'twine'
+    ]
+)
