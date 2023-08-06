941052571fbdfb5b0d04255f144037976e8b17ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:24 +0000

    Add package gdrive2/0.7.0/gdrive2-0.7.0.tar.gz

---
+++ packages/gdrive2/gdrive2-0.7.0.tar.gz/gdrive2-0.7.0/gdrive2/paths.py
@@ -0,0 +1,31 @@
+
+import os
+
+VERSION = '0.7.0'
+#Deafult info during initialization
+DEFAULT_INFO = {
+        'default_parent' : 'origin',
+        'origin' : ['no_user', '', 'root', 'My Drive', 'root', 'client_secrets']
+        #parent_name : [user_name, parent_path, parent_id, drive_name, drive_id, <name of client secrets json>]
+        }
+
+#Folder names
+API_DATA_FOLDER = 'api_data'
+CRED_MAP = 'creds'
+CLIENT_FOLDER = 'client_secrets'
+
+#Credentials and util paths
+ROOT_PATH = os.path.dirname(__file__)
+CREDS_DIR = os.path.join(ROOT_PATH, API_DATA_FOLDER)
+
+#Credentials
+CRED_MAP_PATH = os.path.join(CREDS_DIR, CRED_MAP)
+
+#Default client secrets json name
+DEFAULT_CLIENT = DEFAULT_INFO[DEFAULT_INFO['default_parent']][5]
+CLIENT_SECRETS_DIR = os.path.join(CREDS_DIR, CLIENT_FOLDER)
+
+if not os.path.exists(CREDS_DIR):
+    os.mkdir(CREDS_DIR)
+if not os.path.exists(CLIENT_SECRETS_DIR):
+    os.mkdir(CLIENT_SECRETS_DIR)
---
+++ packages/gdrive2/gdrive2-0.7.0.tar.gz/gdrive2-0.7.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+from gdrive2.paths import VERSION
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gdrive2",
+    version=VERSION,
+    author="Preetham Paul",
+    author_email="preeth@uw.edu",
+    description="File management tools for Google Drive based on PyDrive",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/preethampaul/gdrive2",
+    project_urls={
+    	"Documentation" : "https://gdrive2.readthedocs.io"
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+
+    package_data={
+    "" : ["gd"],
+    },
+    install_requires=[
+        'PyDrive>=1.3.1',
+    ]
+)
