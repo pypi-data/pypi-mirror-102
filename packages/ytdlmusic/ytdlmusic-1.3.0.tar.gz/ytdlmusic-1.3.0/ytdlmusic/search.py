50400b43703e811ec97bd60bb696438c9716b665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:07 +0000

    Add package ytdlmusic/1.3.0/ytdlmusic-1.3.0.tar.gz

---
+++ packages/ytdlmusic/ytdlmusic-1.3.0.tar.gz/ytdlmusic-1.3.0/ytdlmusic/search.py
@@ -0,0 +1,24 @@
+"""
+search utils scripts
+"""
+from ytdlmusic.params import is_verbose
+
+
+def search(artist, song):
+    """
+    search the items with youtube-search-python
+    return a json with 5 entries of YouTube results
+    param : the artist and the song
+    """
+    from youtubesearchpython import VideosSearch
+
+    if is_verbose():
+        print("[debug] artist : " + artist)
+        print("[debug] song : " + song)
+    search_pattern = artist + " " + song
+    print(
+        'search "' + search_pattern + '" with youtube-search-python'
+    )
+    results_search = VideosSearch(search_pattern, limit=5)
+
+    return results_search
