5282063c6dfc012c46ed3f30aa2210c90233bfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:21 +0000

    Add package truster/0.1.1/truster-0.1.1-py3-none-any.whl

---
+++ packages/truster/truster-0.1.1-py3-none-any.whl/truster/cluster.py
@@ -0,0 +1,235 @@
+#! /bin/python3
+import subprocess
+import os
+from .jobHandler import *
+import pysam
+from .bcolors import Bcolors
+
+class Cluster:
+    def __init__(self, clusterName, tsv, logfile):
+        self.clusterName = clusterName
+        self.tsv = tsv
+        self.outdirs = {"tsvToBam" : None, "filterUMIs" : None, "bamToFastq" : None, "concatenateLanes" : None, "mapCluster" : None}
+        self.logfile = logfile
+
+    def tsvToBam(self, sampleId, bam, outdir, slurm=None, modules=None):
+        with open(self.logfile, "a") as log:
+            try:
+                if not os.path.exists("tsvToBam_scripts"):
+                    os.makedirs("tsvToBam_scripts", exist_ok=True)
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+    
+                cmd = ["subset-bam", "--bam", bam, "--cell-barcodes", self.tsv, "--out-bam", os.path.join(outdir, (self.clusterName + ".bam"))]
+    
+                if slurm != None:
+                    cmd = ' '.join(cmd)
+    
+                    jobFile =  os.path.join("tsvToBam_scripts/", (self.clusterName + "_tsvToBam.sh"))
+                    try:
+                        jobId = runJob("tsvToBam", jobFile, cmd, slurm, modules)
+                        msg = sucessSubmit("tsvToBam", (" sample " + sampleId + " cluster " +  self.clusterName), jobId)
+                        log.write(msg)
+
+                        exitCode = waitForJob(jobId)
+                        msg = checkExitCodes("tsvToBam", ("Sample " + sampleId + ", cluster " + self.clusterName),jobId, exitCode)
+                        log.write(msg)
+                        if exitCode == 0:
+                            self.outdirs["tsvToBam"] = outdir
+                        return (jobId, exitCode)
+                    except:
+                        msg = genericError("tsvToBam", (" sample " + sampleId + " cluster " +  self.clusterName))
+                        log.write(msg)
+                        return
+                else:
+                    subprocess.call(cmd)
+            except KeyboardInterrupt:
+                msg = Bcolors.HEADER + "User interrupted" + Bcolors.ENDC
+                log.write(msg)
+
+    def filterUMIs(self, sampleId, inbam, outdir, slurm=None, modules=None):
+        with open(self.logfile, "a") as log:
+            try:
+                if not os.path.exists("filterUMIs_scripts"):
+                    os.makedirs("filterUMIs_scripts", exist_ok=True)
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+                outbam = os.path.join(outdir, (self.clusterName + "_filtered.bam"))
+                
+                cwd = os.path.dirname(os.path.realpath(__file__))
+                
+                cmd = ["python", os.path.join(cwd, "py_scripts/filterUMIs"), "-i", inbam, "-o", outbam]
+                if slurm != None:
+                    cmd = ' '.join(cmd)
+                    jobFile =  os.path.join("filterUMIs_scripts/", (self.clusterName + "_filterUMIs.sh"))
+                    try:
+                        jobId = runJob("filterUMIs", jobFile, cmd, slurm, modules)
+                        msg = sucessSubmit("filterUMIs", (" sample " + sampleId + " cluster " +  self.clusterName), jobId)
+                        log.write(msg)
+
+                        exitCode = waitForJob(jobId)
+                        msg = checkExitCodes("filterUMIs", ("Sample " + sampleId + ", cluster " + self.clusterName),jobId, exitCode)
+                        log.write(msg)
+                        if exitCode == 0:
+                            self.outdirs["filterUMIs"] = outdir
+                        return (jobId, exitCode)
+                    except:
+                        msg = genericError("filterUMIs", (" sample " + sampleId + " cluster " +  self.clusterName))
+                        log.write(msg)
+                        return
+                else:
+                    subprocess.call(cmd)
+            except KeyboardInterrupt:
+                msg = Bcolors.HEADER + "User interrupted" + Bcolors.ENDC
+                log.write(msg)
+
+    def bamToFastq(self, sampleId, bam, outdir, slurm=None, modules=None):
+        with open(self.logfile, "a") as log:
+            try:
+                if not os.path.exists("bamToFastq_scripts"):
+                    os.makedirs("bamToFastq_scripts", exist_ok=True)
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+                cmd = ["bamtofastq-1.2.0", bam, (outdir + "/" + self.clusterName)]
+                if slurm != None:
+                    cmd = ' '.join(cmd)
+                    jobFile =  os.path.join("bamToFastq_scripts/", (self.clusterName + "_bamToFastq.sh"))
+                    try:
+                        jobId = runJob("bamToFastq", jobFile, cmd, slurm, modules)
+                        msg = sucessSubmit("bamToFastq", (" sample " + sampleId + " cluster " +  self.clusterName), jobId)
+                        log.write(msg)
+
+                        exitCode = waitForJob(jobId)
+                        msg = checkExitCodes("bamToFastq", ("Sample " + sampleId + ", cluster " + self.clusterName),jobId, exitCode)
+                        log.write(msg)
+                        
+                        if exitCode == 0:
+                            self.outdirs["bamToFastq"] = outdir
+                        return (jobId, exitCode)
+                    except:
+                        msg = genericError("bamToFastq", (" sample " + sampleId + " cluster " +  self.clusterName))
+                        log.write(msg)
+                        return
+                else:
+                    subprocess.call(cmd)
+            except KeyboardInterrupt:
+                msg = Bcolors.HEADER + "User interrupted" + Bcolors.ENDC
+                log.write(msg)
+
+    def concatenateLanes(self, sampleId, indir, outdir, slurm=None, modules=None):
+        with open(self.logfile, "a") as log:
+            try:
+                if not os.path.exists("concatenateLanes_scripts"):
+                    os.makedirs("concatenateLanes_scripts", exist_ok=True)
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+    
+                cmd = ["cat", os.path.join(indir, self.clusterName, "*/*_R2_001.fastq.gz"), ">", os.path.join(outdir, (self.clusterName + "_R2.fastq.gz"))]
+                if slurm != None:
+                    cmd = ' '.join(cmd)
+                    jobFile =  os.path.join("concatenateLanes_scripts/", (self.clusterName + "_concatenateLanes.sh"))
+                    try:
+                        jobId = runJob("concatenateLanes", jobFile, cmd, slurm, modules)
+                        msg = sucessSubmit("concatenateLanes", (" sample " + sampleId + " cluster " +  self.clusterName), jobId)
+                        log.write(msg)
+
+                        exitCode = waitForJob(jobId)
+                        msg = checkExitCodes("concatenateLanes", ("Sample " + sampleId + ", cluster " + self.clusterName),jobId, exitCode)
+                        log.write(msg)
+                        if exitCode == 0:
+                            self.outdirs["concatenateLanes"] = outdir
+                        return (jobId, exitCode)
+                    except:
+                        msg = genericError("concatenateLanes", (" sample " + sampleId + " cluster " +  self.clusterName))
+                        log.write(msg)
+                        return
+                else:
+                    subprocess.call([cmd])
+            except KeyboardInterrupt:
+                msg = Bcolors.HEADER + "User interrupted" + Bcolors.ENDC
+                log.write(msg)
+
+    def mapCluster(self, sampleId, fastqdir, outdir, geneGTF, starIndex, RAM, outTmpDir = None, unique=False, slurm=None, modules=None):
+        with open(self.logfile, "a") as log:
+            try:
+                if not os.path.exists("mapCluster_scripts"):
+                    os.makedirs("mapCluster_scripts", exist_ok=True)
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+    
+                cmd = ["STAR", "--runThreadN", str(slurm["mapCluster"]["tasks-per-node"]), "--readFilesCommand", "gunzip", "-c", "--outSAMattributes", "All", "--outSAMtype", "BAM", "SortedByCoordinate", "--sjdbGTFfile", str(geneGTF), "--genomeDir", str(starIndex), "--outFileNamePrefix", (str(os.path.join(outdir, self.clusterName)) + "_") , "--limitBAMsortRAM", str(RAM)]
+                if unique:
+                    cmd.extend(["--outFilterMultimapNmax", "1", "--outFilterMismatchNoverLmax", "0.03"])
+                else:
+                    cmd.extend(["--outFilterMultimapNmax", "100", "--winAnchorMultimapNmax", "200"])
+                if outTmpDir != None:
+                    cmd.extend(["--outTmpDir", outTmpDir])
+                cmd.extend(["--readFilesIn", os.path.join(fastqdir, (self.clusterName + "_R2.fastq.gz"))])
+                if slurm != None:
+                    cmd = ' '.join(cmd)
+                    jobFile =  os.path.join("mapCluster_scripts/", (self.clusterName + "_mapCluster.sh"))
+                    try:
+                        jobId = runJob("mapCluster", jobFile, cmd, slurm, modules)
+                        msg = sucessSubmit("mapCluster", (" sample " + sampleId + " cluster " +  self.clusterName), jobId)
+                        log.write(msg)
+
+                        exitCode = waitForJob(jobId)
+                        msg = checkExitCodes("mapCluster", ("Sample " + sampleId + ", cluster " + self.clusterName),jobId, exitCode)
+                        log.write(msg)
+                        
+                        if exitCode == 0:
+                            self.outdirs["mapCluster"] = outdir
+                        return (jobId, exitCode)
+                    except:
+                        msg = genericError("mapCluster", (" sample " + sampleId + " cluster " +  self.clusterName))
+                        log.write(msg)
+                        return
+                else:
+                    subprocess.call(cmd)
+            except KeyboardInterrupt:
+                msg = Bcolors.HEADER + "User interrupted" + Bcolors.ENDC
+                log.write(msg)
+
+    def TEcount(self, experimentName, sampleId, bam, outdir, geneGTF, teGTF, unique=False, slurm=None, modules=None):
+        with open(self.logfile, "a") as log:
+            try:
+                if not os.path.exists("TEcount_scripts"):
+                    os.makedirs("TEcount_scripts", exist_ok=True)
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir, exist_ok=True)
+                
+                if unique:
+                    cmd = ["featureCounts", "-s", str(1), "-F", "GTF", "-g", "transcript_id", "-a", teGTF, "-o", os.path.join(outdir, (experimentName + "_" + self.clusterName + "_uniqueMap.cntTable")), bam]
+                else:
+                    cmd = ["TEcount", "-b", bam, "--GTF", geneGTF, "--TE", teGTF, "--format", "BAM", "--stranded", "yes", "--mode", "multi", "--sortByPos", "--project", os.path.join(outdir, (experimentName + "_" + self.clusterName + "_"))]
+    
+                if slurm != None:
+                    cmd = ' '.join(cmd)
+                    jobFile =  os.path.join("TEcount_scripts/", (self.clusterName + "_TEcount.sh"))
+                    try:
+                        if unique:
+                            function_name = "TEcount_unique"
+                        else:
+                            function_name = "TEcount"
+                        jobId = runJob(function_name, jobFile, cmd, slurm, modules)
+                        msg = sucessSubmit(function_name, (" sample " + sampleId + " cluster " +  self.clusterName), jobId)
+                        log.write(msg)
+                        
+                        exitCode = waitForJob(jobId)
+                        msg = checkExitCodes(function_name, ("Sample " + sampleId + ", cluster " + self.clusterName),jobId, exitCode)
+                        log.write(msg)
+                        
+                        if exitCode == 0:
+                            self.outdirs["TEcount"] = outdir
+                        return (jobId, exitCode)
+                    except:
+                        msg = genericError(function_name, (" sample " + sampleId + " cluster " +  self.clusterName))
+                        log.write(msg)
+                        return
+                else:
+                    subprocess.call(cmd)
+            except KeyboardInterrupt:
+                msg = Bcolors.HEADER + "User interrupted" + Bcolors.ENDC
+                log.write(msg)
+
+
---
+++ packages/truster/truster-0.1.1-py3-none-any.whl/truster/r_scripts/normalize_TEexpression.R
@@ -0,0 +1,164 @@
+#!/bin/env Rscript
+
+library(optparse)
+library(Seurat)
+library(stringr)
+library(data.table)
+library(dplyr)
+library(patchwork)
+set.seed(10)
+
+# Bed file of transposons
+# Tab file of classification of transposons
+# Path to RData 
+# Path to TEcounts folder with output per cluster
+# 
+# 
+# mode = 'perSample'
+# obj_name = 'Seq109_11'
+# outdir = '/Volumes/My Passport/TBI/12.03.21/2_getClusters/clusterPipeline/multi_TEcountsNormalized/Seq109_11/'
+# indir = '/Volumes/My Passport/TBI/12.03.21/2_getClusters/clusterPipeline/multi_TEcounts/Seq109_11/'
+# rdata = '/Volumes/My Passport/TBI/12.03.21/2_getClusters/Seq109_11/Seq109_11.rds'
+# # 
+# mode = 'merged'
+# obj_name = "ctrl"
+# outdir = '/Volumes/My Passport/trim28/05.02.21/3_mergeSamples/ctrl/clusterPipeline/TEcountsNormalized/'
+# indir = '/Volumes/My Passport/trim28/05.02.21/3_mergeSamples/ctrl/clusterPipeline/TEcounts/'
+# rdata = '/Volumes/My Passport/trim28/05.02.21/3_mergeSamples/ctrl/ctrl.RData'
+
+option_list = list(
+  make_option(c("-m", "--mode"), type="character", default=NULL,
+              help="Merged samples or individual? (merged/individual)", metavar="character"),
+  make_option(c("-r", "--RDS"), type="character", default=NULL,
+              help="Path to RDS with Seurat object", metavar="character"),
+  make_option(c("-n", "--name"), type="character", default=NULL,
+              help="Sample id or name", metavar="character"),
+  make_option(c("-i", "--indir"), type="character", default=NULL,
+              help="Path to TEcounts folder with output per cluster or parent directory from all samples folder", metavar="character"),
+  make_option(c("-o", "--outdir"), type="character", default=NULL,
+              help="Output directory for the matrix with normalized counts", metavar="character")
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$indir) | is.null(opt$outdir) | is.null(opt$RDS) | is.null(opt$mode) ){
+  print_help(opt_parser)
+  stop("All argument must be supplied.", call.=FALSE)
+}
+
+mode <- trimws(opt$mode)
+rds <- opt$RDS
+obj_name <- opt$name
+indir <- ifelse(endsWith(opt$indir, "/"), opt$indir, paste(opt$indir, '/', sep=''))
+outdir <- ifelse(endsWith(opt$outdir, "/"), opt$outdir, paste(opt$outdir, '/', sep=''))
+
+if(!dir.exists(outdir)){
+  dir.create(outdir, recursive = T)
+}
+
+print(c("Mode: ", mode))
+print(c("RDS to be used: ", rds))
+print(c("Input path: ", indir))
+print(c("Output path: ", outdir))
+
+seurat.obj <- readRDS(rds)
+
+cluster_sizes <- data.frame(cluster.size=table(seurat.obj@active.ident))  
+colnames(cluster_sizes) <- c('cluster', 'cluster.size')
+cluster_sizes$name <- paste(obj_name, cluster_sizes$cluster, sep='.cluster_')
+
+files <- list.files(indir, recursive = F)
+coldata <- data.frame()
+for(i in 1:length(files)){
+  if(mode == 'merged'){
+    # mergedCluster is how trusTEr is programmed to name the files
+    cluster <- unlist(str_split(unlist(str_split(files[i], "mergedCluster_")[1])[2], "_"))[1]
+    sample = paste(obj_name, cluster, sep="_")
+    name <- paste(obj_name, cluster, sep=".cluster_")
+    coldata <- rbind(coldata, data.frame(sample=sample, name = name, cluster=cluster))
+  }else{
+    file_name <- sub("_$", "", sapply(str_split(files[i], '.cntTable'), `[[`, 1))
+    # sample <- unlist(str_split(file_name, '_clusters'))[1]
+    cluster <- unlist(str_split(file_name, 'clusters_'))[2]
+    name <- paste(obj_name, cluster, sep=".cluster_")
+    coldata <- rbind(coldata, data.frame(sample=obj_name, name=name, cluster=cluster))
+  }
+  
+  file <- paste(indir, files[i], sep='')
+  
+  if (i == 1){
+    TEcounts <- fread(file, data.table = F)
+    colnames(TEcounts) <- c("TE", name)
+  }else{
+    counts <- fread(file, data.table = F)
+    colnames(counts) <- c("TE", name)
+    TEcounts <- merge(TEcounts, counts, all=T, by="TE")
+  }
+}
+
+rownames(TEcounts) <- TEcounts$TE
+rownames(coldata) <- coldata$name
+
+num_reads <- data.frame(id=names(colSums(TEcounts[which(startsWith(TEcounts$TE, "ENS")),rownames(coldata)])),
+                        value=colSums(TEcounts[which(startsWith(TEcounts$TE, "ENS")),rownames(coldata)]))
+if(mode != "merged"){
+  num_reads$sample_id <- sapply(str_split(num_reads$id, '[[.]]'), `[[`, 1)
+  # If we are normalizing by seq depth per sample, we can aggregate per sample id
+  num_reads <- aggregate(num_reads$value, by=list(num_reads$sample_id), FUN=sum)
+}
+
+coldata <- merge(coldata, cluster_sizes[,c('name', 'cluster.size')], by='name')
+rownames(coldata) <- coldata$name
+
+colnames(num_reads) <- c('sample', 'num_reads')
+rownames(num_reads) <- num_reads$sample
+
+TEcounts <- subset(TEcounts, !startsWith(TEcounts$TE, 'ENS'))
+
+if(mode != "merged"){
+  coldata <- merge(coldata, num_reads, by='sample')  
+}
+
+rownames(coldata) <- coldata$name
+te_counts_size <- TEcounts[, rownames(coldata)]
+te_counts_size_norm <- te_counts_size
+te_counts_size_norm[] <- mapply('/', te_counts_size_norm[, rownames(coldata)], coldata$cluster.size)
+
+if(mode != "merged"){
+  # ONLY if it's per sample, divide the reads by the sequencing depth of the sample 
+  # Otherwise we end up dividing per number of reads in a cluster which is against what we want...
+  # Please ensure to take away "num_reads" if we are in merged mode
+  te_counts_size_norm[] <- mapply('/', te_counts_size_norm[, rownames(coldata)], coldata$num_reads)  
+  te_counts_size_norm <- te_counts_size_norm[, rownames(coldata)] * 1e+10
+}
+
+te_counts_size_norm$te_id <- rownames(te_counts_size_norm)
+te_counts_size$te_id <- rownames(te_counts_size)
+te_counts_size_norm_melt <- reshape2::melt(te_counts_size_norm, by=list(c('te_id')))
+te_counts_size_melt <- reshape2::melt(te_counts_size, by=list(c('te_id')))
+
+te_counts_size_norm_melt$cluster <- gsub("cluster_", "", sapply(str_split(te_counts_size_norm_melt$variable, '[[.]]'),`[[`, 2))
+te_counts_size_melt$cluster <- gsub("cluster_", "", sapply(str_split(te_counts_size_melt$variable, '[[.]]'),`[[`, 2))
+
+te_counts_size_norm <- te_counts_size_norm[,c(colnames(te_counts_size_norm)[ncol(te_counts_size_norm)], colnames(te_counts_size_norm)[-ncol(te_counts_size_norm)])]
+te_counts_size <- te_counts_size[,c(colnames(te_counts_size)[ncol(te_counts_size)], colnames(te_counts_size)[-ncol(te_counts_size)])]
+fwrite(te_counts_size_norm, paste(outdir, 'TE_normalizedValues_matrix.csv', sep=''), quote = F, row.names = F, col.names = T, verbose = T)
+# To compare with bulk data
+fwrite(te_counts_size, paste(outdir, 'TE_rawValues_matrix.csv', sep=''), quote = F, row.names = F, col.names = T, verbose = T)
+
+if(mode == 'merged'){
+  te_counts_size_norm_aggr <- aggregate(te_counts_size_norm_melt$value, list(te_counts_size_norm_melt$te_id, te_counts_size_norm_melt$variable, te_counts_size_norm_melt$cluster), mean)
+  te_counts_size_aggr <- aggregate(te_counts_size_melt$value, list(te_counts_size_melt$te_id, te_counts_size_melt$variable, te_counts_size_melt$cluster), mean)
+  # te_counts_size_aggr <- aggregate(te_counts_size_melt$value, list(te_counts_size_melt$te_id, te_counts_size_melt$cluster), mean)
+  colnames(te_counts_size_norm_aggr) <- c('te_id', 'sample_cluster', 'cluster', 'value')
+  colnames(te_counts_size_aggr) <- c('te_id', 'sample_cluster', 'cluster', 'value')
+  fwrite(te_counts_size_norm_aggr, paste(outdir, 'TE_normalizedValues_aggregatedByClusters_melted.csv', sep=''), quote = F, row.names = F, col.names = T, verbose = T)
+  fwrite(te_counts_size_aggr, paste(outdir, 'TE_rawValues_aggregatedByClusters_melted.csv', sep=''), quote = F, row.names = F, col.names = T, verbose = T)
+}else{
+  fwrite(te_counts_size_norm_melt, paste(outdir, 'TE_normalizedValues_melted.csv', sep=''), quote = F, row.names = F, col.names = T, verbose = T)
+  fwrite(te_counts_size_melt, paste(outdir, 'TE_rawValues_melted.csv', sep=''), quote = F, row.names = F, col.names = T, verbose = T)
+}
+
+
+
