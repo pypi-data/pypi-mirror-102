57215b7a8cc5f4e75752f98b02e432b7725c4947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:41 +0000

    Add package bedhost/0.0.5/bedhost-0.0.5.tar.gz

---
+++ packages/bedhost/bedhost-0.0.5.tar.gz/bedhost-0.0.5/bedhost/data_models.py
@@ -0,0 +1,21 @@
+from typing import Dict, List, Text
+
+from pydantic import BaseModel
+
+
+class DBResponse(BaseModel):
+    """
+    Database response data model
+    """
+
+    columns: List
+    data: List[List]
+
+
+class SchemaElement(BaseModel):
+    """
+    Schema element data model
+    """
+
+    type: Text
+    description: Text
