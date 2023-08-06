59fc963c00bfb46bf03e4df1dacd1cbff4907d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:41 +0000

    Add package PyImageOp/0.1.01b/PyImageOp-0.1.01b.tar.gz

---
+++ packages/PyImageOp/PyImageOp-0.1.01b.tar.gz/PyImageOp-0.1.01b/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name='PyImageOp',
+    packages=['PyImageOp'],
+    version='0.1.01b',
+    license='MIT',
+    description='Small library to help in live editing image with python, for now beta version',
+    author='allahaka',
+    author_email='allahaka90@gmail.com',
+    url='https://github.com/allahaka/PyImageOp',
+    download_url='https://github.com/allahaka/PyImageOp/archive/refs/tags/0.1.01b.tar.gz',
+    keywords=['Image', 'AI', 'ImageCapture', 'Image Filters', 'Image Recognition'],
+    install_requires=[
+        'scipy',
+        'opencv_python',
+        'numpy',
+        'pillow',
+        'mss',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
