c2d633fe6b76bea5ece3e51876f8ddbc4d5e7c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:11 +0000

    Add package teedoc/1.12.3/teedoc-1.12.3.tar.gz

---
+++ packages/teedoc/teedoc-1.12.3.tar.gz/teedoc-1.12.3/teedoc/teedoc_debug.py
@@ -0,0 +1,14 @@
+import sys, os
+
+curr_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(curr_path)
+from teedoc_main import main
+
+site_path = '/home/neucrack/data/doc/sipeed_wiki'
+sys.argv.append("--thread")
+sys.argv.append("1")
+sys.argv.append("-d")
+sys.argv.append(site_path)
+sys.argv.append("serve")
+
+main()
