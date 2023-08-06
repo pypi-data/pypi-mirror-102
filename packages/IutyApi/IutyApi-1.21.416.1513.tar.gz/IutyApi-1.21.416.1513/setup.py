96ca2087e2a0d9ea2517f195559fe25556137919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:23 +0000

    Add package IutyApi/1.21.416.1513/IutyApi-1.21.416.1513.tar.gz

---
+++ packages/IutyApi/IutyApi-1.21.416.1513.tar.gz/IutyApi-1.21.416.1513/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+version = '1.21.0416.1513'
+
+entry_points={
+        'console_scripts': [
+            'ipb = IutyApi.server.webserver:main'
+        ]
+    }
+
+setup(
+    name="IutyApi",
+    version= version,
+    #version = ver,
+    packages=[
+        "IutyApi.stock",
+        "IutyApi.task",
+        "IutyApi.usr",
+        "IutyApi.dev",
+        "IutyApi.server"
+        ]
+)
