9d23efa5b52512a56c3ea199cd489eb80d849514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:22 +0000

    Add package Flask-Discord-Interactions/0.1.6/Flask-Discord-Interactions-0.1.6.tar.gz

---
+++ packages/Flask-Discord-Interactions/Flask-Discord-Interactions-0.1.6.tar.gz/Flask-Discord-Interactions-0.1.6/flask_discord_interactions/discord.py
@@ -0,0 +1,215 @@
+import time
+
+import requests
+
+from flask import current_app, request, jsonify
+
+from nacl.exceptions import BadSignatureError
+from nacl.signing import VerifyKey
+
+from .command import SlashCommand, SlashCommandGroup
+from .response import Response, ResponseType
+
+
+class InteractionType:
+    PING = 1
+    APPLICATION_COMMAND = 2
+
+
+class DiscordInteractionsBlueprint:
+    def __init__(self):
+        self.discord_commands = {}
+
+    def add_slash_command(self, command, name=None,
+                          description=None, options=None, annotations=None):
+        slash_command = SlashCommand(
+            command, name, description, options, annotations)
+        self.discord_commands[slash_command.name] = slash_command
+
+    def command(self, name=None, description=None,
+                options=None, annotations=None):
+        "Decorator to create a Slash Command"
+
+        def decorator(func):
+            nonlocal name, description, options
+            self.add_slash_command(
+                func, name, description, options, annotations)
+            return func
+
+        return decorator
+
+    def command_group(self, name, description="No description"):
+        group = SlashCommandGroup(name, description)
+        self.discord_commands[name] = group
+        return group
+
+
+class DiscordInteractions(DiscordInteractionsBlueprint):
+    def __init__(self, app=None):
+        super().__init__()
+
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config.setdefault("DISCORD_CLIENT_ID", "")
+        app.config.setdefault("DISCORD_PUBLIC_KEY", "")
+        app.config.setdefault("DISCORD_CLIENT_SECRET", "")
+        app.discord_commands = self.discord_commands
+        app.discord_token = None
+
+    def fetch_token(self, app=None):
+        if app is None:
+            app = self.app
+
+        response = requests.post(
+            "https://discord.com/api/v8/oauth2/token",
+            data={
+                "grant_type": "client_credentials",
+                "scope": "applications.commands.update"
+            },
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            auth=(
+                app.config["DISCORD_CLIENT_ID"],
+                app.config["DISCORD_CLIENT_SECRET"]
+            )
+        )
+
+        response.raise_for_status()
+        app.discord_token = response.json()
+        app.discord_token["expires_on"] = (time.time()
+                                           + app.discord_token["expires_in"]/2)
+
+    def auth_headers(self, app):
+        if (app.discord_token is None
+                or time.time() > app.discord_token["expires_on"]):
+            self.fetch_token(app)
+        return {"Authorization": f"Bearer {app.discord_token['access_token']}"}
+
+    def update_slash_commands(self, app=None, guild_id=None):
+        if app is None:
+            app = self.app
+
+        needed = app.discord_commands.copy()
+
+        if guild_id:
+            url = ("https://discord.com/api/v8/applications/"
+                   f"{app.config['DISCORD_CLIENT_ID']}/"
+                   f"guilds/{guild_id}/commands")
+        else:
+            url = ("https://discord.com/api/v8/applications/"
+                   f"{app.config['DISCORD_CLIENT_ID']}/commands")
+
+        response = requests.get(url, headers=self.auth_headers(app))
+        response.raise_for_status()
+        current = response.json()
+
+        for command in current:
+            if command["name"] in needed:
+                target = needed[command["name"]]
+                if command["description"] == target.description:
+                    if (not command.get("options") and not target.options
+                            or command.get("options") == target.options):
+                        del needed[command["name"]]
+
+                        target.id = command["id"]
+                        continue
+
+            id = command["id"]
+            if guild_id:
+                delete_url = ("https://discord.com/api/v8/applications/"
+                              f"{app.config['DISCORD_CLIENT_ID']}/"
+                              f"guilds/{guild_id}/commands/{id}")
+            else:
+                delete_url = (
+                    "https://discord.com/api/v8/applications/"
+                    f"{app.config['DISCORD_CLIENT_ID']}/commands/{id}")
+
+            response = requests.delete(
+                delete_url, headers=self.auth_headers(app))
+            response.raise_for_status()
+
+        for name, command in needed.items():
+            response = requests.post(
+                url, json=command.dump(), headers=self.auth_headers(app))
+            self._throttle(response)
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError:
+                raise ValueError(
+                    f"Unable to register command {command.name}\n"
+                    f"{response.status_code} {response.text}")
+            command.id = response.json()["id"]
+
+    def _throttle(self, response):
+        """
+        throttle the number of posts made
+        see discord rate limits here
+        https://discord.com/developers/docs/topics/rate-limits
+        Args:
+            response : requests response object
+        """
+
+        rate_limit_remaining = int(response.headers["X-RateLimit-Remaining"])
+        rate_limit_reset = float(response.headers["X-RateLimit-Reset"])
+        # rate_limit_limit = response.headers["X-RateLimit-Limit"]
+        # rate_limit_bucket = response.headers["X-RateLimit-Bucket"]
+
+        if not rate_limit_remaining:
+            time.sleep(rate_limit_reset - time.time())
+
+    def register_blueprint(self, blueprint, app=None):
+        if app is None:
+            app = self.app
+
+        app.discord_commands.update(blueprint.discord_commands)
+
+    def verify_signature(self, data, signature, timestamp):
+        message = timestamp.encode() + data
+        verify_key = VerifyKey(
+            bytes.fromhex(current_app.config["DISCORD_PUBLIC_KEY"]))
+        try:
+            verify_key.verify(message, bytes.fromhex(signature))
+        except BadSignatureError:
+            return False
+        else:
+            return True
+
+    def run_command(self, data):
+        command_name = data["data"]["name"]
+
+        slash_command = current_app.discord_commands.get(command_name)
+
+        if slash_command is None:
+            raise ValueError(f"Invalid command name: {slash_command}")
+
+        return slash_command.make_context_and_run(self, current_app, data)
+
+    def set_route(self, route, app=None):
+        if app is None:
+            app = self.app
+
+        @app.route(route, methods=["POST"])
+        def interactions():
+            signature = request.headers.get('X-Signature-Ed25519')
+            timestamp = request.headers.get('X-Signature-Timestamp')
+
+            if (signature is None or timestamp is None
+                    or not self.verify_signature(
+                    request.data, signature, timestamp)):
+                return "Bad Request Signature", 401
+
+            if (request.json
+                    and request.json.get("type") == InteractionType.PING):
+                return jsonify({
+                    "type": ResponseType.PONG
+                })
+
+            result = self.run_command(request.json)
+
+            response = Response.from_return_value(result)
+
+            return jsonify(response.dump())
---
+++ packages/Flask-Discord-Interactions/Flask-Discord-Interactions-0.1.6.tar.gz/Flask-Discord-Interactions-0.1.6/flask_discord_interactions/response.py
@@ -0,0 +1,88 @@
+import json
+
+
+class ResponseType:
+    PONG = 1
+    CHANNEL_MESSAGE_WITH_SOURCE = 4
+    DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
+
+
+class Response:
+    def __init__(self, content=None, *, tts=False, embed=None, embeds=None,
+                 allowed_mentions={"parse": ["roles", "users", "everyone"]},
+                 deferred=False, ephemeral=False, file=None, files=None):
+        self.content = content
+        self.tts = tts
+
+        if embed is not None and embeds is not None:
+            raise ValueError("Specify only one of embed or embeds")
+        if embed is not None:
+            embeds = [embed]
+        self.embeds = embeds
+
+        if file is not None and files is not None:
+            raise ValueError("Specify only one of file or files")
+        if file is not None:
+            files = [file]
+        self.files = files
+
+        self.allowed_mentions = allowed_mentions
+
+        if deferred:
+            self.response_type = \
+                ResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
+        else:
+            self.response_type = \
+                ResponseType.CHANNEL_MESSAGE_WITH_SOURCE
+
+        self.flags = 64 if ephemeral else 0
+
+        if (self.content is None and self.embeds is None
+                and self.files is None and not deferred):
+            raise ValueError(
+                "Supply at least one of content, embeds, files, or deferred.")
+
+        if ephemeral and (self.embeds is not None or self.files is not None):
+            raise ValueError(
+                "Ephemeral responses cannot include embeds or files.")
+
+    @staticmethod
+    def from_return_value(result):
+        if result is None:
+            return Response()
+        elif isinstance(result, Response):
+            return result
+        else:
+            return Response(str(result))
+
+    def dump(self):
+        return {
+            "type": self.response_type,
+            "data": {
+                "content": self.content,
+                "tts": self.tts,
+                "embeds": self.embeds,
+                "allowed_mentions": self.allowed_mentions,
+                "flags": self.flags
+            }
+        }
+
+    def dump_followup(self):
+        return {
+            "content": self.content,
+            "tts": self.tts,
+            "embeds": self.embeds,
+            "allowed_mentions": self.allowed_mentions
+        }
+
+    def dump_multipart(self):
+        if self.files:
+            payload_json = json.dumps(self.dump_followup())
+
+            multipart = []
+            for file in self.files:
+                multipart.append(("file", file))
+
+            return {"data": {"payload_json": payload_json}, "files": multipart}
+        else:
+            return {"json": self.dump_followup()}
