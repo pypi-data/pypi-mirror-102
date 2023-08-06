b6fde58b394fc32b88251b84f9c7fa091f1bf755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:14 +0000

    Add package defi/1.0.13/defi-1.0.13.tar.gz

---
+++ packages/defi/defi-1.0.13.tar.gz/defi-1.0.13/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(name="defi",
+	packages=find_packages() ,
+	version="1.0.13", 
+	descrription="Tools for use in DeFi", 
+	long_description = "file: README.md",
+	long_description_content_type = "text/markdown",
+	author="Juan Pablo Pisano",
+	author_email="jpp.programacion@gmail.com", 
+	url="https://github.com/gauss314/defi",
+	keywords="defi, impermanent loss, finance, cryptos, bitcoin, liquidity pool, farming, bsc, eth, terra, heco, blockchain " ,
+	classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent"],
+	python_requires=">=3.6",
+	install_requires=["pandas","matplotlib", "datetime","requests","scipy","numpy"])
