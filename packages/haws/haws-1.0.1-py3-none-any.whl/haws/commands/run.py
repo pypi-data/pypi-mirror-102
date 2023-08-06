5c36149bc46604c0ed33b1404404f8cd664d2e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:08 +0000

    Add package haws/1.0.1/haws-1.0.1-py3-none-any.whl

---
+++ packages/haws/haws-1.0.1-py3-none-any.whl/haws/commands/run.py
@@ -0,0 +1,39 @@
+import click
+from haws.services.aws.policy_check import *
+from haws.services.aws.organization_check import *
+from haws.services.aws.cost_allocation_tags import *
+from haws.exceptions.authentication import *
+from haws.services.setup_helper import setup_cli
+from rich.prompt import Confirm
+import sys
+from pathlib import Path
+import os
+from os import path
+from haws.main import logger, runtime
+from haws.services.lx_api_connector import overwrite_scan_config
+
+
+@click.command()
+@click.option('--save-runtime', is_flag=True, default=False, help="whether to save credentials entered after run")
+@click.option('--write-config', is_flag=True, default=False, help="whether to overwrite the scan config to the workspace")
+def cli(save_runtime, write_config):
+    try:
+        run_policy_check()
+        payload = run_org_check()
+        
+        
+    except AccessDenied:
+        pass
+
+    finally:
+        run_cost_tag_check()
+
+        if write_config:
+            overwrite_scan_config(scan_config=payload)
+
+        if not save_runtime:
+            if path.exists(runtime):
+                os.remove(runtime)
+                logger.info("[info]removed runtime config [/info]",
+                            extra={"markup": True})
+        
---
+++ packages/haws/haws-1.0.1-py3-none-any.whl/haws/exceptions/authentication.py
@@ -0,0 +1,22 @@
+class UnauthenticatedUserCredentials(Exception):
+    pass
+
+class FailedPolicyCheck(Exception):
+    pass
+
+
+class InvalidUserCredentials(Exception):
+    pass
+
+
+class GeneralAuthError(Exception):
+    pass
+
+class NoRuntimeSettings(Exception):
+    pass
+
+class MultipleRoots(Exception):
+    pass
+
+class AccessDenied(Exception):
+    pass
