35bc0e4d632a468b820a560fb39659cd1b16ebc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:44 +0000

    Add package codebots/0.5.0/codebots-0.5.0.tar.gz

---
+++ packages/codebots/codebots-0.5.0.tar.gz/codebots-0.5.0/src/codebots/__init__.py
@@ -0,0 +1,12 @@
+__author__ = """Francesco Ranaudo"""
+__email__ = 'francesco.ranaudo@gmail.com'
+__version__ = '0.5.0'
+
+import os
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, '../../'))
+DATA = os.path.abspath(os.path.join(HOME, 'data'))
+DOCS = os.path.abspath(os.path.join(HOME, 'docs'))
+TEMP = os.path.abspath(os.path.join(HOME, 'temp'))
