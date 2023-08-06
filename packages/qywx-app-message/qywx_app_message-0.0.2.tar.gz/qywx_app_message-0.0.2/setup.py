0bdea360426c1ea6d083439bf838776b2fed7253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:55 +0000

    Add package qywx-app-message/0.0.2/qywx_app_message-0.0.2.tar.gz

---
+++ packages/qywx-app-message/qywx_app_message-0.0.2.tar.gz/qywx_app_message-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='qywx_app_message',
+      version='0.0.2',
+      description='企业微信应用推送消息',
+      url='https://github.com/not-know/qywx_app_message',
+      author='zhr',
+      author_email='zhangran1033@gmail.com',
+      license='MIT',
+      packages=['qywx_app_message'])
