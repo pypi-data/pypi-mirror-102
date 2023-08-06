781bf5570bf1014f292eee9f4e2379377917d727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:25 +0000

    Add package semopy/2.1.3/semopy-2.1.3.tar.gz

---
+++ packages/semopy/semopy-2.1.3.tar.gz/semopy-2.1.3/semopy/effects/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Random Effects models."""
+from .effect_base import EffectBase
+from .effect_blank import EffectBlank
+from .effect_static import EffectStatic
+from .effect_free import EffectFree
