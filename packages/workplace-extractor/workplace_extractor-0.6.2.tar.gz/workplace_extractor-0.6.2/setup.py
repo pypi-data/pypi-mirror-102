c1733a3bb96648db903ef64309c4ec9206c68e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:31 +0000

    Add package workplace-extractor/0.6.2/workplace_extractor-0.6.2.tar.gz

---
+++ packages/workplace-extractor/workplace_extractor-0.6.2.tar.gz/workplace_extractor-0.6.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='workplace_extractor',
+    packages=['workplace_extractor', 'workplace_extractor.Nodes', 'workplace_extractor.Extractors'],
+    version='0.6.2',
+    license='MIT',
+    description='Extract posts created in a corporate Workplace by Facebook installation using the Graph API',
+    author='Denis Duarte',
+    author_email='den.duarte@gmail.com',
+    url='https://github.com/denisduarte/workplace_extractor',
+    download_url='https://github.com/denisduarte/workplace_extractor/archive/refs/tags/v_0.6.2.tar.gz',
+    keywords=['extraction', 'posts', 'workplace'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'aiohttp',
+        'asyncio'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
