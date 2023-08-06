5ad6448d1f6370dbd83c73ac0aa5a5b5f058c7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:54 +0000

    Add package bedrock-cgi/1.0.3/bedrock_cgi-1.0.3-py3-none-any.whl

---
+++ packages/bedrock-cgi/bedrock_cgi-1.0.3-py3-none-any.whl/bedrock_cgi/constant.py
@@ -0,0 +1,8 @@
+# because I disagree with the python language design
+false = False
+true = True
+
+# constants we use globally
+MIME_TYPE_JSON = "application/json"
+CHARSET = "charset"
+CHARSET_UTF8 = "utf-8"
