c84f37a9e0a0d9f4dee92b7c9ff6d3d414e04f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:06 +0000

    Add package open-graphics/0.1.37/open-graphics-0.1.37.tar.gz

---
+++ packages/open-graphics/open-graphics-0.1.37.tar.gz/open-graphics-0.1.37/graphics/function/__init__.py
@@ -0,0 +1,10 @@
+from .Classifier import Classifier, train_test_split
+from .Color import Color, get_temperature, get_hsv, get_hist
+from .FaceDetector import FaceDetector
+from .MtcnnDetector import MtcnnDetector
+from .Detection import Detection
+from .MultiLabelClassify import MultiLabelClassify
+from .OcrDetector import OcrDetector
+from .Recognizer import Recognizer
+from .SceneDetector import SceneDetector
+from .Segmentation import Segmentation
