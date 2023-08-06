7333c202550f0fbad9429f2ee545c5f1b1b1d038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:12 +0000

    Add package streamstate-utils/0.2.1/streamstate-utils-0.2.1.tar.gz

---
+++ packages/streamstate-utils/streamstate-utils-0.2.1.tar.gz/streamstate-utils-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['streamstate_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['marshmallow-dataclass>=8.4.1,<9.0.0']
+
+setup_kwargs = {
+    'name': 'streamstate-utils',
+    'version': '0.2.1',
+    'description': 'Utilities for cassandra and spark streaming specifically for streamstate',
+    'long_description': None,
+    'author': 'Daniel Stahl',
+    'author_email': 'danstahl1138@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/StreamState/streamstate-utils',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
