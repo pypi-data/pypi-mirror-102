eb45c8d86890a607a8e3ae33e6a3916f99db2398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:31 +0000

    Add package simplpython/0.2/simplpython-0.2.tar.gz

---
+++ packages/simplpython/simplpython-0.2.tar.gz/simplpython-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='simplpython',
+      version='0.2',
+      description='simplpython | The simpler way of Pythoning',
+      packages=['simplpython', 'gtts', 'playsound'],
+      author_email='samit.mohnot@gmail.com',
+      zip_safe=False)
