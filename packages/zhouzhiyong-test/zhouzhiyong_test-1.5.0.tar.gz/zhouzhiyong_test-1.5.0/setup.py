7635d6ca76a101d087173ebe67bd5562e25ba87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:42 +0000

    Add package zhouzhiyong-test/1.5.0/zhouzhiyong_test-1.5.0.tar.gz

---
+++ packages/zhouzhiyong-test/zhouzhiyong_test-1.5.0.tar.gz/zhouzhiyong_test-1.5.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+                        name                    ='zhouzhiyong_test',
+                        version                 ='1.5.0',
+                        py_modules          =['zhouzhiyong_test'],
+                        author                  ='zhouzhiyong',
+                        author_email        ='756981877@qq.com',
+                        url                         ='http://user.qzone.qq.com/756981877',
+                        description             ='分层次（可选择是否开启）打印列表（包括镶嵌），并且单词首字母大写',
+                )
+    
+                        
