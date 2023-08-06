353fefbd2afabb3fb96d5c0361bd87895c183c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:03 +0000

    Add package fwdpy11/0.14.1/fwdpy11-0.14.1.tar.gz

---
+++ packages/fwdpy11/fwdpy11-0.14.1.tar.gz/fwdpy11-0.14.1/deployment/linux_wheels/build_and_audit.sh
@@ -0,0 +1,18 @@
+rm -rf dist/
+
+for py in cp36-cp36m cp37-cp37m cp38-cp38
+do
+    rm -rf build
+    PYPATH=/opt/python/${py}
+    PYBIN=${PYPATH}/bin/python 
+    ${PYBIN} -m pip install --no-cache-dir -r requirements.txt
+    PATH=${PYPATH}:$PATH ${PYBIN} setup.py build_ext -i
+    PATH=${PYPATH}:$PATH ${PYBIN} setup.py bdist_wheel
+done
+
+cd dist
+for whl in *.whl; do
+    auditwheel -v repair "$whl"
+    rm "$whl"
+done
+cd ..
