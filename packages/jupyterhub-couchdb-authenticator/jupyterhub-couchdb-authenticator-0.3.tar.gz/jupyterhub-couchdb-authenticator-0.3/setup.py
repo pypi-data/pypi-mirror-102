9284ad4d0ec08eaffcbe4d9d9270792bd623797d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:15 +0000

    Add package jupyterhub-couchdb-authenticator/0.3/jupyterhub-couchdb-authenticator-0.3.tar.gz

---
+++ packages/jupyterhub-couchdb-authenticator/jupyterhub-couchdb-authenticator-0.3.tar.gz/jupyterhub-couchdb-authenticator-0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='jupyterhub-couchdb-authenticator',
+    version='0.3',
+    description='Couchdb Authenticator for JupyterHub',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/1kastner/CouchDBAuthenticator',
+    author='Marvin Kastner',
+    author_email='marvin.kastner@tuhh.de',
+    license='MIT',
+    packages=['couchdbauthenticator'],
+    install_requires=[
+        'requests'
+    ],
+    extras_require={
+        'example': [
+            'python-dotenv'
+        ]
+    }
+)
