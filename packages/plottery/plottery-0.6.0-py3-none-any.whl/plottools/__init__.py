aa14edcbef9e25016ea717c706183a49626912c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:42 +0000

    Add package plottery/0.6.0/plottery-0.6.0-py3-none-any.whl

---
+++ packages/plottery/plottery-0.6.0-py3-none-any.whl/plottools/__init__.py
@@ -0,0 +1,12 @@
+"""
+Custom plotting utilities
+
+(c) Cristobal Sifon 2013-2017
+    sifon@astro.princeton.edu
+
+"""
+
+
+__all__ = ('astroplots', 'colormaps', 'plotutils', 'statsplots')
+__author__ = "Cristobal Sifon"
+__version__ = "0.4.6"
