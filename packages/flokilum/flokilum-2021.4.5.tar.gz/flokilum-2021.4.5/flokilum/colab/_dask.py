d64a80fd4b58f5e31af0b884d850e3e0b7820b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:31 +0000

    Add package flokilum/2021.4.5/flokilum-2021.4.5.tar.gz

---
+++ packages/flokilum/flokilum-2021.4.5.tar.gz/flokilum-2021.4.5/flokilum/colab/_dask.py
@@ -0,0 +1,16 @@
+def dask():
+
+    from packaging.version import parse
+    from importlib import import_module
+
+    from .._install_min import install_min
+
+    install_min(package="dask", pip_name="dask[complete]", version="2021.4.0")
+
+    print(F"{'dask'.rjust(50)} : {INFO}")
+
+    from dask.distributed import Client, LocalCluster
+    cluster = LocalCluster()
+    client  = Client(cluster)
+
+    return client, import_module("dask.array"), import_module("dask.dataframe"), import_module("dask.bag")
