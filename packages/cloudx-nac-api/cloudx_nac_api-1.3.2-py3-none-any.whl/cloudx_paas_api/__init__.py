c7d88592a9afe2b51a69ef363a331541a49f672e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:23 +0000

    Add package cloudx-nac-api/1.3.2/cloudx_nac_api-1.3.2-py3-none-any.whl

---
+++ packages/cloudx-nac-api/cloudx_nac_api-1.3.2-py3-none-any.whl/cloudx_paas_api/__init__.py
@@ -0,0 +1,11 @@
+'''This module implements CloudX PaaS APIs 
+* monitoring_management     Monitoring service  Management API set 
+* logging_management        Logging service Management API set
+* smart_dns_management      Smart DNS  Management API set 
+* database_management       Database Management API set
+* load_balancer_management  Load Balancer Management API set
+* service_mesh_management   Service Management API set
+* iam_management            IAM Management API Set
+* api_gw_management         API Gateway Managament API set
+* backup_restore_managemnt  Backup/Restore Management API set
+'''
