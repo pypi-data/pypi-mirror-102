aaf37215844de60b2515af8bfa0d34d5c595c11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:34 +0000

    Add package simple-imaging/0.1.1/simple_imaging-0.1.1-py2.py3-none-any.whl

---
+++ packages/simple-imaging/simple_imaging-0.1.1-py2.py3-none-any.whl/simple_imaging/errors.py
@@ -0,0 +1,22 @@
+class ValidationError(Exception):
+    pass
+
+
+class InvalidFileError(Exception):
+    pass
+
+
+class InvalidConfigsError(Exception):
+    pass
+
+
+class InvalidHeaderError(Exception):
+    pass
+
+
+class ImcompatibleImages(Exception):
+    pass
+
+
+class UnkownError(Exception):
+    pass
