bb025e6cea16fa3dbc2e18298c57d6f2d173af51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package elapid/0.1.1/elapid-0.1.1-py3-none-any.whl

---
+++ packages/elapid/elapid-0.1.1-py3-none-any.whl/elapid/models.py
@@ -0,0 +1,186 @@
+"""Model classes for species distribution modeling"""
+
+import numpy as np
+import pandas as pd
+from glmnet.logistic import LogitNet
+
+from elapid import features as _features
+from elapid.utils import MAXENT_DEFAULTS, _ncpus
+
+
+class MaxentModel(object):
+    def __init__(
+        self,
+        feature_types=MAXENT_DEFAULTS["feature_types"],
+        tau=MAXENT_DEFAULTS["tau"],
+        clamp=MAXENT_DEFAULTS["clamp"],
+        scorer=MAXENT_DEFAULTS["scorer"],
+        beta_multiplier=MAXENT_DEFAULTS["beta_multiplier"],
+        beta_lqp=MAXENT_DEFAULTS["beta_lqp"],
+        beta_hinge=MAXENT_DEFAULTS["beta_hinge"],
+        beta_threshold=MAXENT_DEFAULTS["beta_lqp"],
+        beta_categorical=MAXENT_DEFAULTS["beta_categorical"],
+        n_hinge_features=MAXENT_DEFAULTS["n_hinge_features"],
+        n_threshold_features=MAXENT_DEFAULTS["n_threshold_features"],
+        convergence_tolerance=MAXENT_DEFAULTS["tolerance"],
+        use_lambdas=MAXENT_DEFAULTS["use_lambdas"],
+        n_cpus=_ncpus,
+    ):
+        """
+        Creates a model estimator for Maxent-style species distribution models.
+
+        :param feature_types: list or string of maxent feature types to fit. must be in ["linear", "quadratic", "product", "hinge", "threshold", "auto"] or string "lqphta"
+        :param tau: float of the maxent tau (prevalence) value for scaling logistic output
+        :param clamp: bool of whether to clamp features during inference
+        :param scorer: the sklearn scoring function for model training
+        :param beta_multiplier: scalar for all regularization parameters, where higher values exclude more features
+        :param beta_lqp: scalar for linear, quadratic and product feature regularization parameters
+        :param beta_hinge: scalar for hinge feature regularization parameters
+        :param beta_threshold: scalar for threshold feature regularization parameters
+        :param beta_categorical: scalar for categorical feature regularization parameters
+        :param convergence_tolerance: scalar for the model convergence tolerance level
+        :param use_lambdas: guide for which model lambdas to select, from options ["best", "last"]
+        :param n_cpus: integer number of cpu threads to use during model training
+        :returns: none
+        """
+        self.feature_types_ = _features.validate_feature_types(feature_types)
+        self.tau_ = tau
+        self.clamp_ = clamp
+        self.convergence_tolerance_ = convergence_tolerance
+        self.beta_multiplier_ = beta_multiplier
+        self.beta_hinge_ = beta_hinge
+        self.beta_lqp_ = beta_lqp
+        self.beta_threshold_ = beta_threshold
+        self.beta_categorical_ = beta_categorical
+        self.n_hinge_features_ = n_hinge_features
+        self.n_threshold_features_ = n_threshold_features
+        self.n_cpus_ = n_cpus
+        self.scorer_ = scorer
+        self.use_lambdas_ = use_lambdas
+
+        self.initialized_ = False
+        self.beta_scores_ = None
+        self.entropy_ = 0.0
+        self.alpha_ = 0.0
+        self.estimator = None
+        self.transformer = None
+
+    def fit(self, x, y, categorical=None, labels=None, is_features=False):
+        """
+        Trains a maxent model using a set of covariates and presence/background points.
+
+        :param x: array-like of shape (n_samples, n_features) with covariate data
+        :param y: array-like of shape (n_samples,) with binary presence/background (1/0) values
+        :param categorical: either a 2D a array-like akin to "x", or a 1d array-like of column indices indicating which columns are categorical
+        :param labels: covariate labels. Ignored if x is a pandas dataframe.
+        :param is_features: boolean to specify that the x data has already been transformed from covariates to features
+        :returns: none. updates the model object
+        """
+        # data pre-processing
+        if is_features:
+            features = x
+        else:
+            self.transformer = _features.MaxentFeatureTransformer(
+                feature_types=self.feature_types_,
+                clamp=self.clamp_,
+                n_hinge_features=self.n_hinge_features_,
+                n_threshold_features=self.n_threshold_features_,
+            )
+            features = self.transformer.fit_transform(x, categorical=categorical, labels=labels)
+
+        weights = _features.compute_weights(y)
+        regularization = _features.compute_regularization(features, y)
+        lambdas = _features.compute_lambdas(y, weights, regularization)
+
+        # model fitting
+        self.initialize_model(lambdas=lambdas)
+
+        self.estimator.fit(
+            features,
+            y,
+            sample_weight=weights,
+            relative_penalties=regularization,
+        )
+
+        if self.use_lambdas_ == "last":
+            self.beta_scores_ = self.estimator.coef_path_[0, :, -1]
+        elif self.use_lambdas_ == "best":
+            self.beta_scores_ = self.estimator.coef_path_[0, :, self.estimator.lambda_best_inx_]
+
+        # maxent specific transformations
+        rr = self.predict(features[y == 0], transform="exponential", is_features=True)
+        raw = rr / np.sum(rr)
+        self.entropy_ = -np.sum(raw * np.log(raw))
+        self.alpha_ = -np.log(np.sum(rr))
+
+    def predict(self, x, transform="logistic", is_features=False):
+        """
+        Applies a maxent model to a set of covariates or features. Requires that a model has been fit.
+
+        :param x: array-like of shape (n_samples, n_features) with covariate data
+        :param transform: the maxent model transformation type. Select from ["raw", "exponential", "logistic", "cloglog"].
+        :param is_features: boolean to specify that the x data has already been transformed from covariates to features
+        :returns predictions: array-like of shape (n_samples,) with model predictions
+        """
+        assert self.initialized_, "Model must be fit first"
+
+        # feature transformations
+        if is_features:
+            features = x
+        else:
+            features = self.transformer.transform(x)
+
+        # apply the model
+        link = np.matmul(features, self.beta_scores_) + self.alpha_
+
+        if transform == "raw":
+            return link
+        elif transform == "exponential":
+            return np.exp(link)
+        elif transform == "logistic":
+            return 1 / (1 + np.exp(-self.entropy_ - link))
+        elif transform == "cloglog":
+            return 1 - np.exp(0 - np.exp(self.entropy_ + link))
+
+    def fit_predict(self, x, y, categorical=None, labels=None, transform="logistic", is_features=False):
+        """
+        :param x: array-like of shape (n_samples, n_features) with covariate data
+        :param y: array-like of shape (n_samples,) with binary presence/background (1/0) values
+        :param categorical: either a 2D a array-like akin to "x", or a 1d array-like of column indices indicating which columns are categorical
+        :param labels: covariate labels. Ignored if x is a pandas dataframe.
+        :param transform: the maxent model transformation type. Select from ["raw", "exponential", "logistic", "cloglog"].
+        :param is_features: boolean to specify that the x data has already been transformed from covariates to features
+        :returns predictions: array-like of shape (n_samples,) with model predictions
+        """
+        self.fit(x, y, categorical=categorical, labels=labels)
+        predictions = self.predict(x, transform=transform, is_features=is_features)
+
+        return predictions
+
+    def initialize_model(
+        self,
+        lambdas,
+        alpha=1,
+        standardize=False,
+        fit_intercept=True,
+    ):
+        """
+        Creates the Logistic Regression with elastic net penalty model object.
+
+        :param lambdas: the lambda values for the model. Get from features.compute_lambdas()
+        :param alpha: the elasticnet mixing parameter. alpha=1 for lasso, alpha=0 for ridge
+        :param standardize: boolean flag to specify coefficient normalization
+        :param fit_intercept: boolean flag to include an intercept parameter
+        :return none: updates the self.estimator with an sklearn model estimator
+        """
+        self.estimator = LogitNet(
+            alpha=alpha,
+            lambda_path=lambdas,
+            standardize=standardize,
+            fit_intercept=fit_intercept,
+            scoring=self.scorer_,
+            n_jobs=self.n_cpus_,
+            tol=self.convergence_tolerance_,
+        )
+
+        self.initialized_ = True
---
+++ packages/elapid/elapid-0.1.1-py3-none-any.whl/elapid/utils.py
@@ -0,0 +1,201 @@
+"""Backend helper functions that don't need to be exposed to users"""
+
+import gzip
+import multiprocessing as mp
+import os
+import pickle
+import sys
+
+import numpy as np
+import pandas as pd
+import rasterio as rio
+
+_ncpus = mp.cpu_count()
+
+MAXENT_DEFAULTS = {
+    "clamp": True,
+    "beta_multiplier": 1.0,
+    "beta_hinge": 1.0,
+    "beta_lqp": 1.0,
+    "beta_threshold": 1.0,
+    "beta_categorical": 1.0,
+    "feature_types": ["linear", "hinge", "product"],
+    "n_hinge_features": 50,
+    "n_threshold_features": 50,
+    "scorer": "roc_auc",
+    "tau": 0.5,
+    "tolerance": 1e-7,
+    "use_lambdas": "last",
+}
+
+
+def repeat_array(x, length=1, axis=0):
+    """
+    Repeats a 1D numpy array along an axis to an arbitrary length
+
+    :param x: the n-dimensional array to repeat
+    :param length: the number of times to repeat the array
+    :param axis: the axis along which to repeat the array (valid values include 0 to n+1)
+    :returns: an n+1 dimensional numpy array
+    """
+    return np.expand_dims(x, axis=axis).repeat(length, axis=axis)
+
+
+def load_sample_data(name="bradypus"):
+    """
+    Loads example species presence/background and covariate data.
+
+    :param name: the sample dataset to load. options currently include ["bradypus"], from the R 'maxnet' package
+    :returns: (x, y), a tuple of dataframes containing covariate and response data, respectively
+    """
+    assert name.lower() in ["bradypus"], "Invalid sample data requested"
+
+    package_path = os.path.realpath(__file__)
+    package_dir = os.path.dirname(package_path)
+
+    if name.lower() == "bradypus":
+
+        file_path = os.path.join(package_dir, "data", "bradypus.csv.gz")
+        df = pd.read_csv(file_path, compression="gzip").astype("int64")
+        y = df["presence"].astype("int8")
+        x = df[df.columns[1:]].astype({"ecoreg": "category"})
+        return x, y
+
+
+def save_object(obj, path, compress=True):
+    """
+    Writes a python object to disk for later access.
+
+    :param obj: a python object to be saved (e.g., a MaxentModel() instance)
+    :param path: the output file path
+    :returns: none
+    """
+    obj = pickle.dumps(obj)
+
+    if compress:
+        obj = gzip.compress(obj)
+
+    with open(path, "wb") as f:
+        f.write(obj)
+
+
+def load_object(path, compressed=True):
+    """
+    Reads a python object into memory that's been saved to disk.
+
+    :param path: the file path of the object to load
+    :param compressed: flag to specify whether the file was compressed prior to saving
+    :returns: obj, the python object that has been saved (e.g., a MaxentModel() instance)
+    """
+    with open(path, "rb") as f:
+        obj = f.read()
+
+    if compressed:
+        obj = gzip.decompress(obj)
+
+    return pickle.loads(obj)
+
+
+def create_output_raster_profile(
+    raster_paths, template_idx, windowed=True, nodata=None, compress=None, driver="GTiff", bigtiff=True, dtype="float32"
+):
+    """
+    Gets parameters for windowed reading/writing to output rasters.
+
+    :param raster_paths: a list of raster paths of covariates to apply the model to
+    :param template_idx: the index of the raster file to use as a template. template_idx=0 sets the first raster as template
+    :param windowed: bool to perform a block-by-block data read. slower, but reduces memory use.
+    :param nodata: the output nodata value to set
+    :param output_driver: the output raster file format (from rasterio.drivers.raster_driver_extensions())
+    :param compress: str of the compression type to apply to the output file
+    :param bigtiff: bool of whether to specify the output file as a bigtiff (for rasters > 2GB)
+    :returns: windows, profile, an iterable and a dictionary for the window reads and the raster profile
+    """
+    with rio.open(raster_paths[template_idx]) as src:
+        if windowed:
+            windows = src.block_windows()
+        else:
+            idx = (0, 0)
+            window = rio.windows.Window(0, 0, src.width, src.height)
+            windows = iter([(idx, window)])
+
+        dst_profile = src.profile
+        dst_profile.update(
+            count=1,
+            dtype=dtype,
+            nodata=nodata,
+            compress=compress,
+            driver=driver,
+        )
+        if bigtiff and driver == "GTiff":
+            dst_profile.update(BIGTIFF="YES")
+
+    return windows, dst_profile
+
+
+def get_raster_band_indexes(raster_paths):
+    """
+    Gets the band indexes of multiple raster bands to handle indexing multi-source and multi-band covariates.
+
+    :param raster_paths: a list of raster covariate paths
+    :returns: nbands, band_idx, int and list of the total number of bands and the 0-based start/stop band index
+    """
+    nbands = 0
+    band_idx = [0]
+    for i, raster_path in enumerate(raster_paths):
+        with rio.open(raster_path) as src:
+            nbands += src.count
+            band_idx.append(band_idx[i] + src.count)
+
+    return nbands, band_idx
+
+
+def check_raster_alignment(raster_paths):
+    """
+    Checks whether the extent, resolution and projection of multiple rasters match exactly.
+
+    :param raster_paths: a list of raster covariate paths
+    :returns: bool indicating wither they all align
+    """
+    first = raster_paths[0]
+    rest = raster_paths[1:]
+
+    with rio.open(first) as src:
+        res = src.res
+        bounds = src.bounds
+        transform = src.transform
+
+    for path in rest:
+        with rio.open(path) as src:
+            if src.res != res or src.bounds != bounds or src.transform != transform:
+                return False
+
+    return True
+
+
+def in_notebook():
+    """
+    Tests whether the module is currently running in a jupyter notebook.
+
+    :returns: bool
+    """
+    return "ipykernel" in sys.modules
+
+
+def get_tqdm():
+    """
+    Returns the appropriate tqdm progress tracking module based on the user context, as
+      behavior changes inside/outside of jupyter notebooks.
+
+    :returns: the tqdm module
+    """
+    if in_notebook():
+        from tqdm.notebook import tqdm
+    else:
+        from tqdm import tqdm
+
+    return tqdm
+
+
+class NoDataException(Exception):
+    pass
