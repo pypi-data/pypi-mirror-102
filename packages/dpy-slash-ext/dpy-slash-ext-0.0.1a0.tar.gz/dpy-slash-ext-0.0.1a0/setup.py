a27760cf821101af411ec689f727a60af77632df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:24 +0000

    Add package dpy-slash-ext/0.0.1a0/dpy-slash-ext-0.0.1a0.tar.gz

---
+++ packages/dpy-slash-ext/dpy-slash-ext-0.0.1a0.tar.gz/dpy-slash-ext-0.0.1a0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+
+
+
+
+
+
+DESCRIPTION = 'A discord.py slash commands extension'
+
+# Setting up
+setup(
+    name="dpy-slash-ext",
+    version="0.0.1a",
+    author="quiktea",
+    author_email="wishymovies@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    packages=["discord.ext.slash"],
+    install_requires=['discord.py', 'aiohttp'],
+    keywords=['python', 'discord.py', 'slash commands', 'discord', 'api', 'discord api'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    project_urls={
+        "GitHub" : "https://github.com/quiktea/slash-ext"
+    }
+)
