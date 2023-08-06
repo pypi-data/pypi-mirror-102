bc8f4c5e08c8514b049f5ecb26f516917fed45a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:06 +0000

    Add package atv-search-results-scraper/1.0.0/atv-search-results-scraper-1.0.0.tar.gz

---
+++ packages/atv-search-results-scraper/atv-search-results-scraper-1.0.0.tar.gz/atv-search-results-scraper-1.0.0/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '1.0.0'
+PACKAGE_NAME = 'atv-search-results-scraper'
+AUTHOR = 'DataKund'
+AUTHOR_EMAIL = 'datakund@gmail.com'
+URL = 'https://datakund.com'
+KEYWORDS='atv python bot_studio scraper web-scraping search-results search keyword'
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'A python library to search keyword and scrape search results data from atv.com .'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'requests','bot-studio'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      url=URL,
+      keywords = KEYWORDS
+      )
