4160d8077a7adde48e3a9b5f95e37eecf1271d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:28 +0000

    Add package binary-bits/1.0.0/binary-bits-1.0.0.tar.gz

---
+++ packages/binary-bits/binary-bits-1.0.0.tar.gz/binary-bits-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="binary-bits",
+    version="1.0.0",
+    author="S Groesz",
+    description="Provide additional methods for working with binary data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wolfpackmars2/bits",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['bits'],
+    package_dir={'': 'src'},
+    test_suite='tests',
+    extras_require={
+        'test': ['coverage'],
+    },
+    project_urls={
+        'Source': 'http://git.groesz.org/wp/bits/',
+    },
+)
