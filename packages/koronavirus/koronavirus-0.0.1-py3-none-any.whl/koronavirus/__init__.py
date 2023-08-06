5d3d8cfec50264cbc5b495a1bfff422339039be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:16 +0000

    Add package koronavirus/0.0.1/koronavirus-0.0.1-py3-none-any.whl

---
+++ packages/koronavirus/koronavirus-0.0.1-py3-none-any.whl/koronavirus/__init__.py
@@ -0,0 +1,11 @@
+from .koronavirus import *
+
+"""
+Koronavirüs (Covid-19) verilerine erişmenizi sağlayan bir Python modülü.
+"""
+
+__author__ = "Dorukyum"
+__title__ = "koronavirus"
+__copyright__ = "Copyright Dorukyum 2021"
+__version__ = "0.0.1"
+__license__ = "MIT"
