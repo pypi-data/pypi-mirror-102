37e31d63141ae2cb1bbb69443540c3de58a20941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:25 +0000

    Add package smarty-left-pad/1.0.1/smarty-left-pad-1.0.1.tar.gz

---
+++ packages/smarty-left-pad/smarty-left-pad-1.0.1.tar.gz/smarty-left-pad-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+# from distutils.core import setup
+
+setup(
+    name='smarty-left-pad',
+    version='1.0.1',
+    packages=['smarty_left_pad'],
+    url='https://github.com/nirmalchandra/smarty-left-pad',
+    license='Apache 2.0',
+    author='Nirmal Chandra',
+    author_email='nirmal.fleet@gmail.com',
+    description='A python implementation for left-pad'
+)
