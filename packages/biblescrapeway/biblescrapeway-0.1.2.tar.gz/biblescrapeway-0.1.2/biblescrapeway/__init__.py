0118d0b820de43785f87f3753405cc0f16c8e787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:28 +0000

    Add package biblescrapeway/0.1.2/biblescrapeway-0.1.2.tar.gz

---
+++ packages/biblescrapeway/biblescrapeway-0.1.2.tar.gz/biblescrapeway-0.1.2/biblescrapeway/__init__.py
@@ -0,0 +1,10 @@
+"""`biblescrapeway` tool for scraping bible verses from the web
+"""
+
+from .scraper import scrap, Verse
+from . import errors
+from . import reference
+from . import cli
+
+
+__author__ = "Jonathan D B Van Schenck"
