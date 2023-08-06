de66f9819c07a60c947363a465681b1185597173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:31 +0000

    Add package perceptilabs/0.11.15/perceptilabs-0.11.15-cp36-cp36m-win_amd64.whl

---
+++ packages/perceptilabs/perceptilabs-0.11.15-cp36-cp36m-win_amd64.whl/perceptilabs/trainer/stats/__init__.py
@@ -0,0 +1,5 @@
+from perceptilabs.trainer.stats.sample import SampleStatsTracker, SampleStats
+from perceptilabs.trainer.stats.gradients import GradientStatsTracker, GradientStats
+from perceptilabs.trainer.stats.categorical import CategoricalOutputStatsTracker, CategoricalOutputStats
+from perceptilabs.trainer.stats.image import ImageOutputStatsTracker, ImageOutputStats
+
