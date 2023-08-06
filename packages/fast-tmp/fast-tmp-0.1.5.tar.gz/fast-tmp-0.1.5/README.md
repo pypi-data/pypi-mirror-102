4c9e0c26c6a94d98487365c5efaafc36868f43b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:02 +0000

    Add package fast-tmp/0.1.5/fast-tmp-0.1.5.tar.gz

---
+++ packages/fast-tmp/fast-tmp-0.1.5.tar.gz/fast-tmp-0.1.5/README.md
@@ -0,0 +1,3 @@
+# fast-tmp
+通过脚本的方式创建项目模板
+tortoise+fastapi
---
+++ packages/fast-tmp/fast-tmp-0.1.5.tar.gz/fast-tmp-0.1.5/fast_tmp/conf/__init__.py
@@ -0,0 +1,39 @@
+import importlib
+import logging
+import os
+
+from ..utils.db import init_model
+from . import global_settings
+
+logger = logging.Logger(__name__)
+FASTAPI_VARIABLE = "FASTAPI_SETTINGS_MODULE"
+
+
+class Settings:
+    def __init__(self):
+        settings_module = os.environ.get(FASTAPI_VARIABLE)
+        if not settings_module:
+            project_slug = os.path.split(os.getcwd())[1]
+            settings_module = project_slug + ".settings"
+        for setting in dir(global_settings):
+            if setting.isupper():
+                setattr(self, setting, getattr(global_settings, setting))
+        self.SETTINGS_MODULE = settings_module
+        try:
+            mod = importlib.import_module(self.SETTINGS_MODULE)
+        except Exception as e:
+            raise ImportError(f"导入settings报错:{e}")
+
+        for setting in dir(mod):
+            if setting.isupper():
+                setting_value = getattr(mod, setting)
+                setattr(self, setting, setting_value)
+        if not getattr(self, "SECRET_KEY"):
+            raise AttributeError("SECRET_KEY不能为空")
+        if not getattr(self, "TORTOISE_ORM"):
+            logger.warning("TORTOISE_ORM为空")
+        else:
+            init_model(self)
+
+
+settings = Settings()
