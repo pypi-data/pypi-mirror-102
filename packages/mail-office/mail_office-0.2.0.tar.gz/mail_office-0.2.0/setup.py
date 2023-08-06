5850a91f93a77667a529bca266fee41a1c7779c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:46 +0000

    Add package mail-office/0.2.0/mail_office-0.2.0.tar.gz

---
+++ packages/mail-office/mail_office-0.2.0.tar.gz/mail_office-0.2.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['mail_office']
+install_requires = \
+['Django>=3.2,<4.0']
+
+setup_kwargs = {
+    'name': 'mail-office',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'mikki',
+    'author_email': 'mikki@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
