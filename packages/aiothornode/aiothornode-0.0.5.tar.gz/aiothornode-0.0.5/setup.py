c241cd7380bc623a5803dcf3ebb9ff8920dc372d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:52 +0000

    Add package aiothornode/0.0.5/aiothornode-0.0.5.tar.gz

---
+++ packages/aiothornode/aiothornode-0.0.5.tar.gz/aiothornode-0.0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+setuptools.setup(
+    name='aiothornode',
+    version='0.0.5',
+    description='THORChain node connection library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tirinox/aiothornode',
+    author='Maksim Koltsov',
+    author_email='delevoper@tirinox.ru',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=parse_requirements("requirements.txt")
+)
