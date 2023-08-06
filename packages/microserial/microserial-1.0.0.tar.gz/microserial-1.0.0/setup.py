9ab4b4d2a0a04c70b1f86e1d39068e61a6faf159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:11 +0000

    Add package microserial/1.0.0/microserial-1.0.0.tar.gz

---
+++ packages/microserial/microserial-1.0.0.tar.gz/microserial-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='microserial',
+    version='1.0.0',
+    install_requires=['pyserial'],
+    author="Adam Jenca",
+    author_email='jenca.a@gjh.sk',
+    url="https://github.com/jenca-adam/microbit/microserial",
+)
