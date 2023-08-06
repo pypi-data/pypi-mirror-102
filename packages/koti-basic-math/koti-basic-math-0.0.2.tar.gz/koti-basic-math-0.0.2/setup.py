44af9099e502d1f6cb1ec8ea4d802a0a930239f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:21 +0000

    Add package koti-basic-math/0.0.2/koti-basic-math-0.0.2.tar.gz

---
+++ packages/koti-basic-math/koti-basic-math-0.0.2.tar.gz/koti-basic-math-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="koti-basic-math", # Replace with your own username
+    version="0.0.2",
+    author="Koteswararao Gummadidala",
+    author_email="gkrkoti3@gmail.com",
+    description="A small math package",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
