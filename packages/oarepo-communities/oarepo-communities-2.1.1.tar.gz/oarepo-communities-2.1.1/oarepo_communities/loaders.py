19f082567111d465a5b994e7e1fae6334c798e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:21 +0000

    Add package oarepo-communities/2.1.1/oarepo-communities-2.1.1.tar.gz

---
+++ packages/oarepo-communities/oarepo-communities-2.1.1.tar.gz/oarepo-communities-2.1.1/oarepo_communities/loaders.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CESNET.
+#
+# OARepo-Communities is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""OArepo module that adds support for communities"""
+from flask import request
+from werkzeug.exceptions import BadRequest
+
+from oarepo_communities.proxies import current_oarepo_communities
+
+
+def community_json_loader():
+    data = request.get_json(force=True)
+    rcomid = request.view_args['community_id']
+    dcomid = current_oarepo_communities.get_primary_community_field(data)
+    if dcomid:
+        if rcomid != dcomid:
+            raise BadRequest('Primary Community mismatch')
+    else:
+        current_oarepo_communities.set_primary_community_field(data, rcomid)
+
+    return data
