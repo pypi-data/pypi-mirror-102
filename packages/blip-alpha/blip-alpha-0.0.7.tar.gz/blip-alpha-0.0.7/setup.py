22d27dfeddef08563b2b719cadeae7f65198dda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:09 +0000

    Add package blip-alpha/0.0.7/blip-alpha-0.0.7.tar.gz

---
+++ packages/blip-alpha/blip-alpha-0.0.7.tar.gz/blip-alpha-0.0.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from setuptools import find_packages
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+    
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='blip-alpha',
+    version='0.0.7',
+    author='Sharan Banagiri',
+    author_email='banag002@umn.edu',
+    description='A bayesian pipeline for detecting stochastic backgrounds with LISA.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(where='runblip'),
+    package_dir={'':'runblip'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'blip = runblip.blip:main'
+        ]
+    },
+    install_requires=requirements
+)
+
+if __name__ == "__main__":
+    print(find_packages())
