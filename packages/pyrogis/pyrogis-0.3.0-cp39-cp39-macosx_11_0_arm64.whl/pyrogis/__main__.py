9b6f32bb83d69d1d543bd15b2db4af5411b019cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:08 +0000

    Add package pyrogis/0.3.0/pyrogis-0.3.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/pyrogis/pyrogis-0.3.0-cp39-cp39-macosx_11_0_arm64.whl/pyrogis/__main__.py
@@ -0,0 +1,32 @@
+import multiprocessing as mp
+import sys
+from typing import Callable
+
+from .kitchen import Chef, Server, Kitchen
+from .restaurant import Restaurant
+
+
+def run(args=None, report_callback: Callable = None):
+    if args is None:
+        args = sys.argv[1:]
+
+    server = Server(report_callback)
+
+    mp.set_start_method('spawn', force=True)
+    kitchen = Kitchen(Chef())
+
+    server.take_order(
+        args,
+        kitchen
+    )
+
+
+def main():
+    """cli program"""
+
+    restaurant = Restaurant()
+    sys.exit(restaurant.open(run))
+
+
+if __name__ == '__main__':
+    main()
