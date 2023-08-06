04e347af4c6c9842163a9ad4d4ab01cdecd611f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:47 +0000

    Add package turbofunc/1.4/turbofunc-1.4.tar.gz

---
+++ packages/turbofunc/turbofunc-1.4.tar.gz/turbofunc-1.4/turbofunc/__init__.py
@@ -0,0 +1,35 @@
+from .clearscreen import *
+from .pressanykey import *
+from .standtextout import *
+from .multiprint import *
+"""
+FUNCTIONS
+    clearScreen()
+        Credit:
+        https://www.sololearn.com/Discuss/764024/how-to-clear-the-screen-in-python-terminal
+
+    clearScreenOld()
+        This will NOT work on windows unless you have colorama package. Tries three times to clear the screen.
+
+    clearScreenV1()
+        Harder to understand but not many lines (can be shortened to two by putting imports on the same line, and removing the del statement). Doesn't work if the user doesn't have `clear' or `cls' installed.
+
+    standTextOut(string, printMechanismDash=<built-in function print>, printMechanismString=<built-in function print>)
+        param string: the string to sandwich in between the dashes.
+        param printMechanismDash: how it will output the dashes. e.g. do `logging.info' to output it with logging.info. Defaults to print.
+            ***IF YOU CHOOSE A PRINT MECHANISM IT NEEDS TO BE IMPORTED IN YOUR ORIGINAL PROGRAM, **NOT** THIS MODULE! How does it work?! you pass the function of output and it uses it.
+        param printMechanismString: how it will output the string that is sandwidched in between the dashes. Defaults to print.
+            ***READ THE ABOVE IMPORTANT NOTICE (of printMechanismDash)!!!***
+
+    standTextOut_Return(string)
+        Will return the finished string so you can output it the way you want.
+pressanykey(string='Press any key to continue...', verbose=True)
+        SOURCE: https://raw.githubusercontent.com/TheTechRobo/python-text-calculator/master/FOR%20CLEARING%20THE%20SCREEN%20AND%20PRESS%20ANY%20KEY%20TO%20CONTINUE.md
+    multiprint(stuffToPrint, _=None, **kwargs)
+        Accepts a dictionary. With key/value pairs text/function.
+        For example, you might pass {"Welcome to ": cprint.info, "Palc": cprint.ok, "!" + MANYSPACE: cprint.info}
+        which will run cprint.info("Welcome to ");cprint.ok("Palc");cprint.info("!" + MANYSPACE)
+        Setting gettext to True will run whateverFunctionYouPassed(_(item), *args, **kwargs) instead of just whateverFunctionYouPassed(item, *args, **kwargs). 
+        The **kwargs is given to EVERY function. Useful as an "end=True" or similar.
+
+"""
