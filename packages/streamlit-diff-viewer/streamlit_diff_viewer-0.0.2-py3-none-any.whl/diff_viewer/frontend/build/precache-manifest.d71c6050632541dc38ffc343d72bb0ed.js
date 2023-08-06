916f2c49890c790fca3546fd4b2b256ad4430e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:59 +0000

    Add package streamlit-diff-viewer/0.0.2/streamlit_diff_viewer-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-diff-viewer/streamlit_diff_viewer-0.0.2-py3-none-any.whl/diff_viewer/frontend/build/precache-manifest.d71c6050632541dc38ffc343d72bb0ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bba9f3729599b7d1e46eae77b404071",
+    "url": "./index.html"
+  },
+  {
+    "revision": "334f09b5c2518c5cb993",
+    "url": "./static/js/2.d9e47561.chunk.js"
+  },
+  {
+    "revision": "24424b1d6eb9b4aff719ea5397a60016",
+    "url": "./static/js/2.d9e47561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e8a3051b270a2627e9",
+    "url": "./static/js/main.d8ab51cc.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
