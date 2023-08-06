013183e4fefbddb8396531357c241b3170769c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:45 +0000

    Add package IutyScripts/1.21.419.1331/IutyScripts-1.21.419.1331.tar.gz

---
+++ packages/IutyScripts/IutyScripts-1.21.419.1331.tar.gz/IutyScripts-1.21.419.1331/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup,find_packages
+
+name="IutyScripts"
+version = '1.21.0419.1331'
+
+author = "Iuty"
+author_email = "dfdfggg@126.com"
+
+packages = [
+        "IutyScripts.coding",
+        "IutyScripts.file",
+    ]
+
+
+#data_files = [(r"D:/templates/","IutyScripts/templates/"),]
+#package_dir = {'templates': "IutyScripts/templates"}
+scripts = []
+install_requires = ["IutyLib"]
+
+entry_points={
+        'console_scripts': [
+            'ipb = IutyScripts.coding.publisher:main',
+            'ifs = IutyScripts.file.fileclient:main'
+        ]
+    }
+
+
+
+
+
+setup(
+    name=name,
+    version= version,
+    author = author,
+    author_email = author_email,
+    #data_files = data_files,
+    packages=packages,
+    #package_dir=package_dir,
+    entry_points=entry_points,
+    install_requires = install_requires
+)
