a3685eb0947564fcec74892a9ea733e4deabb802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:59 +0000

    Add package pykahoot/1.0.2/pykahoot-1.0.2.tar.gz

---
+++ packages/pykahoot/pykahoot-1.0.2.tar.gz/pykahoot-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pykahoot',
+    version='1.0.2',
+    packages=['pykahoot'],
+    description='A working kahoot python API.',
+    long_description='A kahoot API for python to replace the now terminated KahootPY and KahootJS-Updated'
+    )
