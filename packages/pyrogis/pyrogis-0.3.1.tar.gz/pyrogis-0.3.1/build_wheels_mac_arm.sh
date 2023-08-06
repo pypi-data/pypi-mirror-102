7c8c58f67b30127af8fdac17b8a94cc066073b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:18 +0000

    Add package pyrogis/0.3.1/pyrogis-0.3.1.tar.gz

---
+++ packages/pyrogis/pyrogis-0.3.1.tar.gz/pyrogis-0.3.1/build_wheels_mac_arm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -ex
+
+# only 3.9 on arm with pyenv
+for PYBIN in ~/.pyenv/versions/{3.8,3.9}*/bin; do
+    "${PYBIN}/pip" install -r requirements.txt
+    "${PYBIN}/python" setup.py bdist_wheel
+done
+
+# upload wheels
+~/.pyenv/versions/3.9*/bin/pip install -U awscli
+export DIST_DIR=$(~/.pyenv/versions/3.9*/bin/python setup.py -V)
+~/.pyenv/versions/3.9*/bin/python -m awscli s3 sync --exact-timestamps ./dist "s3://pierogis/dist/$DIST_DIR"
