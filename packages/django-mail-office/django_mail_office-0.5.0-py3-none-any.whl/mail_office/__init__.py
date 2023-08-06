698c5deb6709faf5f68ca0882a77f0d9ef01be1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:19 +0000

    Add package django-mail-office/0.5.0/django_mail_office-0.5.0-py3-none-any.whl

---
+++ packages/django-mail-office/django_mail_office-0.5.0-py3-none-any.whl/mail_office/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.1"
+__author__ = "Michael Aboryone"
+__email__ = "aboryone@gmail.com"
+__license__ = "MIT"
---
+++ packages/django-mail-office/django_mail_office-0.5.0-py3-none-any.whl/mail_office/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MailOfficeConfig(AppConfig):
+    name = 'mail_office'
+    default_auto_field = 'django.db.models.BigAutoField'
