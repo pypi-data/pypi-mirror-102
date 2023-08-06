5ede7b5593088db0e2fa04b1ac219c4961d17e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:22 +0000

    Add package data-purifier/0.0.9/data_purifier-0.0.9-py3-none-any.whl

---
+++ packages/data-purifier/data_purifier-0.0.9-py3-none-any.whl/datapurifier/__init__.py
@@ -0,0 +1,19 @@
+"""An Automated Exploratory Data Analysis Library For Machine Learning and Deep Learning in Python.
+
+Classes:
+    MLeda
+    Nlpeda
+    
+Methods:
+    pass
+    
+"""
+
+
+from .eda.mleda import Mleda
+from .eda.nlpeda import Nlpeda
+from .cleaning.nlpurifier import Nlpurifier
+
+# from datapurifier.cleaning.contractions import CONTRACTIONS
+
+# from .main import Purify
