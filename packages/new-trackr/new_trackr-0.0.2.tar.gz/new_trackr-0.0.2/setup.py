cd15bdc246fd5fcc4d05e448a1b831dc30dac613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:10 +0000

    Add package new-trackr/0.0.2/new_trackr-0.0.2.tar.gz

---
+++ packages/new-trackr/new_trackr-0.0.2.tar.gz/new_trackr-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="new_trackr",
+    version="0.0.2",
+    author="ewg",
+    author_email="essen.wang@outlook.com",
+    description="rewrite version of pytrackr.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iessen/jcapi.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
