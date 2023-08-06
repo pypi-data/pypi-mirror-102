027c9f661c42299eab37e3852b28200d0b7dac88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:03 +0000

    Add package constant-nums/0.3/constant_nums-0.3.tar.gz

---
+++ packages/constant-nums/constant_nums-0.3.tar.gz/constant_nums-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+with open('README.md','r') as f:
+    long_description = f.read()
+
+
+
+setup(name="constant_nums"
+,version="0.3"
+,description="A final function for your constant variables"
+,long_description = long_description
+,long_description_content_type='text/markdown'
+,author = "Ashutosh Joshi"
+,packages = ['constant_nums'],
+install_requires = [])
