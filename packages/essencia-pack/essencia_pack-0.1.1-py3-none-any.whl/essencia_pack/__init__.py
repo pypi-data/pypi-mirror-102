a5ba9b17cf9dfd4257701a5a21fff4e41c2ee1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:49 +0000

    Add package essencia-pack/0.1.1/essencia_pack-0.1.1-py3-none-any.whl

---
+++ packages/essencia-pack/essencia_pack-0.1.1-py3-none-any.whl/essencia_pack/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.1'
+from essencia_pack.licence import Licence
+from essencia_pack.gender import Gender
+from essencia_pack.calendar import Day
+from essencia_pack.calendar import Month
+from essencia_pack.places import BrazilianRegion
