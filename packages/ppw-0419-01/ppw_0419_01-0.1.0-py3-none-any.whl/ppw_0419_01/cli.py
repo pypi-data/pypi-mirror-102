4397b35275c561fb8f95dd7288ecbc70ee2674d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:52 +0000

    Add package ppw-0419-01/0.1.0/ppw_0419_01-0.1.0-py3-none-any.whl

---
+++ packages/ppw-0419-01/ppw_0419_01-0.1.0-py3-none-any.whl/ppw_0419_01/cli.py
@@ -0,0 +1,17 @@
+"""Console script for ppw_0419_01."""
+
+import fire
+
+
+def help():
+    print("ppw_0419_01")
+    print("=" * len("ppw_0419_01"))
+    print("Skeleton project created by Python Project Wizard (ppw)")
+
+
+def main():
+    fire.Fire({"help": help})
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
