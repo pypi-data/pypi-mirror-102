9477688fe426e31ff49e66175785981171f7d15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:38 +0000

    Add package pytentiostat/0.0.5/pytentiostat-0.0.5.tar.gz

---
+++ packages/pytentiostat/pytentiostat-0.0.5.tar.gz/pytentiostat-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import subprocess
+import webbrowser
+import os
+
+subprocess.call([r'scripts\deploy_docs.bat'])
+
+cwd = os.getcwd()
+url = 'file:///' + cwd + '/docs/_build/html/index.html'
+webbrowser.open(url, new=2)  # open in new tab
+
+setup(
+    name="pytentiostat",
+    version='0.0.5',
+    packages=find_packages(),
+    description="python API for JUAMI potentiostat",
+    zip_safe=False,
+    package_data={"pytentiostat": ["config/*"]},
+    include_package_data=True,
+    url="https://github.com/juami/pytentiostat",
+    scripts=["scripts/pytentiostat"],
+)
