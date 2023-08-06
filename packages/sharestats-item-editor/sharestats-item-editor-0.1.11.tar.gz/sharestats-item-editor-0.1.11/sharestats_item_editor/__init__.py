ddb65e1ac44c680ac6f9c07172718006c54dfa2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:20 +0000

    Add package sharestats-item-editor/0.1.11/sharestats-item-editor-0.1.11.tar.gz

---
+++ packages/sharestats-item-editor/sharestats-item-editor-0.1.11.tar.gz/sharestats-item-editor-0.1.11/sharestats_item_editor/__init__.py
@@ -0,0 +1,31 @@
+"""
+"""
+
+__version__ = '0.1.11'
+__author__ = 'Oliver Lindemann'
+
+DEVELOPER_VERSION = False # include preliminary features
+
+
+import sys as _sys
+from . import consts
+from .json_settings import JSONSettings as _JSONSettings
+
+PYTHON_VERSION = "{0}.{1}.{2}".format(_sys.version_info[0],
+                                      _sys.version_info[1],
+                                      _sys.version_info[2])
+
+if _sys.version_info[0] != 3 or _sys.version_info[1]<5:
+
+    raise RuntimeError("{} {} ".format(consts.APPNAME, __version__) +
+                      "is not compatible with Python {0}.{1}.".format(
+                                                    _sys.version_info[0],
+                                                    _sys.version_info[1]) +
+                      "\n\nPlease use Python 3.5 or higher.")
+
+settings = _JSONSettings(appname=consts.APPNAME.replace(" ", "_").lower(),
+                         settings_file_name="settings.json",
+                         defaults= {"recent_dirs": []})
+
+if DEVELOPER_VERSION:
+    __version__ += "-dev"
