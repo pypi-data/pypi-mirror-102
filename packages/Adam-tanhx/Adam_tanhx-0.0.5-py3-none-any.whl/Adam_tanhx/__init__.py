3e0ec4d16761d29c934c980897ed75fb168d821d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:11 +0000

    Add package Adam-tanhx/0.0.5/Adam_tanhx-0.0.5-py3-none-any.whl

---
+++ packages/Adam-tanhx/Adam_tanhx-0.0.5-py3-none-any.whl/Adam_tanhx/__init__.py
@@ -0,0 +1,9 @@
+import math
+import torch
+from torch.optim.optimizer import Optimizer
+import torch.nn.functional as F
+import numpy as np
+import torch.nn as nn
+
+from .Adam_tanhx import *
+name = "Adam_tanhx"
