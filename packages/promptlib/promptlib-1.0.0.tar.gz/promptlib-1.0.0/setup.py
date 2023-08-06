d361975faa67d29b6476745f392df923bb476490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:31 +0000

    Add package promptlib/1.0.0/promptlib-1.0.0.tar.gz

---
+++ packages/promptlib/promptlib-1.0.0.tar.gz/promptlib-1.0.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='promptlib',
+    version='1.0.0',
+    description='Prompt library',
+    long_description='Easy prompt lib',
+    packages=["promptlib"]
+    )
+
