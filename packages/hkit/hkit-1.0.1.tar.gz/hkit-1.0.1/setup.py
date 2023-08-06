d12d007159dd3eb66f5bbd5b4bcd2e5ebd784990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:42 +0000

    Add package hkit/1.0.1/hkit-1.0.1.tar.gz

---
+++ packages/hkit/hkit-1.0.1.tar.gz/hkit-1.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="hkit",
+    version="1.0.1",
+    author="huangweiwei",
+    author_email="geverway@gmail.com",
+    description="simple tools for daily use",
+    long_description="#ReadMe",
+    long_description_content_type="text/markdown",
+    url="https://404.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
