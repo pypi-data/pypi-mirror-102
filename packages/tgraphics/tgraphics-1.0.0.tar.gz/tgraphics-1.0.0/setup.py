f93984d8e021212a0d27fdd9018d7d68042f226d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:59 +0000

    Add package tgraphics/1.0.0/tgraphics-1.0.0.tar.gz

---
+++ packages/tgraphics/tgraphics-1.0.0.tar.gz/tgraphics-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    read = f.read()
+
+
+setup(
+    name='tgraphics',
+    version='1.0.0',
+    description='An omni-platform terminal graphics module',
+    long_description=read,
+    long_description_content_type='text/markdown',
+    author='Wizard',
+    author_email='nlarsen23.student@gmail.com',
+    url='https://repl.it/@GrandMoff100/tgraphics',
+    packages=['tgraphics', 'tgraphics.readchar'],  
+)
