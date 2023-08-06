15ca2bd6f12e217a23ebc129e2fe794425a73a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package kdelearn/1.0.0/kdelearn-1.0.0-py3-none-any.whl

---
+++ packages/kdelearn/kdelearn-1.0.0-py3-none-any.whl/kdelearn/__init__.py
@@ -0,0 +1,8 @@
+from .kde import Kde
+from .kernels import uniform, gaussian, epanechnikov, cauchy
+
+__all__ = ['uniform',
+           'gaussian',
+           'epanechnikov',
+           'cauchy',
+           'Kde']
