f7cceb3e742e9fcf73c7ee7ed8cd5666a41b8ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:13 +0000

    Add package xiaoxiong/0.0.1/xiaoxiong-0.0.1-py3-none-any.whl

---
+++ packages/xiaoxiong/xiaoxiong-0.0.1-py3-none-any.whl/xiaoxiongxiong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File     : __init__.py
+# @Author   : LiuYan
+# @Time     : 2021/4/16 10:05
+
+from __future__ import absolute_import
+from xiaoxiongxiong.oh_bear import *
+
+name = 'Oh Bear !'
---
+++ packages/xiaoxiong/xiaoxiong-0.0.1-py3-none-any.whl/xiaoxiongxiong/oh_bear.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File     : oh_bear
+# @Author   : LiuYan
+# @Time     : 2021/4/16 10:05
+
+def output():
+    print('小熊熊你好呀！')
