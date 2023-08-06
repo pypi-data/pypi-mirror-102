f9b81d30415092e23258d133bd151adf8f2fce02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:04 +0000

    Add package bridge-markup/2.3.0/bridge-markup-2.3.0.tar.gz

---
+++ packages/bridge-markup/bridge-markup-2.3.0.tar.gz/bridge-markup-2.3.0/src/bml/bml2latex.py
@@ -0,0 +1,12 @@
+import re
+
+from bml import bml
+from bml import latex
+
+
+def main():
+    bml.args = bml.parse_arguments(description='Convert BML to LaTeX.')
+    if not bml.args.outputfile:
+        bml.args.outputfile = '-' if bml.args.inputfile == '-' else re.sub(r'\..+\Z', '.tex', bml.args.inputfile)
+    bml.logger.debug("Output file:", bml.args.outputfile)
+    latex.bml2latex(bml.args.inputfile, bml.args.outputfile)
