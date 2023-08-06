1900ee8a9c4932568c605350e509a82e6ad17d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:04 +0000

    Add package create-pyproj/0.2.0/create-pyproj-0.2.0.tar.gz

---
+++ packages/create-pyproj/create-pyproj-0.2.0.tar.gz/create-pyproj-0.2.0/create_pyproj/code/_config/__init__.py
@@ -0,0 +1,5 @@
+from .logging import configureLogging
+from .settings import loadSettings, saveSettings, updateSettings
+from .version import getVersion
+
+__all__ = [configureLogging, loadSettings, saveSettings, updateSettings, getVersion]
---
+++ packages/create-pyproj/create-pyproj-0.2.0.tar.gz/create-pyproj-0.2.0/create_pyproj/code/_config/version.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Module Description.
+
+
+"""
+from pathlib import Path
+
+DIR = Path(__file__).parent.absolute()
+
+
+def getVersion():
+    with open(DIR.parent.parent / 'VERSION', 'r') as f:
+        VERSION = f.read().strip()
+    return VERSION
+
+
