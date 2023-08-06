c902ea2f37c7cc573314590817d2529a24fef0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:34 +0000

    Add package cloudx-nac-api/1.3.5/cloudx_nac_api-1.3.5-py3-none-any.whl

---
+++ packages/cloudx-nac-api/cloudx_nac_api-1.3.5-py3-none-any.whl/cloudx_paas_api/backup_restore_management.py
@@ -0,0 +1,10 @@
+"""
+This module allows Cloud^X Edge PaaS Bakcup and Restore Management API set
+Following functions are exposed:
+
+    create_backup_schedule  allow easy to backup_schedule
+    create_restore_task     allow easy to restore task 
+    subscribe_to_service    allow to subscribe to service
+    unsubscribe_to_service  allow to unsubscribe to service
+
+"""
---
+++ packages/cloudx-nac-api/cloudx_nac_api-1.3.5-py3-none-any.whl/cloudx_paas_api/iam_management.py
@@ -0,0 +1,12 @@
+"""
+This module allows Cloud^X Edge PaaS Identity and Access Management API set
+Following functions are exposed:
+
+    create_user             allow easy to create user account  
+    create_role             allow easy to create user roles
+    assign_role             allow easy to assign role to user
+    create_policy           allow easy to create policy
+    subscribe_to_service    allow to subscribe to service
+    unsubscribe_to_service  allow to unsubscribe to service
+
+"""
