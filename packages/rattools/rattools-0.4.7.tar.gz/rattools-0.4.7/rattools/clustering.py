dc96f5d2924d3558c4960bdc5868ac6580404e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:29 +0000

    Add package rattools/0.4.7/rattools-0.4.7.tar.gz

---
+++ packages/rattools/rattools-0.4.7.tar.gz/rattools-0.4.7/rattools/clustering.py
@@ -0,0 +1,37 @@
+def SSE_elbow(df, n_clusters=20, max_iter=500, tol=1e-04, init="random", n_init=200, algorithm='auto'):
+    from sklearn.cluster import KMeans
+    import matplotlib.pyplot as plt
+    inertia_values = []
+    for i in range(1, n_clusters+1):
+        km = KMeans(n_clusters=i, max_iter=max_iter, tol=tol, init=init, n_init=n_init, random_state=1, algorithm=algorithm)
+        km.fit_predict(df)
+        inertia_values.append(km.inertia_)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    plt.plot(range(1, n_clusters+1), inertia_values, color='red')
+    plt.xlabel('No. of Clusters', fontsize=15)
+    plt.ylabel('SSE / Inertia', fontsize=15)
+    plt.title('SSE / Inertia vs No. Of Clusters', fontsize=15)
+    plt.grid()
+    plt.show()
+    
+    
+def silhouette_scores(df, n_clusters=20, max_iter=500, tol=1e-04, init="random", n_init=200, algorithm='auto'):
+    from sklearn.cluster import KMeans
+    from sklearn.metrics import silhouette_score
+    import matplotlib.pyplot as plt
+    
+    inertia_values = []
+    
+    for i in range(2, n_clusters+1):
+        km = KMeans(n_clusters=i, max_iter=max_iter, tol=tol, init=init, n_init=n_init, random_state=1, algorithm=algorithm)
+        df_kmeans =  km.fit_predict(df)
+        inertia_values.append(silhouette_score(df, df_kmeans))
+        
+        
+    fig, ax = plt.subplots(figsize=(8, 6))
+    plt.plot(range(1, n_clusters), inertia_values, color='red')
+    plt.xlabel('No. of Clusters', fontsize=15)
+    plt.ylabel('Silhouette Score', fontsize=15)
+    plt.title('Silhouette Score vs No. Of Clusters', fontsize=15)
+    plt.grid()
+    plt.show()
