1fc99a75e3c0920a0db9d434172f9ad7e56924a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:24 +0000

    Add package fastrlapi/0.0.1/fastrlapi-0.0.1-py3-none-any.whl

---
+++ packages/fastrlapi/fastrlapi-0.0.1-py3-none-any.whl/fastrlapi/callbacks/duration_test.py
@@ -0,0 +1,15 @@
+import unittest
+from fastrlapi import agents
+from fastrlapi.callbacks import duration, log
+
+
+class DurationTest(unittest.TestCase):
+    def test_single_episode(self):
+        agent = agents.PpoAgent("CartPole-v0")
+        count=log._CallbackCounts()
+        agent.train([duration._SingleEpisode(), log._Callbacks(), count])
+        assert count.train_iteration_begin_count == 1
+
+
+if __name__ == '__main__':
+    unittest.main()
