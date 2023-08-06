b48d8caf498f90d43c75579c2cb580338430cfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:05 +0000

    Add package bergen/0.3.79/bergen-0.3.79-py3-none-any.whl

---
+++ packages/bergen/bergen-0.3.79-py3-none-any.whl/bergen/enums.py
@@ -0,0 +1,29 @@
+
+from enum import Enum
+
+class HostProtocol(str, Enum):
+    WEBSOCKET = "WEBSOCKET"
+
+class ProviderProtocol(str, Enum):
+    WEBSOCKET = "WEBSOCKET"
+
+class PostmanProtocol(str, Enum):
+    WEBSOCKET = "WEBSOCKET"
+    KAFKA = "KAFKA"
+    RABBITMQ = "RABBITMQ"
+
+class ClientType(str, Enum):
+    HOST = "HOST"
+    CLIENT = "CLIENT"
+    PROVIDER = "PROVIDER"
+    POINT = "POINT"
+
+
+class DataPointType(str, Enum):
+    GRAPHQL = "GRAPHQL"
+    REST = "REST"
+
+
+
+class TYPENAMES(str, Enum):
+    MODELPORTTYPE = "ModelPortType"
