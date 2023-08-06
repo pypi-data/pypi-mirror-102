455b9a4e59da6eb0196168d2a526a7f7f6b582dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:50 +0000

    Add package simpleicons/4.20.0/simpleicons-4.20.0-py3-none-any.whl

---
+++ packages/simpleicons/simpleicons-4.20.0-py3-none-any.whl/simpleicons/icons/farfetch.py
@@ -0,0 +1,2 @@
+from simpleicons.icon import Icon
+icon= Icon(title='Farfetch', slug='farfetch', hex='000000', source='https://www.farfetch.com/', svg="""<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Farfetch icon</title><path d="M9.635 0L4.883 4.811V24h4.752v-9.593h7.119V9.6H9.635V4.811h9.482V0Z"/></svg>""", guidelines=None, license=None)
