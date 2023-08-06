a9d8a50ae6bf0db1dc15f81009fe2711dc30b3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:14 +0000

    Add package edi-835-parser/1.3.0/edi-835-parser-1.3.0.tar.gz

---
+++ packages/edi-835-parser/edi-835-parser-1.3.0.tar.gz/edi-835-parser-1.3.0/edi_835_parser/elements/adjustment_group_code.py
@@ -0,0 +1,16 @@
+from edi_835_parser.elements import Element
+
+# https://x12.org/codes/claim-adjustment-group-codes
+adjustment_group_codes = {
+	'CR': 'corrections and reversals',
+	'OA': 'other adjustment',
+	'PR': 'patient responsibility',
+	'CO': 'contractual obligation',
+	'PI': 'payor initiated reduction',
+}
+
+
+class AdjustmentGroupCode(Element):
+
+	def parser(self, value: str) -> str:
+		return adjustment_group_codes.get(value, value)
