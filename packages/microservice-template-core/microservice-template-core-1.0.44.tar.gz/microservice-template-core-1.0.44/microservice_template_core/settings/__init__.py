c8fb1300ef352297785f43c75f9a47e738e3d08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:14 +0000

    Add package microservice-template-core/1.0.44/microservice-template-core-1.0.44.tar.gz

---
+++ packages/microservice-template-core/microservice-template-core-1.0.44.tar.gz/microservice-template-core-1.0.44/microservice_template_core/settings/__init__.py
@@ -0,0 +1,6 @@
+from .global_settings import FlaskConfig
+from .global_settings import ServiceConfig
+from .global_settings import LoggerConfig
+from .global_settings import TracerConfig
+from .global_settings import DbConfig
+from .global_settings import AerospikeConfig
