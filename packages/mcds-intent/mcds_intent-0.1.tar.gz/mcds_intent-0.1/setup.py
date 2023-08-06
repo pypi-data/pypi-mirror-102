1262c077951b4afdf722d62c583f5355f40e5d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:48 +0000

    Add package mcds-intent/0.1/mcds_intent-0.1.tar.gz

---
+++ packages/mcds-intent/mcds_intent-0.1.tar.gz/mcds_intent-0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'mcds_intent',
+  packages = ['mcds_intent'],
+  version = '0.1',
+  license='GPL',
+  description = 'mcds_intent',
+  author = 'MAILCHIMP',
+  author_email = 'info@mailchimp.com',
+  url = 'https://mailchimp.com',
+  keywords = [],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
