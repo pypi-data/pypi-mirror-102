eab73153ab94a63514e952c7e4612e370eb74201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:47 +0000

    Add package PingF/1.0.1/PingF-1.0.1.tar.gz

---
+++ packages/PingF/PingF-1.0.1.tar.gz/PingF-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PingF", # Replace with your own PyPI username(id)
+    version="1.0.1",
+    author="PingF",
+    author_email="ping01@ruu.kr",
+    description="SSHI AH !",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
