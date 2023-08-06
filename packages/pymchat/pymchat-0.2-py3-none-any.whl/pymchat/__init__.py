71d12e611cb86d4331e847c4f8812fb6e78823a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:01 +0000

    Add package pymchat/0.2/pymchat-0.2-py3-none-any.whl

---
+++ packages/pymchat/pymchat-0.2-py3-none-any.whl/pymchat/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+# Created at 30/03/2021
+__license__ = "GNU General Public License v3.0"
+__author__ = "Alexandre Silva // MrKelpy"
+__copyright__ = "© Alexandre Silva 2021"
---
+++ packages/pymchat/pymchat-0.2-py3-none-any.whl/pymchat/message.py
@@ -0,0 +1,8 @@
+class Message:
+
+    def __init__(self, *args):
+
+        self.content = args[0]
+        self.id = args[1]
+        self.date = args[2]
+
