9acb135d48263901dd003b737f369af1be8670cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:07 +0000

    Add package machin/0.4.1/machin-0.4.1-py3-none-any.whl

---
+++ packages/machin/machin-0.4.1-py3-none-any.whl/machin/auto/__init__.py
@@ -0,0 +1,8 @@
+from . import envs
+from . import config
+from . import dataset
+from . import launcher
+from . import pl_logger
+from . import pl_plugin
+
+__all__ = ["env", "config", "dataset", "launcher", "pl_logger", "pl_plugin"]
