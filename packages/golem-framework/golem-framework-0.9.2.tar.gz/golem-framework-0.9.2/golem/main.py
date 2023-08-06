5a983e587140609084cb025af28f68037d72b965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package golem-framework/0.9.2/golem-framework-0.9.2.tar.gz

---
+++ packages/golem-framework/golem-framework-0.9.2.tar.gz/golem-framework-0.9.2/golem/main.py
@@ -0,0 +1,14 @@
+"""Main point of entrance to the application"""
+import sys
+
+from .cli import argument_parser, commands
+
+
+def execute_from_command_line(testdir):
+    # deactivate .pyc extention file generation
+    sys.dont_write_bytecode = True
+    sys.path.insert(0, '')
+
+    args = argument_parser.get_parser().parse_args()
+
+    commands.command_dispatcher(args, testdir)
