5184c45e5291c3a43bfde5669200467dd872363c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:38 +0000

    Add package extpy/0.0.9/extpy-0.0.9.tar.gz

---
+++ packages/extpy/extpy-0.0.9.tar.gz/extpy-0.0.9/extpy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+# name = 'expy'
+
+from logging import CRITICAL, DEBUG, ERROR, INFO, WARNING
+
+from expy.Classes import Ithread, IthreadBoundedSemaphore
+from expy.Methods import genChromeHeaders, genLogger, genQRCode
