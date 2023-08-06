5b9a7827cd7f3525b11be2286ae29f8517b05b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:26 +0000

    Add package codebots/0.4.1/codebots-0.4.1-py2.py3-none-any.whl

---
+++ packages/codebots/codebots-0.4.1-py2.py3-none-any.whl/codebots/utilities/cli.py
@@ -0,0 +1,16 @@
+"""Console script for codebots."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for codebots."""
+    click.echo("Replace this message by putting your code into "
+               "codebots.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
