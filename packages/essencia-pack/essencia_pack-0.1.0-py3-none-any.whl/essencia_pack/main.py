d299a8356ab82bed2c3e3243ae69614a7067ebe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:29 +0000

    Add package essencia-pack/0.1.0/essencia_pack-0.1.0-py3-none-any.whl

---
+++ packages/essencia-pack/essencia_pack-0.1.0-py3-none-any.whl/essencia_pack/main.py
@@ -0,0 +1,12 @@
+from essencia_pack import Licence
+from essencia_pack import Day
+
+if __name__ == '__main__':
+    x = Licence('crm', 9553, 'GO')
+    y = Day.Dia07
+    print(x)
+    print(y)
+
+
+
+
