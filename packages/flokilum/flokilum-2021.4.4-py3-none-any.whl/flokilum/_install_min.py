893bdaea195daf6c7b0eeaad0824f8ba3c295fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package flokilum/2021.4.4/flokilum-2021.4.4-py3-none-any.whl

---
+++ packages/flokilum/flokilum-2021.4.4-py3-none-any.whl/flokilum/_install_min.py
@@ -0,0 +1,14 @@
+def install_min(package, pip_name, version):
+
+    from packaging.version import parse
+    from importlib import import_module
+
+    from ._command import command
+
+    try:
+        from package import __version__ as INFO
+        if parse(INFO) < parse(version):
+            command(F"pip install --upgrade {pip_name}")
+    except:
+        command(F"pip install --upgrade {pip_name}")
+        from package import __version__ as INFO
---
+++ packages/flokilum/flokilum-2021.4.4-py3-none-any.whl/flokilum/colab/_bokeh.py
@@ -0,0 +1,13 @@
+def bokeh():
+
+    from packaging.version import parse
+    from importlib import import_module
+
+    from .._install_min import install_min
+
+    install_min(package="bokeh", pip_name="bokeh", version="2.3.1")
+
+    print(F"{'bokeh'.rjust(50)} : {INFO}")
+
+    ## do not need to load this directly
+    # return import_module("bokeh")
