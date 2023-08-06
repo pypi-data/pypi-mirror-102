360b14794861adc56869522adcda58ba551c185e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:32 +0000

    Add package pytest-salt-factories/0.902.0/pytest_salt_factories-0.902.0-py3-none-any.whl

---
+++ packages/pytest-salt-factories/pytest_salt_factories-0.902.0-py3-none-any.whl/saltfactories/factories/cli/__init__.py
@@ -0,0 +1,13 @@
+"""
+CLI
+===
+"""
+from . import call
+from . import client
+from . import cloud
+from . import cp
+from . import key
+from . import run
+from . import salt
+from . import spm
+from . import ssh
