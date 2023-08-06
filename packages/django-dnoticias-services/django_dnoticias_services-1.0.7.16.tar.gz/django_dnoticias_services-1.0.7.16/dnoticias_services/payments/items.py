94f6efc417971179f141b103ec309cc47300b3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:42 +0000

    Add package django-dnoticias-services/1.0.7.16/django_dnoticias_services-1.0.7.16.tar.gz

---
+++ packages/django-dnoticias-services/django_dnoticias_services-1.0.7.16.tar.gz/django_dnoticias_services-1.0.7.16/dnoticias_services/payments/items.py
@@ -0,0 +1,98 @@
+from urllib.parse import urljoin
+
+import requests
+from django.conf import settings
+from dnoticias_services.utils.request import get_headers
+
+from .base import BasePaymentRequest
+
+
+class BaseItemRequest(BasePaymentRequest):
+    def __init__(self):
+        self.url = self.get_url()
+
+    def get_url(self):
+        return settings.ITEM_API_URL
+
+class CreateItem(BaseItemRequest):
+    def __call__(self, name, slug, price, active=True, extra_attrs=dict(), description="", images=dict(), shippable=False, interval=None, interval_count=None, trial_interval=None, trial_interval_count=None, offers=[], category=None, api_key=None, timeout=None):
+        url = self.get_url()
+        _api_key = api_key or self.api_key
+        _timeout = timeout or self.timeout
+        
+        response = requests.post(
+            url,
+            headers=get_headers(_api_key),
+            json={
+                "name" : name,
+                "slug" : slug,
+                "extra_attrs" : extra_attrs,
+                "description" : description,
+                "price" : price,
+                "active" : active,
+                "images" : images,
+                "shippable" : shippable,
+                "interval" : interval,
+                "interval_count" : interval_count,
+                "trial_interval" : trial_interval,
+                "trial_interval_count" : trial_interval_count,
+                "offers" : offers,
+                "category" : category
+            },
+            timeout=_timeout
+        )
+        response.raise_for_status()
+        return response
+
+class ResolveItem(BaseItemRequest):
+    def get_url(self, uuid):
+        return urljoin(settings.ITEM_API_URL, uuid)
+
+class UpdateItem(ResolveItem):
+    def __call__(self, uuid, name=None, slug=None, extra_attrs=dict(), description="", price=None, active=None, images=None, shippable=None, interval=None, interval_count=None, trial_interval=None, trial_interval_count=None, offers=[], category=None, api_key=None, timeout=None):
+        assert uuid
+
+        url = self.get_url(uuid)
+        _api_key = api_key or self.api_key
+        _timeout = timeout or self.timeout
+        
+        response = requests.post(
+            url,
+            headers=get_headers(_api_key),
+            json={
+                "name" : name,
+                "slug" : slug,
+                "extra_attrs" : extra_attrs,
+                "description" : description,
+                "price" : price,
+                "active" : active,
+                "images" : images,
+                "shippable" : shippable,
+                "interval" : interval,
+                "interval_count" : interval_count,
+                "trial_interval" : trial_interval,
+                "trial_interval_count" : trial_interval_count,
+                "offers" : offers,
+                "category" : category
+            },
+            timeout=_timeout
+        )
+        response.raise_for_status()
+        return response
+
+
+class DeleteItem(ResolveItem):
+    def __call__(self, uuid, api_key=None, timeout=None):
+        _api_key = api_key or self.api_key
+        _timeout = timeout or self.timeout
+        response = requests.delete(
+            self.get_url(uuid),
+            headers=get_headers(_api_key),
+            timeout=_timeout
+        )
+        response.raise_for_status()
+        return response
+
+create_item = CreateItem()
+update_item = UpdateItem()
+delete_item = DeleteItem()
