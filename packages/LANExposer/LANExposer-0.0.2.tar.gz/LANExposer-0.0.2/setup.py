b55fdfcb55ba0a983c37023641efa5e1029a703d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:01 +0000

    Add package LANExposer/0.0.2/LANExposer-0.0.2.tar.gz

---
+++ packages/LANExposer/LANExposer-0.0.2.tar.gz/LANExposer-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='LANExposer',
+    version='0.0.2',
+    description='Make LAN requests remotely.',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    entry_points = {
+        'console_scripts': [
+            'lanexpose=lanexpose.cli:cli',
+        ],
+    }
+)
