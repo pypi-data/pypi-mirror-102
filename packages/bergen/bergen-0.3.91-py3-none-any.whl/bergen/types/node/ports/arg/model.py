ea26bc55f57eb9ca6487a1a25ff580b118e6cc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:05 +0000

    Add package bergen/0.3.91/bergen-0.3.91-py3-none-any.whl

---
+++ packages/bergen/bergen-0.3.91-py3-none-any.whl/bergen/types/node/ports/arg/model.py
@@ -0,0 +1,21 @@
+
+
+from bergen.types.node.ports.arg.base import BaseArgPort
+from typing import Type
+from bergen.types.node.widgets import QueryWidget
+from bergen.types.model import ArnheimModel
+
+class ModelArgPort(BaseArgPort):
+
+  def __init__(self, modelMeta, widget=None, **kwargs) -> None:
+      self.modelMeta = modelMeta
+      if widget is None:
+        if hasattr(self.modelMeta, "selector_query"):
+            widget = QueryWidget(query=self.modelMeta.selector_query)
+
+      assert widget is not None, "You didn't provide a widget nor has it been declared in the meta class of the Model"
+        
+      super().__init__(widget, **kwargs)
+
+  def serialize(self):
+      return {**super().serialize(),"identifier" : self.modelMeta.identifier}
