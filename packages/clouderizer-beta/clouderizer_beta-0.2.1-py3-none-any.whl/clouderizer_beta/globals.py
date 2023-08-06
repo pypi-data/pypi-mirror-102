84a99b339a27c5ac13a301a4dba31e947d9c8e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:10 +0000

    Add package clouderizer-beta/0.2.1/clouderizer_beta-0.2.1-py3-none-any.whl

---
+++ packages/clouderizer-beta/clouderizer_beta-0.2.1-py3-none-any.whl/clouderizer_beta/globals.py
@@ -0,0 +1,21 @@
+import os
+
+
+def init():
+    global BASE_URL
+    global LOCAL_PATH
+    global LOCAL_DIR
+    global DOCKERFILES_LOC
+    global LAMBDA_FUNCTION_DIR
+    global S3_CLDZ_PRESIGNED_ENDPOINT
+
+    BASE_URL="https://betaconsole.clouderizer.com"
+
+    LOCAL_PATH=os.getenv("HOME") + "/.clouderizer/creds"
+    if "beta" in BASE_URL:
+	    LOCAL_PATH = os.getenv("HOME") + "/.clouderizer/beta/creds"
+    LOCAL_DIR=os.getenv("HOME") + "/.clouderizer"
+    DOCKERFILES_LOC = LOCAL_DIR+"/dockerfiles/"
+    LAMBDA_FUNCTION_DIR = LOCAL_DIR+ "/lambda-dir/function/"
+
+    S3_CLDZ_PRESIGNED_ENDPOINT = BASE_URL + '/api/awsconfig/generatepresignedurl'
