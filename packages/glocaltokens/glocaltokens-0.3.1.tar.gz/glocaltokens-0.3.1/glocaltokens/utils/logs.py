2ce00dceeb80b56cb3942d0b6e0cf87a5886124d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:15 +0000

    Add package glocaltokens/0.3.1/glocaltokens-0.3.1.tar.gz

---
+++ packages/glocaltokens/glocaltokens-0.3.1.tar.gz/glocaltokens-0.3.1/glocaltokens/utils/logs.py
@@ -0,0 +1,14 @@
+"""Log utilities"""
+
+from typing import Optional
+
+
+def censor(text: Optional[str]) -> str:
+    """
+    Replaces characters in a str with the asterisks
+
+    text: The text to censure.
+    """
+    char = "*"
+    text = text if text else ""
+    return text[0] + (len(text) - 1) * char if text else text
