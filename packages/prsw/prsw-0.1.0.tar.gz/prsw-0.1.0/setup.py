97bc3ec37ea265747f5195e327bdd5355a5980fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:21 +0000

    Add package prsw/0.1.0/prsw-0.1.0.tar.gz

---
+++ packages/prsw/prsw-0.1.0.tar.gz/prsw-0.1.0/setup.py
@@ -0,0 +1,49 @@
+"""prsw setup.py"""
+
+import re
+from os import path
+
+from setuptools import setup
+
+PACKAGE_NAME = "prsw"
+PATH = path.abspath(path.dirname(__file__))
+with open(path.join(PATH, "README.rst"), encoding="utf-8") as fp:
+    README = fp.read()
+with open(path.join(PATH, PACKAGE_NAME, "__init__.py"), encoding="utf-8") as fp:
+    VERSION = re.search('__version__ = "([^"]+)"', fp.read()).group(1)
+
+extras = {
+    "dev": ["packaging", "pre-commit"],
+    "lint": [
+        "black",
+        "flake8",
+        "pydocstyle",
+        "sphinx",
+        "sphinx_rtd_theme",
+    ],
+    "readthedocs": ["sphinx"],
+    "test": ["pytest >= 2.7.3"],
+}
+extras["dev"] += extras["lint"] + extras["test"]
+
+setup(
+    name=PACKAGE_NAME,
+    author="Jonathan P. Voss",
+    author_email="jonathan@expbits.io",
+    python_requires="~=3.6",
+    description=(
+        "PRSW, the `Python RIPE Stat Wrapper`, is a python package that"
+        " simplifies access to the RIPE Stat public data API."
+    ),
+    extras_require=extras,
+    install_requires=["requests >=2"],
+    keywords="RIPE RIPEStat api wrapper",
+    license="Simplified BSD License",
+    long_description=README,
+    project_urls={
+        "Documentation": "https://prsw.readthedocs.io/",
+        "Issue Tracker": "https://github.com/jvoss/prsw/issues",
+        "Source Code": "https://github.com/jvoss/prsw",
+    },
+    version=VERSION,
+)
