77f695e9b46791030f24cc9add16d628dc9619a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:41 +0000

    Add package pyspark-bundle/0.1.0a1/pyspark_bundle-0.1.0a1-py3-none-any.whl

---
+++ packages/pyspark-bundle/pyspark_bundle-0.1.0a1-py3-none-any.whl/pysparkbundle/csv/lineage/CsvRead.py
@@ -0,0 +1,8 @@
+from pysparkbundle.lineage.FileRead import FileRead
+
+
+class CsvRead(FileRead):
+
+    @property
+    def type(self):
+        return "csv"
---
+++ packages/pyspark-bundle/pyspark_bundle-0.1.0a1-py3-none-any.whl/pysparkbundle/json/lineage/JsonRead.py
@@ -0,0 +1,8 @@
+from pysparkbundle.lineage.FileRead import FileRead
+
+
+class JsonRead(FileRead):
+
+    @property
+    def type(self):
+        return "json"
