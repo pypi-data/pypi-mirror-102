4b65c605bd5d6557c591f214c06211ba5678ee3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package text-to-owo/1.1.5/text_to_owo-1.1.5-py3-none-any.whl

---
+++ packages/text-to-owo/text_to_owo-1.1.5-py3-none-any.whl/owo.py
@@ -0,0 +1,108 @@
+import random
+
+# TODO: Add blocklist for bad words
+
+prefixes = (
+    "<3 ",
+    "0w0 ",
+    "H-hewwo?? ",
+    "HIIII! ",
+    "Haiiii! ",
+    "Huohhhh. ",
+    "OWO ",
+    "OwO ",
+    "UwU ",
+    "Whats this? "
+)
+
+suffixes = (
+    " ( ͡° ᴥ ͡°)",
+    " (இωஇ )",
+    " (๑•́ ₃ •̀๑)",
+    " (• o •)",
+    " (●´ω｀●)",
+    " (◠‿◠✿)",
+    " (✿ ♡‿♡)",
+    " (❁´◡`❁)",
+    " (　\"◟ \")",
+    " (人◕ω◕)",
+    " (；ω；)",
+    " (｀へ´)",
+    " ._.",
+    " *nuzzles u*",
+    " :3",
+    " :D",
+    " :P",
+    " ;-;",
+    " ;3",
+    " ;_;",
+    " <{^v^}>",
+    " >_<",
+    " >_>",
+    " UwU",
+    " XDDD",
+    " ^-^",
+    " ^_^",
+    " x3c",
+    " x3",
+    " xD",
+    " ÙωÙ",
+    " ʕʘ‿ʘʔ",
+    " ㅇㅅㅇ",
+    ", fwendo",
+    "（＾ｖ＾）",
+    " nya~"
+)
+
+substitutions = {
+    "love": "wuv", # keep before l->w
+    "Love": "Wuv",
+    "loving": "wuving",
+    "Loving": "Wuving",
+    "r": "w",
+    "l": "w",
+    "R": "W",
+    "L": "W",
+    # "ow": "OwO",
+    "th ": "f ",
+    "no": "nu",
+    "No": "Nu",
+    "has": "haz",
+    "Has": "Haz",
+    "have": "haz",
+    "Have": "Haz",
+    " says": " sez",
+    "you": "uu",
+    "I\"ve": "I",
+    "the ": "da ",
+    "The ": "Da ",
+    "THE ": "THE ",
+    "qu": "qw",
+    "Qu": "Qw",
+    "pause ": "paws ",
+    "Pause ": "Paws ",
+    "paus": "paws",
+    "Paus": "paws"
+}
+
+
+def add_prefix(str, prefixes=prefixes):
+    return random.choice(prefixes) + str
+
+
+def add_suffix(str, suffixes=suffixes):
+    return str + random.choice(suffixes)
+
+
+def add_affixes(str, prefixes=prefixes, suffixes=suffixes):
+    return random.choice(prefixes) + str + random.choice(suffixes)
+
+
+def substitute(str, substitutions=substitutions):
+    for sub in substitutions:
+        str = str.replace(sub, substitutions[sub])
+    return str
+
+
+def owo(str, substitutions=substitutions, prefixes=prefixes, suffixes=suffixes):
+    return add_affixes(substitute(str, substitutions=substitutions), prefixes=prefixes, suffixes=suffixes)
