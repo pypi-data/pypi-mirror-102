869ee1d54dd0ae6475819b5a5cfc837b35ca72c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:23 +0000

    Add package get-chrome-driver/1.1.42/get-chrome-driver-1.1.42.tar.gz

---
+++ packages/get-chrome-driver/get-chrome-driver-1.1.42.tar.gz/get-chrome-driver-1.1.42/get_chrome_driver/constants.py
@@ -0,0 +1,12 @@
+CHROMEDRIVER_CHROMIUM_URL = 'https://chromedriver.chromium.org'
+CHROMEDRIVER_STORAGE_URL = 'https://chromedriver.storage.googleapis.com'
+
+ZIP_TYPE = '.zip'
+
+CHROMEDRIVER = 'chromedriver'
+WEBDRIVER = 'webdriver'
+
+CSS_VERSIONS_SELECTOR_UL = '.sites-layout-tile > div:nth-child(1) > div:nth-child(8) > ul:nth-child(1)'
+
+LATEST_STABLE_VERSION_STR = 'Latest stable release'
+LATEST_BETA_VERSION_STR = 'Latest beta release'
