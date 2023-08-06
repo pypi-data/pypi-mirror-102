6567320274ea8b6d7aa71ee5585fdd973dfbaea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package cloudx-nac-api/1.3.1/cloudx_nac_api-1.3.1-py3-none-any.whl

---
+++ packages/cloudx-nac-api/cloudx_nac_api-1.3.1-py3-none-any.whl/cloudx_caas_api/__init__.py
@@ -0,0 +1,5 @@
+'''This module implements CloudX CaaS APIs 
+* cluster_management        Clsuter Management service API set 
+* repository_management     Docker repository Management service API set
+* package_management        Helm Package  Management API set 
+'''
---
+++ packages/cloudx-nac-api/cloudx_nac_api-1.3.1-py3-none-any.whl/cloudx_iaas_api/__init__.py
@@ -0,0 +1,6 @@
+'''This module implements CloudX IaaS APIs 
+* network_management        Network Management service API set 
+* network_vpn_management    VPN Connecitivity service Management API set
+* storage_management        Storage Management API set 
+* compute_management        Virtualization Management service API set
+'''
