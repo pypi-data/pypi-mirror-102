ca119c93a877a6156e27d8dfd340e61a648c53d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:24 +0000

    Add package guvva/1.0.0/guvva-1.0.0.tar.gz

---
+++ packages/guvva/guvva-1.0.0.tar.gz/guvva-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='guvva',  
+     version='1.0.0',
+     scripts=['guvva'] ,
+     author="MasterMind.inc",
+     author_email="mastermindm2rd@gmail.com",
+     description="A New language made using python, very harder",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
