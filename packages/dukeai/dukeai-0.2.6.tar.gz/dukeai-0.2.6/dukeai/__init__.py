893b75dac618e76c6d69ce4f23ece201a15d3d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:27 +0000

    Add package dukeai/0.2.6/dukeai-0.2.6.tar.gz

---
+++ packages/dukeai/dukeai-0.2.6.tar.gz/dukeai-0.2.6/dukeai/__init__.py
@@ -0,0 +1,4 @@
+from .bookkeeping import get_token, get_ifta_trip_id, get_tax_info, send_ifta_data, update_tax_info
+from .rpa import get_rpa_token
+from .auto import duke_auto, rpa_auto, ralock_auto
+from .ocr import duke_ocr
