04033e361af827d8dd0b354dda7cfbd37e1aea28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:17 +0000

    Add package shortcut-alias/0.2.2/shortcut-alias-0.2.2.tar.gz

---
+++ packages/shortcut-alias/shortcut-alias-0.2.2.tar.gz/shortcut-alias-0.2.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    require = fh.readlines()
+
+setuptools.setup(
+    name="shortcut-alias",
+    version="0.2.2",
+    author="Matt Limb",
+    author_email="matt.limb17@gmail.com",
+    description="Powerful Configurable Aliases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MattLimb/shortcut-alias/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        req.strip() 
+        for req in require
+    ],
+    python_requires='>=3.6',
+    entry_points={  
+        'console_scripts': [
+            'shortcut-alias=shortcut_alias.__main__:main',
+            'shortcut=shortcut_alias.__main__:main',
+            'sa=shortcut_alias.__main__:main',
+        ],
+    }
+)
