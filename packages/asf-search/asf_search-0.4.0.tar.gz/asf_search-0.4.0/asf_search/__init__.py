5e88d406d0c0b89256015805ba2eda7b6cd176b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:08 +0000

    Add package asf-search/0.4.0/asf_search-0.4.0.tar.gz

---
+++ packages/asf-search/asf_search-0.4.0.tar.gz/asf_search-0.4.0/asf_search/__init__.py
@@ -0,0 +1,17 @@
+from importlib.metadata import PackageNotFoundError, version
+
+from .ASFProduct import ASFProduct
+from .ASFSearchResults import ASFSearchResults
+from .exceptions import *
+from .constants import *
+from .health import *
+from .search import *
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    print('package is not installed!\n'
+          'Install in editable/develop mode via (from the top of this repo):\n'
+          '   python -m pip install -e .\n'
+          'Or, to just get the version number use:\n'
+          '   python setup.py --version')
