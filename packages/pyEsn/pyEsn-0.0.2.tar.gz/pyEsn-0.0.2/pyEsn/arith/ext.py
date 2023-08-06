25b32efea4997fadeb8d18924fcb48511e2d8ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:19 +0000

    Add package pyEsn/0.0.2/pyEsn-0.0.2.tar.gz

---
+++ packages/pyEsn/pyEsn-0.0.2.tar.gz/pyEsn-0.0.2/pyEsn/arith/ext.py
@@ -0,0 +1,9 @@
+def mul(x,y):
+    return x*y
+    
+def div(x,y):
+    return x/y
+    
+from .add import *
+def add2(x,y):
+    return add(x**2,y**2)
---
+++ packages/pyEsn/pyEsn-0.0.2.tar.gz/pyEsn-0.0.2/pyEsn/hello.py
@@ -0,0 +1,5 @@
+def greet1(name):
+    print("Hello "+name)
+    
+def greet2(name):
+    print("Welcome "+name)
