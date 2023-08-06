b89f25614aaf683396b488bf098015e8f15bd1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:40 +0000

    Add package OpenResearchMigration/0.0.15/OpenResearchMigration-0.0.15-py3-none-any.whl

---
+++ packages/OpenResearchMigration/OpenResearchMigration-0.0.15-py3-none-any.whl/ormigrate/eventSeriesFixer.py
@@ -0,0 +1,40 @@
+'''
+Created on 2021-04-16
+
+@author: wf
+'''
+from ormigrate.fixer import PageFixer
+from ormigrate.rating import Rating
+
+class EventSeriesProvenanceFixer(PageFixer):
+    '''
+    fixes Event Series
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    @classmethod
+    def getRating(cls,eventRecord):
+        hasDblp=eventRecord.get('dblpSeries') is not  None
+        hasWikidata=eventRecord.get('wikidataId') is not None
+        if hasDblp and hasWikidata:
+            return Rating(1,Rating.ok,'Gold Standard series')
+        if hasDblp:
+            return Rating(4,Rating.ok,'Wikdata id missing for dblp series')
+        if hasWikidata:
+            return Rating(3,Rating.ok,'Wikdata only series')
+        return Rating(5,Rating.invalid,'Series provenance data missing')
+    
+class EventSeriesTitleFixer(PageFixer):
+    '''
+    https://github.com/SmartDataAnalytics/OpenResearch/issues/136
+    
+    Missing titles in event Series #136
+    '''    
+    @classmethod
+    def getRating(cls,eventRecord):
+        return  Rating(7,Rating.invalid,'Rating not implemented yet')
