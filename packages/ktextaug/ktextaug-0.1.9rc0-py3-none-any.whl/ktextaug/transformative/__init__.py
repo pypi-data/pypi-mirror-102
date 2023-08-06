79254c855bc038b77aafcebf7b865f1f2ecbcc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:49 +0000

    Add package ktextaug/0.1.9rc0/ktextaug-0.1.9rc0-py3-none-any.whl

---
+++ packages/ktextaug/ktextaug-0.1.9rc0-py3-none-any.whl/ktextaug/transformative/__init__.py
@@ -0,0 +1,7 @@
+
+from .back_translation import back_translate
+from .noise_addition import noise_add
+from .random_process import random_delete, random_swap, random_insert
+from .synonym_replacement import synonym_replace
+from .utils import *
+
