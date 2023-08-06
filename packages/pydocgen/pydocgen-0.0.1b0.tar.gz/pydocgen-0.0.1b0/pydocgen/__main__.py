7265b023605f0737b447fbb9b979900ec3d89b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:14 +0000

    Add package pydocgen/0.0.1b0/pydocgen-0.0.1b0.tar.gz

---
+++ packages/pydocgen/pydocgen-0.0.1b0.tar.gz/pydocgen-0.0.1b0/pydocgen/__main__.py
@@ -0,0 +1,67 @@
+import argparse
+import pydocgen
+import importlib
+from pydocgen import documentation_generator
+
+parser = argparse.ArgumentParser(
+    description='Automatically generate docstrings and ' 
+                'documentation for Python modules.',
+    formatter_class=argparse.RawTextHelpFormatter
+)
+
+add_arg = parser.add_argument
+
+add_arg(
+    '-i', '--input', 
+    metavar='',
+    type=str,
+    help='Set the input file for which docstrings and documentation '
+         'needs to be generated\n'
+         'Currently supports one module only',
+    required=True,
+    default='doc'
+)
+
+add_arg(
+    '-v', '--version', 
+    help='Prints installed version of PyDocGen',
+    action='version', 
+    version='%(prog)s ' + argparse.__version__
+)
+
+add_arg(
+    '-g', '--generator',
+    metavar='',
+    type=str,
+    help='Set the underlying documentation output generator\n'
+         'Currently supports pdoc3 only\n'
+         'DEFAULT: pdoc3',
+    default='pdoc3'
+)
+
+add_arg(
+    '-f', '--format',
+    metavar='',
+    type=str,
+    help='Set the expected output of docstrings to be generated\n'
+         'Currently supports numpy style only\n'
+         'DEFAULT: numpy',
+    default='numpy'
+)
+
+add_arg(
+    '-t', '--output-type', 
+    metavar='',
+    type=str,
+    help='Set the format for storing generated documentation\n'
+         'DEFAULT: html',
+    default='html'
+)
+
+def main(args=None):
+    module = importlib.import_module(args.input)
+    doc_generator = documentation_generator.Pdoc([module], args.output_dir, args.file_type)
+    doc_generator.save()
+
+if __name__ == "__main__":
+    main(parser.parse_args())
---
+++ packages/pydocgen/pydocgen-0.0.1b0.tar.gz/pydocgen-0.0.1b0/pydocgen/documentation_generator.py
@@ -0,0 +1,31 @@
+import pdoc
+import os
+import os.path as path
+CWD = os.getcwd()
+
+class Pdoc():
+    def __init__(self, modules, output_dir, file_type):
+        self.context = pdoc.Context()
+        self.output_dir = output_dir
+        self.file_type = file_type
+        self.modules = [pdoc.Module(module, context=self.context) 
+                        for module in modules]
+        pdoc.link_inheritance(self.context)
+    
+    def __save(self, module):
+        yield module.name, module.html()
+        for sub_module in module.submodules():
+            yield from self.__save(sub_module)
+    
+    def save(self):
+        for module in self.modules:
+            for module_name, output in self.__save(module):
+                path_ = path.join(CWD, self.output_dir, module_name 
+                                  + '.' + self.file_type)
+                os.makedirs(os.path.dirname(path_), exist_ok=True)
+                with open(path_, 'a+') as fout:
+                    fout.write(output)
+
+import helper
+d = Pdoc([helper], 'doc', 'html')
+d.save()
