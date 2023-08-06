1e14909e22a7d700369323d6ec4d3c566531e7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:36 +0000

    Add package Binomialdistributions2021/0.1/Binomialdistributions2021-0.1.tar.gz

---
+++ packages/Binomialdistributions2021/Binomialdistributions2021-0.1.tar.gz/Binomialdistributions2021-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Binomialdistributions2021',
+      version='0.1',
+      description='Binomial distributions ToTry',
+      packages=['distributions'],
+      zip_safe=False)
