539949128695ffcc1be9dc289090427b8440eb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:02 +0000

    Add package testuiop/1.0/testuiop-1.0.tar.gz

---
+++ packages/testuiop/testuiop-1.0.tar.gz/testuiop-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testuiop',
+    version='1.0',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages()
+)
+
