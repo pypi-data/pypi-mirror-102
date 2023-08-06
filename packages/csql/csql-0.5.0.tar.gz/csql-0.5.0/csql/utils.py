68979d16bfae0a9af845bbd634c71784be735799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:25 +0000

    Add package csql/0.5.0/csql-0.5.0.tar.gz

---
+++ packages/csql/csql-0.5.0.tar.gz/csql-0.5.0/csql/utils.py
@@ -0,0 +1,25 @@
+from typing import *
+from typing import NoReturn # py 3.6
+
+
+#https://stackoverflow.com/a/34073559/5264127
+T = TypeVar('T')
+R = TypeVar('R')
+class Collector(Generic[T, R]):
+	returned: R
+	def __init__(self, generator: Generator[T, None, R]):
+		self.generator = generator
+	def __iter__(self) -> Iterator[T]:
+		self.returned = yield from self.generator
+
+
+def unique(gen: Iterable[T], fn:Callable[[T], Any] = hash) -> Iterable[T]:
+	seen = set()
+	for val in gen:
+		if fn(val) in seen:
+			continue
+		yield val
+		seen.add(fn(val))
+
+def assert_never(x: NoReturn) -> NoReturn:
+	assert False, f'Unhandled type: {type(x).__name__}'
