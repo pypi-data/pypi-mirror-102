be1fb3bfb35790fa1a2b946a52ec55a16b81689a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:08 +0000

    Add package trackerlab/0.0.4/trackerlab-0.0.4.tar.gz

---
+++ packages/trackerlab/trackerlab-0.0.4.tar.gz/trackerlab-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="trackerlab",
+    version="0.0.4",
+    author="Martin",
+    author_email="martin.fraenzl@physik.uni-leipzig.de",
+    description="Package for detecting features in digital microscopy images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fraem24/TrackerLab",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={'': ['*.ui',]}
+    #include_package_data = True, # http://peak.telecommunity.com/DevCenter/setuptools#including-data-files
+    #python_requires='>=3.6',
+)
