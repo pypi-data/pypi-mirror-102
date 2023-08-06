9b6ee94e821eea1c7e54bee2de305d39a4fdd4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:07 +0000

    Add package athletemodelsbhp/1.2.0/athletemodelsbhp-1.2.0.tar.gz

---
+++ packages/athletemodelsbhp/athletemodelsbhp-1.2.0.tar.gz/athletemodelsbhp-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'athletemodelsbhp',
+    version = '1.2.0',
+    py_modules = ['athletemodelsbhp'],
+    author = 'sourav',
+    author_email = 'bhsourav17@gmail.com',
+    description = 'Athlete model'
+    )
