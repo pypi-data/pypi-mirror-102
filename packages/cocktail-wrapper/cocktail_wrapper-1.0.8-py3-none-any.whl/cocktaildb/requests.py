f30d18ddd774272d9f6d6a01774370b991a89f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:27 +0000

    Add package cocktail-wrapper/1.0.8/cocktail_wrapper-1.0.8-py3-none-any.whl

---
+++ packages/cocktail-wrapper/cocktail_wrapper-1.0.8-py3-none-any.whl/cocktaildb/requests.py
@@ -0,0 +1,65 @@
+import aiohttp 
+from aiohttp import client_exceptions as errs
+
+class BadResponse(Exception):
+	def __init__(self, message):
+		super(BadResponse, self).__init__(message)
+
+class Reqs:
+	ses = aiohttp.ClientSession()
+	"""Methods to handle requests"""
+
+	@staticmethod ## check for headers
+	def header_check(headers: any = None):
+		if not headers:
+			headers = {}
+		else:
+			headers = {headers}
+
+		return headers
+
+	@staticmethod ## check for data {POST} 
+	def data_check(data: any = None):
+		if not data:
+			data = {}
+		else:
+			data = {data}
+
+		return data
+
+	@classmethod ## GET request
+	async def get(cls, url: str, *, headers: any = None):
+		headers = Reqs.header_check(headers)
+		ses = cls.ses
+		async with ses.get(url, headers=headers) as r:
+			try:
+				if r.status in range(200, 299):
+					data = await r.json()
+					return data
+				else:
+					raise BadResponse(r.status)
+			except errs.ContentTypeError:
+				if r.status in range(200, 299):
+					data = await r.json(content_type="text/html")
+					return data
+				else:
+					raise BadResponse(r.status)
+	
+	@classmethod ## POST request
+	async def post(cls, url: str, *, headers: any = None, data: any = None):
+		headers = Reqs.header_check(headers)
+		data = Reqs.data_check(data)
+		ses = cls.ses
+		async with ses.post(url, headers=headers, data=data) as r:
+			try:
+				if r.status in range(200, 299):
+					data = await r.json()
+					return data
+				else:
+					raise BadResponse(r.status)
+			except errs.ContentTypeError:
+				if r.status in range(200, 299):
+					data = await r.json(content_type="text/html")
+					return data
+				else:
+					raise BadResponse(r.status)
