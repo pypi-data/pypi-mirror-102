bccf8916da880183497b3a3967bb21276e368b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:28 +0000

    Add package TickTickAPI/1.0/TickTickAPI-1.0.tar.gz

---
+++ packages/TickTickAPI/TickTickAPI-1.0.tar.gz/TickTickAPI-1.0/setup.py
@@ -0,0 +1,15 @@
+"""This module contains setting to build PyPi packet """
+
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+setup(
+    name='TickTickAPI',
+    version='1.0',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    url='https://gitwork.ru/results/practice-3k/2021/2018-4-02-aku.git',
+    author='Akulinushkin Oleg',
+    author_email='oleg.akulinishkin.01@mail.ru',
+    python_requires=">=3.8",
+)
