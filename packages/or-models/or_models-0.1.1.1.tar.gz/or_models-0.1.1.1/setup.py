881d49efe862a53381036cc545a6c6aa2a57bee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:24 +0000

    Add package or-models/0.1.1.1/or_models-0.1.1.1.tar.gz

---
+++ packages/or-models/or_models-0.1.1.1.tar.gz/or_models-0.1.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="or_models",
+    version="0.1.1.1",
+    url="https://github.com/hak91/OptimizationModels",
+    author="Hussain Kharodawala",
+    author_email="hussain.abbas52@gmail.com",
+    description="Some standard textbook Optimization Models and Algorithms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords = ['set cover', 'Operations Research','Mathematical Optimization', 'heuristic', 'greedy', 'Lagrangian Relaxation'],
+    install_requires=[
+        'numpy>=1.19.2',
+        'ortools>=8.2.8710'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
