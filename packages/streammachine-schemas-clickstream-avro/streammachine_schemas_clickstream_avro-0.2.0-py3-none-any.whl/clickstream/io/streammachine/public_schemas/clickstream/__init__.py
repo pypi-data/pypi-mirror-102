767d6af672ef8e41249872babbe1e260685fa497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:03 +0000

    Add package streammachine-schemas-clickstream-avro/0.2.0/streammachine_schemas_clickstream_avro-0.2.0-py3-none-any.whl

---
+++ packages/streammachine-schemas-clickstream-avro/streammachine_schemas_clickstream_avro-0.2.0-py3-none-any.whl/clickstream/io/streammachine/public_schemas/clickstream/__init__.py
@@ -0,0 +1,4 @@
+from .....schema_classes import SchemaClasses
+ClickstreamEvent = SchemaClasses.io.streammachine.public_schemas.clickstream.ClickstreamEventClass
+Customer = SchemaClasses.io.streammachine.public_schemas.clickstream.CustomerClass
+StrmMeta = SchemaClasses.io.streammachine.public_schemas.clickstream.StrmMetaClass
