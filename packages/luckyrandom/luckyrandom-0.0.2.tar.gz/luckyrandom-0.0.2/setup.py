f8ae4385070b76823428c5310c8bf27916915b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:12 +0000

    Add package luckyrandom/0.0.2/luckyrandom-0.0.2.tar.gz

---
+++ packages/luckyrandom/luckyrandom-0.0.2.tar.gz/luckyrandom-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="luckyrandom", # Replace with your own username
+    version="0.0.2",
+    author="Maximilian Wu",
+    author_email="me@maxesisn.online",
+    description="Encourage users with bad luck",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxesisn/luckyrandom",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
