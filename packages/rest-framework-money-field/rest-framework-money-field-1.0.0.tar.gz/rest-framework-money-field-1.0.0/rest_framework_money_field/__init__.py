2f311c580b9d43ad92f6c1174ffd697dfa176c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:16 +0000

    Add package rest-framework-money-field/1.0.0/rest-framework-money-field-1.0.0.tar.gz

---
+++ packages/rest-framework-money-field/rest-framework-money-field-1.0.0.tar.gz/rest-framework-money-field-1.0.0/rest_framework_money_field/__init__.py
@@ -0,0 +1,45 @@
+from decimal import Decimal
+from typing import Dict, Literal, Optional, Union
+
+from moneyed import CurrencyDoesNotExist, Money, get_currency
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import CharField, Field, IntegerField
+from rest_framework.serializers import Serializer
+
+__all__ = ("MoneyField",)
+
+
+MoneyRepresentation = Dict[Literal["amount", "currency"], Union[int, str]]
+
+
+class MoneySerializer(Serializer):
+    amount = IntegerField()
+    currency = CharField()
+
+
+class MoneyField(Field):
+    def to_internal_value(self, data: Optional[MoneyRepresentation]) -> Optional[Money]:
+        if data is None:
+            return None
+
+        serializer = MoneySerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        try:
+            currency = get_currency(code=validated_data["currency"])
+        except CurrencyDoesNotExist as e:
+            raise ValidationError(e)
+
+        amount = Decimal(validated_data["amount"]) / currency.sub_unit
+
+        return Money(amount=amount, currency=currency)
+
+    def to_representation(self, value: Optional[Money]) -> MoneyRepresentation:
+        if value is None:
+            return None
+
+        return {
+            "amount": round(value.amount * value.currency.sub_unit),
+            "currency": value.currency.code,
+        }
