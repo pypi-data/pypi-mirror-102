3ceb81dd61320c0f9a8d39b0f595cb3f54355d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:52 +0000

    Add package triehard/0.1.8/triehard-0.1.8.tar.gz

---
+++ packages/triehard/triehard-0.1.8.tar.gz/triehard-0.1.8/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['triehard', 'triehard.lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-nubia>=0.2b5,<0.3',
+ 'requests>=2.25.1,<3.0.0',
+ 'termcolor>=1.1.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['trie = triehard.cli:main']}
+
+setup_kwargs = {
+    'name': 'triehard',
+    'version': '0.1.8',
+    'description': 'A badly implemented trie',
+    'long_description': None,
+    'author': 'iCalculated',
+    'author_email': 'fufa0001@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
