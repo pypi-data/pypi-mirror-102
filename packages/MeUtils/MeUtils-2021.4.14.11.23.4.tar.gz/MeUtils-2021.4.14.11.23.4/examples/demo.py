4946c3412603ade9872b4c2ffdec17aae038aaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:11 +0000

    Add package MeUtils/2021.4.14.11.23.4/MeUtils-2021.4.14.11.23.4.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.4.14.11.23.4.tar.gz/MeUtils-2021.4.14.11.23.4/examples/demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : demo
+# @Time         : 2021/2/26 6:53 下午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
+from meutils.common import Main
+
+
+class A(Main):
+
+    def __init__(self, ):
+        pass
+
+
+    def main(self,):
+        pass
+
+
+if __name__ == '__main__':
+    A.cli()
