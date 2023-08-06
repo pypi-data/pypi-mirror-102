f49a5fcb05a93ea5f4ebac2e1e08647c9cbbed48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:40 +0000

    Add package StockFast/0.0.5/StockFast-0.0.5.tar.gz

---
+++ packages/StockFast/StockFast-0.0.5.tar.gz/StockFast-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.5'
+DESCRIPTION = 'A Stock and Crypto Predictor using Neural Networks'
+LONG_DESCRIPTION = 'A package that allows users to predict closing stock prices and crypto prices, built on Neural Networks'
+
+# Setting up
+setup(
+    name="StockFast",
+    version=VERSION,
+    author="Ari.Dev (Arihant Tripathi)",
+    author_email="tarihant2001@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    install_requires=['numpy', 'pandas', 'pandas_datareader', 'matplotlib', 'sklearn', 'tensorflow'],
+    keywords=['python', 'osiris', 'stocks', 'crypto', 'neural networks', 'tensorflow'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
