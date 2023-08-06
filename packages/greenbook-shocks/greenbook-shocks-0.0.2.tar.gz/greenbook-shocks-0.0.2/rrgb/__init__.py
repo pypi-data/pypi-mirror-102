db41d80699be6e63c5f7a648b3efd6a14e6495b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:29 +0000

    Add package greenbook-shocks/0.0.2/greenbook-shocks-0.0.2.tar.gz

---
+++ packages/greenbook-shocks/greenbook-shocks-0.0.2.tar.gz/greenbook-shocks-0.0.2/rrgb/__init__.py
@@ -0,0 +1,1 @@
+from .rrgb import RRGB
---
+++ packages/greenbook-shocks/greenbook-shocks-0.0.2.tar.gz/greenbook-shocks-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="greenbook-shocks",  # Replace with your own username
+    version="0.0.2",
+    author="Joe Saia",
+    author_email="joe5saia@gmail.com",
+    description="Estimates the Greenbook narrative shocks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joe5saia/GreenbookNarrativeShocks",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'openpyxl',
+        'sklearn',
+        'datapungi_fed',
+        'urllib3',
+        'bs4'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    python_requires='>=3.9',
+    include_package_data=True,
+    package_data={'': ['data/rr.pkl.gz']},
+)
