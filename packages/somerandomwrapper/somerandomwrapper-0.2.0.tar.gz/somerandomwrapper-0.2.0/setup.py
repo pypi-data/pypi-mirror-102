fa30bcb431d824a073525c5a56a34a51cbe89531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:16 +0000

    Add package somerandomwrapper/0.2.0/somerandomwrapper-0.2.0.tar.gz

---
+++ packages/somerandomwrapper/somerandomwrapper-0.2.0.tar.gz/somerandomwrapper-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    description = file.read()
+
+setup(
+    name='somerandomwrapper',
+    version='0.2.0',
+    description='A small local Wrapper for Some Random API written on Python.',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    packages=['somerandomwrapper'],
+    author_email='support@kerdokan.co',
+    url='https://github.com/Kerdokan/SomeRandomWrapper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+        'googletrans'
+    ]
+)
---
+++ packages/somerandomwrapper/somerandomwrapper-0.2.0.tar.gz/somerandomwrapper-0.2.0/somerandomwrapper/__init__.py
@@ -0,0 +1,15 @@
+"""
+SomeRandom Wrapper
+~~~~~~~~~~~~~~~~~~~
+A small local Wrapper for Some Random API written on Python.
+:copyright: (c) 2021 Julheer.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .wrapper import SomeRandomWrapper
+
+__title__ = 'SomeRandomWrapper'
+__author__ = 'Julheer'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Julheer'
+__version__ = '0.2.0'
