bbab6499220634d0b3c34f463933ab0f45846245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:52 +0000

    Add package FNNH/0.0.6/FNNH-0.0.6.tar.gz

---
+++ packages/FNNH/FNNH-0.0.6.tar.gz/FNNH-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+def readme():
+    with open('readme.md') as f:
+        return f.read()
+    
+setuptools.setup(
+    name="FNNH", # Replace with your own username
+    version="0.0.6",
+    author="Apratim Ray",
+    author_email="apratimr55@gmail.com",
+    description="A flexible CNN based Hash Function",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/apratimr/fnn-hash",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="FNNH"),
+    py_modules=["FNNH"],
+    install_requires = ["numpy"],
+    python_requires=">=3.6",
+)
