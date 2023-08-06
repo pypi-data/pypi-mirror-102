c41901acbf6e6ea8f01717b49e0a062eb9c95e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:31 +0000

    Add package django-blue-users/2.1/django-blue-users-2.1.tar.gz

---
+++ packages/django-blue-users/django-blue-users-2.1.tar.gz/django-blue-users-2.1/blue_users/authentication.py
@@ -0,0 +1,72 @@
+# accounts.authentication
+
+import jwt
+from rest_framework.authentication import BaseAuthentication
+from django.middleware.csrf import CsrfViewMiddleware
+from rest_framework import exceptions
+from django.conf import settings
+from blue_users.models import User
+
+
+class CSRFCheck(CsrfViewMiddleware):
+    def _reject(self, request, reason):
+        # Return the failure reason instead of an HttpResponse
+        return reason
+
+
+class SafeJWTAuthentication(BaseAuthentication):
+    '''
+        custom authentication class for DRF and JWT
+        https://github.com/encode/django-rest-framework/blob/master/rest_framework/authentication.py
+    '''
+
+    def authenticate(self, request):
+
+        #User = get_user_model()
+        authorization_heaader = request.headers.get('Authorization')
+
+        if not authorization_heaader:
+            return None
+        try:
+            # header = 'Token xxxxxxxxxxxxxxxxxxxxxxxx'
+            access_token = authorization_heaader.split(' ')[1]
+            payload = jwt.decode(
+                access_token, settings.SECRET_KEY, algorithms=['HS256'])
+
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('access_token expired')
+        except IndexError:
+            raise exceptions.AuthenticationFailed('Token prefix missing')
+
+        print("MI CONFIGURACION DE USUARIOS")
+        user_exists = User.objects.filter(id=payload['user_id']).exists()
+        if user_exists is False:
+            user = User(
+                username=payload['user_id'],
+                password=payload['user_id'],
+                email="prueba"+payload['user_id']+"bluedraft.com"
+            )
+            user.save()
+        else:
+            user = User.objects.filter(id=payload['user_id']).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed('user is inactive')
+
+        self.enforce_csrf(request)
+        return (user, None)
+
+    def enforce_csrf(self, request):
+        """
+        Enforce CSRF validation
+        """
+        check = CSRFCheck()
+        # populates request.META['CSRF_COOKIE'], which is used in process_view()
+        check.process_request(request)
+        reason = check.process_view(request, None, (), {})
+        print(reason)
+        if reason:
+            # CSRF failed, bail with explicit error message
+            raise exceptions.PermissionDenied('CSRF Failed: %s' % reason)
