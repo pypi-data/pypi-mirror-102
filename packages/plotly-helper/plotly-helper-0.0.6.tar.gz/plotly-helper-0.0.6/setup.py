d53409d1fb8bcceed9ed2c8725b6488eeda48b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:03 +0000

    Add package plotly-helper/0.0.6/plotly-helper-0.0.6.tar.gz

---
+++ packages/plotly-helper/plotly-helper-0.0.6.tar.gz/plotly-helper-0.0.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name="plotly-helper",
+    author="Blue Brain Project, EPFL",
+    description="Package that makes plotly easy",
+    url="https://github.com/bluebrain/plotly-helper",
+    license="LGPLv3",
+    install_requires=[
+        'plotly>=3.4.2',
+        'numpy>=1.15.4',
+        'neurom>=2.0,<3.0',
+        'six>=1.12.0',
+        'click>=6.0',
+    ],
+    extras_require={
+        'docs': ['sphinx', 'sphinx-bluebrain-theme'],
+    },
+    entry_points={
+        'console_scripts': ['viewer=plotly_helper.cli:cli']
+    },
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
