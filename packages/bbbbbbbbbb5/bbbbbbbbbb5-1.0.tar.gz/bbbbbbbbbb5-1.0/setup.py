eaed80fde829fa72725466fde6f57ecd7dba2803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:47 +0000

    Add package bbbbbbbbbb5/1.0/bbbbbbbbbb5-1.0.tar.gz

---
+++ packages/bbbbbbbbbb5/bbbbbbbbbb5-1.0.tar.gz/bbbbbbbbbb5-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from distutils.core import  setup
+setup(
+    name='bbbbbbbbbb5',#对外我们模块的名字
+    version='1.0',#版本号
+    description='这是我第一个对外发布的模块',#描述
+    author='cfc',#作者
+    author_email='18663268209@163.com',
+    py_modules=['baizhan.demo1','baizhan.demo2']#要发布的模块
+)
