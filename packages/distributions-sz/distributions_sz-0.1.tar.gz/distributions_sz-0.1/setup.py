75d30e5a627b010f4835f3fcaf7aae337e8b7316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:54 +0000

    Add package distributions-sz/0.1/distributions_sz-0.1.tar.gz

---
+++ packages/distributions-sz/distributions_sz-0.1.tar.gz/distributions_sz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sz',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sz'],
+      zip_safe=False)
