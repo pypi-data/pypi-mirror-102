701de8c412916f438d9127097b2f74511d9da992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:22 +0000

    Add package mongo-schema/1.0.0.post0/mongo-schema-1.0.0.post0.tar.gz

---
+++ packages/mongo-schema/mongo-schema-1.0.0.post0.tar.gz/mongo-schema-1.0.0.post0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+A minimal ``setup.py`` is still required when using setuptools.
+
+See ``setup.cfg`` for package configuration.
+"""
+
+from setuptools import setup
+setup()
