fc34955a5760e376bdbf0461e6af830ec8e91efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:39 +0000

    Add package zq-logger/0.1/zq_logger-0.1.tar.gz

---
+++ packages/zq-logger/zq_logger-0.1.tar.gz/zq_logger-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="zq_logger",
+    version="0.1",
+    author="zzqq2199",
+    author_email="zhouquanjs@qq.com",
+    description="customized logger",
+    url='http://www.google.com/search?q=zq_logger',
+    packages=['zq_logger'],
+    install_requires=['logging', 'colorlog']
+)
