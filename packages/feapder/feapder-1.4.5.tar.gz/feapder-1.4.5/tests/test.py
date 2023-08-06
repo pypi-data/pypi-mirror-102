5833d8a25b84773718d224d680ddf6bbce13b57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:28 +0000

    Add package feapder/1.4.5/feapder-1.4.5.tar.gz

---
+++ packages/feapder/feapder-1.4.5.tar.gz/feapder-1.4.5/tests/test.py
@@ -0,0 +1,12 @@
+# from feapder.utils import tools
+#
+# url = "https://oapi.dingtalk.com/robot/send?access_token=5ce47232d4183665e93e7d7bf66a914d53c8c62d2d5b4558f94cd3475d24b653"
+#
+# tools.dingding_warning("feapder test", url=url, user_phone="13811037553")
+
+
+# from selenium import webdriver
+# option = webdriver.ChromeOptions()
+# option.add_experimental_option('debuggerAddress','127.0.0.1:9222')
+# driver = webdriver.Chrome(chrome_options=option)
+# driver.get('https://www.aqistudy.cn/historydata/daydata.php?city=北京')
