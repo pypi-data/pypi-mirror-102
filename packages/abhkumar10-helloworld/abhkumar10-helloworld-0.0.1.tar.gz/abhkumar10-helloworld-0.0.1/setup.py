36ecbfd453d0676eed9ce4a5a63c712e1a7ea1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:45 +0000

    Add package abhkumar10-helloworld/0.0.1/abhkumar10-helloworld-0.0.1.tar.gz

---
+++ packages/abhkumar10-helloworld/abhkumar10-helloworld-0.0.1.tar.gz/abhkumar10-helloworld-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="abhkumar10-helloworld",
+    version="0.0.1",  ## 0.0.X is unstable package number
+    description="Say Hello!",
+    py_modules=["helloworld"],
+    package_dir={"": "src"},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "blessings ~= 1.7",
+    ],
+    extras_require={
+        "dev": ["pytest>=3.7"],
+    },
+    url="https://github.com/meabhishekkumar/abhkumar10-helloworld",
+    author="Abhishek Kumar",
+    author_email="abhishek.kumar6@publicissapient.com",
+)
