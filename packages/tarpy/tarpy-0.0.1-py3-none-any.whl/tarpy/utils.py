f9d33aa4eca8fba8100e21a125ad0fbc227025e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:46 +0000

    Add package tarpy/0.0.1/tarpy-0.0.1-py3-none-any.whl

---
+++ packages/tarpy/tarpy-0.0.1-py3-none-any.whl/tarpy/utils.py
@@ -0,0 +1,29 @@
+''' Utils
+
+Module of tarpy.
+'''
+
+import os
+import pathlib
+
+
+def real_path(path: str) -> pathlib.PosixPath[str]:
+    ''' Real Paths
+    '''
+    try:
+        return pathlib.Path(path).resolve()
+    except TypeError:
+        pass
+
+def check_existence(to_check: str) -> bool:
+    ''' Check if File / Directory
+        exists or not
+    '''
+    if not os.path.exists(to_check):
+        return False
+    return True
+
+def check_symlink(to_check: str) -> bool:
+    ''' Check if File is a Symlink
+    '''
+    return pathlib.Path(to_check).is_symlink()
