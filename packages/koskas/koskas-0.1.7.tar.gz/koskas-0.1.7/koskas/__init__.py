a6911bf408c47b48556cab92ab76af74e19e8785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package koskas/0.1.7/koskas-0.1.7.tar.gz

---
+++ packages/koskas/koskas-0.1.7.tar.gz/koskas-0.1.7/koskas/__init__.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import math
+__version__ = "0.1.7"
+__author__ = 'Yair Koskas'
+def greet():
+    print('Hello, i\'m koskas and this is my python package!')
+
+
+def joke():
+    r = requests.get('http://api.icndb.com/jokes/random')
+    s = r.text
+    if type(s) != str:
+        s = s.encode('utf-8')
+    s = s.replace('&quot;','"')
+    print(dict(json.loads(r.text)['value'])['joke'])
+
+
+def isprime(n):
+    for i in range(2,math.ceil(math.sqrt(n))):
+        if n%i == 0:
+            return False
+    return True
+
+
+class Koskas:
+    def __init__(self):
+        pass
+
+    def speak(self):
+        print('Hello, I\'m Yair Koskas')
+        print('If you see this, you are a very special person!')
+        print('Because you are the first person to ever care about that joke of a python package i made!')
+        print('Have a nice day!')
