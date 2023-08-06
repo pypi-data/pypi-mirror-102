b8aac3795ea31d90c96476cd4392003d8d07fe9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:54 +0000

    Add package seven-wxapp/1.0.7/seven_wxapp-1.0.7.tar.gz

---
+++ packages/seven-wxapp/seven_wxapp-1.0.7.tar.gz/seven_wxapp-1.0.7/seven_wxapp/handlers/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJingCan
+@Date: 2020-04-16 14:38:22
+:LastEditTime: 2021-04-09 11:33:22
+:LastEditors: HuangJingCan
+@Description: 
+"""
+__all__ = ["coin", "prize_order", "task", "user"]
