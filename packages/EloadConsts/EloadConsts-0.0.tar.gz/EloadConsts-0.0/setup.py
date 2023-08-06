6b09b2600d6406d5934beb7fa86598069ce99fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:35 +0000

    Add package EloadConsts/0.0/EloadConsts-0.0.tar.gz

---
+++ packages/EloadConsts/EloadConsts-0.0.tar.gz/EloadConsts-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='EloadConsts',
+      version='0.0',
+      description='This module contains consts for the programm "eload" ',
+      packages=['EloadConsts'],
+      author="Igor Korobeinikov",
+      author_email='igorkorobigor@gmail.com',
+      zip_safe=False)
