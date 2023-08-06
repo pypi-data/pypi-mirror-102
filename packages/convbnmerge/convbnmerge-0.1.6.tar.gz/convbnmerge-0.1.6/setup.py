c721b9de438204a6085ce95fea97b31b7f4c138f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:19 +0000

    Add package convbnmerge/0.1.6/convbnmerge-0.1.6.tar.gz

---
+++ packages/convbnmerge/convbnmerge-0.1.6.tar.gz/convbnmerge-0.1.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="convbnmerge",
+    version="0.1.6",
+    description="One-clicked merge convolution and batchnorm to one unified convolution",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    url="https://github.com/vietnamican/conv-bn-merge",
+    author="vietnamican",
+    author_email="vietnamican@gmail.com",
+    packages=["convbnmerge"],
+)
