cd25df7831cac0265730a12b669927f9b6a73f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:22 +0000

    Add package OpenResearchMigration/0.0.12/OpenResearchMigration-0.0.12.tar.gz

---
+++ packages/OpenResearchMigration/OpenResearchMigration-0.0.12.tar.gz/OpenResearchMigration-0.0.12/ormigrate/issue71.py
@@ -0,0 +1,53 @@
+'''
+Created on 2021-04-06
+
+@author: wf
+'''
+import re
+from ormigrate.fixer import PageFixer
+from ormigrate.toolbox import HelperFunctions
+
+
+class DateFixer(PageFixer):
+    '''
+    fixer for Acceptance Rate Not calculated
+    https://github.com/SmartDataAnalytics/OpenResearch/issues/152
+    '''
+
+    def __init__(self, wikiId="or", baseUrl="https://www.openresearch.org/wiki/", debug=False,restoreOut=False):
+        '''
+        Constructor
+        '''
+        # call super constructor
+        super(DateFixer, self).__init__(wikiId, baseUrl)
+        self.debug = debug
+        self.restoreOut = restoreOut
+
+
+    def getFixedDate(self, page, event, datetype='date'):
+        '''
+        fix the date of the given page and event and mark unfixable pages
+        Returns:
+            Fixed text of the page.
+        '''
+        generateLink=False
+        dates = re.findall('|.*'+datetype+'.*=.*\n', event)
+        if len(dates) != 0:
+            for element in dates:
+                name,value=self.getNameValue(element)
+                if name is not None and value is not None:
+                    fixedDate = HelperFunctions.parseDate(value)
+                    if fixedDate is not None:
+                        event = event.replace(element,'|'+name+'='+fixedDate)
+                    else:
+                        generateLink=True
+        if self.debug and generateLink: print(self.generateLink(page))
+        return event
+
+
+if __name__ == "__main__":
+    fixer = DateFixer()
+    fixer.debug = True
+    # fixer.checkAllFiles(fixer.getFixedDate, 'deadline')
+    fixer.fixAllFiles(fixer.getFixedDate, "Date", 'date')
+    fixer.fixAllFiles(fixer.getFixedDate, "Deadline", 'deadline')
