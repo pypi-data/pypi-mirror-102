fa0d9adcb0e5e5d9b7c078e67eb857b04583b6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:27 +0000

    Add package kakaowork/0.1.2/kakaowork-0.1.2.tar.gz

---
+++ packages/kakaowork/kakaowork-0.1.2.tar.gz/kakaowork-0.1.2/kakaowork/__init__.py
@@ -0,0 +1,13 @@
+from kakaowork.blockkit import (BlockType, ButtonStyle, ButtonActionType, HeaderStyle, BlockKitType, SelectBlockOption, TextBlock, ImageLinkBlock, ButtonBlock,
+                                DividerBlock, HeaderBlock, ActionBlock, DescriptionBlock, SectionBlock, ContextBlock, LabelBlock, InputBlock, SelectBlock,
+                                BlockKitBuilder)
+
+from kakaowork.exceptions import (KakaoworkError, InvalidBlock)
+
+from kakaowork.client import Kakaowork
+
+from kakaowork.models import (ErrorCode, ColorTone, ProfileNameFormat, ProfilePositionFormat, BotStatus, ErrorField, UserField, ConversationField, MessageField,
+                              DepartmentField, SpaceField, BotField, BaseResponse, UserResponse, UserListResponse, ConversationResponse,
+                              ConversationListResponse, MessageResponse, DepartmentListResponse, SpaceResponse, BotResponse)
+
+__version__ = '0.1.2'
