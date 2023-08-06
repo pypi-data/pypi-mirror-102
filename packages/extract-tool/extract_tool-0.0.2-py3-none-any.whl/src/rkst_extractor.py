47f88b5b4917f6ec6f6bf57438fc1415be51b32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:42 +0000

    Add package extract-tool/0.0.2/extract_tool-0.0.2-py3-none-any.whl

---
+++ packages/extract-tool/extract_tool-0.0.2-py3-none-any.whl/src/rkst_extractor.py
@@ -0,0 +1,10 @@
+import tldextract
+
+class JobUtils:
+
+    def __init__(self):
+        self.tld_extractor = tldextract.TLDExtract(cache_file='../input/tld_lists.txt')
+
+    def parse(self, record):
+        ext = self.tld_extractor(record)
+        return ext
