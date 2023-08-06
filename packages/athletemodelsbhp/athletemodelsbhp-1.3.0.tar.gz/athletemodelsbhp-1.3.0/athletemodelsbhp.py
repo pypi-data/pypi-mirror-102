c65ccc0e40ccf0c65e3c595c0e5df4116ac0fd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:27 +0000

    Add package athletemodelsbhp/1.3.0/athletemodelsbhp-1.3.0.tar.gz

---
+++ packages/athletemodelsbhp/athletemodelsbhp-1.3.0.tar.gz/athletemodelsbhp-1.3.0/athletemodelsbhp.py
@@ -0,0 +1,53 @@
+import os
+import pickle
+import json
+from athletelistsbhp import AthleteListSBhp
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+
+    (mins,secnd) = timestrin.split(splitter)
+    return(mins + '.' + second)
+
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline()
+        temp = data.strip().split(',')
+        return(AthleteListSBhp(temp.pop(0),temp.pop(0),temp))
+    except IOError as Err1:
+        print('File IOError (get_coach_data):'+ Err1)
+
+def put_to_store(filelist):
+    all_athletes = {}
+    for each_file in filelist:
+        athlist = get_coach_data(each_file)
+        all_athletes[athlist.Name] = athlist
+    try:
+        with open('athlete.pickle','wb') as athpck:
+            pickle.dump(all_athletes,athpck)
+    except IOError as Err2:
+        print('File IO Error (Put_to _store):' + str(Err2))
+    return(all_athletes)
+
+def get_from_store():
+    all_athletes = {}
+    try:
+        with open('athlete.pickle','rb') as athpck:
+            all_athletes = pickle.load(athpck)
+    except IOError as Err3:
+        print('File IO Error (get_from_store):' + str(Err3))
+    return(all_athletes)
+
+def get_names_from_store():
+    athletes = get_from_store()
+    names = [athletes[each_atlete].Name for each_atlete in athletes]
+    namesJson = json.dumps(names)
+    return(namesJson)
+
+
