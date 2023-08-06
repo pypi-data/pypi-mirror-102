75189b23cf34b141e5df3d83a2162fccebe1b784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:38 +0000

    Add package pyvacon/0.1.10/pyvacon-0.1.10-cp38-cp38-win_amd64.whl

---
+++ packages/pyvacon/pyvacon-0.1.10-cp38-cp38-win_amd64.whl/pyvacon-0.1.10.data/purelib/pyvacon/version.py
@@ -0,0 +1,14 @@
+__major_version__=0
+__minor_version__=1
+__patch_version__=10
+is_beta=False
+__version__ = '0.1.10'
+version=__version__
+version_long = '0.1.10:fav_0.1.10'
+description = 'fav_0.1.10'
+commit = '793e9c6256139677c57026810dd400224d79e910'
+branch = 'feature/swig_stickyness'
+is_dirty = False
+email ='c.menn@frontmark.de'
+author ='Christian Menn'
+prefix='fav'
