6853006d2c616b2a716c4902085a98e580cf6da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:21 +0000

    Add package PyWin10UI/0.0.4/PyWin10UI-0.0.4.tar.gz

---
+++ packages/PyWin10UI/PyWin10UI-0.0.4.tar.gz/PyWin10UI-0.0.4/setup.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+from setuptools import setup
+
+setup(name='PyWin10UI',
+      version='0.0.4',
+      description='2021/4/17',
+      url='http://pywin10ui.grmine.cn/',
+      author='bytfr 啃竹子',
+      author_email='2678509244@qq.com',
+      license='MIT',
+      packages=['pywin10ui'])
