0291d348ec5f055df1b4ec437645f1e99550b09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:43 +0000

    Add package youtube-downloader-website/0.0.1/youtube%20downloader%20website-0.0.1.tar.gz

---
+++ packages/youtube-downloader-website/youtube%20downloader%20website-0.0.1.tar.gz/youtube downloader website-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='youtube downloader website',
+    packages=['youtube downloader website'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'pytube',
+    ],
+	package_dir={"": r"C:\Users\ASUS\PycharmProjects\youtube downloader website"},
+
+)
